c71e89cb73482df3a891c1fa8bb868a45cbfc55c
Charles Baynham <c.baynham@imperial.ac.uk>
2022-05-16 17:05:34 +0100

    Use new structure

--- flake.nix
+++ flake.nix
@@ -1,19 +1,35 @@
 {
-  inputs.artiq.url = "git+ssh://git@gitlab.com/aion-physics/code/artiq/forks/artiq_fork.git";
-  inputs.nixpkgs.follows = "artiq/nixpkgs";
+  inputs = {
+    artiq.url = "git+ssh://git@gitlab.com/aion-physics/code/artiq/forks/artiq_fork.git";
+    nixpkgs.follows = "artiq/nixpkgs";
+  };
 
   outputs = { self, artiq, nixpkgs }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in {
+    in rec {
       devShell.x86_64-linux = pkgs.mkShell {
         name = "icl-artiq-environment";
         buildInputs = [
           (pkgs.python3.withPackages(ps: [
-            artiq.packages.x86_64-linux.artiq 
+            artiq.packages.x86_64-linux.artiq
             ps.numpy ps.ipython ps.jupyter ps.pip
            ]))
         ];
       };
+
+      devShells.x86_64-linux.flash = pkgs.mkShell {
+        name = "artiq-environment";
+        buildInputs = artiq.devShell.x86_64-linux.buildInputs ++
+        [
+          artiq.packages.x86_64-linux.openocd-bscanspi
+        ];
+      };
     };
+
+  nixConfig = {
+    extra-trusted-public-keys = "nixbld.m-labs.hk-1:5aSRVA5b320xbNvu30tqxVPXpld73bhtOeH6uAjRyHc=";
+    extra-substituters = "https://nixbld.m-labs.hk";
+    extra-sandbox-paths = "/opt";
+  };
 }
