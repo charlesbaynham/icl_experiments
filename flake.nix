eddeabe2b1aaf9c62547d5df6ab04b98adc5ab5f
Charles Baynham <c.baynham@imperial.ac.uk>
2022-06-15 18:10:07 +0100

    Break apart code for debugging

--- flake.nix
+++ flake.nix
@@ -17,23 +17,18 @@
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
 
+      patched_artiq = artiq.packages.x86_64-linux.artiq  // { "version" = "1.2.3"; };
+
       artiq_override = self: super: {
         # artiq = artiq.packages.x86_64-linux.artiq;
-        artiq = artiq.packages.x86_64-linux.artiq  // { "version" = "1.2.3"; };
+        artiq = patched_artiq;
       };
 
-    in
-    rec {
-      inherit pkgs;
-
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        name = "icl-artiq-environment";
-        buildInputs = [
-          (mach-nix.lib.x86_64-linux.mkPython {
+      mnix = mach-nix.lib.x86_64-linux.mkPython {
             requirements = ''
               numpy  # (for example - I actually need more)
               pip
-              artiq
+              artiq > 1.0
             '';
             packagesExtra = [
               pyaion.packages.x86_64-linux.pyaion
@@ -42,7 +37,31 @@
             providers = {
               artiq = "nixpkgs";
             };
-          })
+          };
+
+    in
+    rec {
+      inherit pkgs mnix patched_artiq;
+
+      # mnixPkgs  = mach-nix.lib.x86_64-linux.mkNixpkgs  {
+      #       requirements = ''
+      #         numpy  # (for example - I actually need more)
+      #         pip
+      #         artiq > 1.0
+      #       '';
+      #       packagesExtra = [
+      #         pyaion.packages.x86_64-linux.pyaion
+      #       ];
+      #       overridesPre = [ artiq_override ];
+      #       providers = {
+      #         artiq = "nixpkgs";
+      #       };
+      #     };
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        name = "icl-artiq-environment";
+        buildInputs = [
+          (mnix)
         ];
       };
     };
