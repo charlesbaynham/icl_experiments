9c4d0fdb0f29006e6f9d0eff40c75e6b684a70d8
Stronlab - ARTIQ server <stronlab@ic.ac.uk>
2022-07-09 18:23:34 +0000

    debug

--- repository/monitor_weather.py
+++ repository/monitor_weather.py
@@ -80,6 +80,8 @@ class MonitorWeather(EnvExperiment):
             try:
                 timestamp, weather_data = self.get_weather()
 
+                print(weather_data)
+
                 if timestamp != last_timestamp:
                     last_timestamp = timestamp
                     self.influx_logger.write(
