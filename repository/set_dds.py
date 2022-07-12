4c4b8c09e38013e276a34780dc94396ad49967ea
Stronlab - ARTIQ server <stronlab@ic.ac.uk>
2022-07-12 16:02:42 +0000

    Extend simple dds setter

--- repository/set_dds.py
+++ repository/set_dds.py
@@ -1,7 +1,11 @@
+import re
+
 from artiq.coredevice.ad9912 import AD9912
+from artiq.experiment import BooleanValue
 from artiq.experiment import EnumerationValue
 from artiq.experiment import EnvExperiment
 from artiq.experiment import kernel
+from artiq.experiment import NumberValue
 
 
 class SetDDS(EnvExperiment):
@@ -10,7 +14,22 @@ class SetDDS(EnvExperiment):
     def build(self):
         self.setattr_device("core")
 
-        urukuls = [k for k in self.get_device_db().keys() if "urukul" in k]
+        self.setattr_argument(
+            "frequency",
+            NumberValue(default=20e6, unit="MHz", step=0.1e6, ndecimals=1, min=0),
+        )
+        self.setattr_argument(
+            "attenuation",
+            NumberValue(default=0, unit="dB", step=0.1, ndecimals=1, min=0),
+        )
+        self.setattr_argument(
+            "switch_status",
+            BooleanValue(default=True),
+        )
+
+        urukuls = [
+            k for k in self.get_device_db().keys() if re.match(r"urukul\d_ch\d", k)
+        ]
 
         self.setattr_argument("dds_id", EnumerationValue(urukuls))
 
@@ -23,6 +42,6 @@ class SetDDS(EnvExperiment):
         dds = self.dds  # type: AD9912
 
         dds.init()
-        dds.set(10e6, 0.0)
-        dds.set_att(0.0)
-        dds.sw.on()
+        dds.set(self.frequency, 0.0)
+        dds.set_att(self.attenuation)
+        dds.sw.set_o(self.switch_status)
