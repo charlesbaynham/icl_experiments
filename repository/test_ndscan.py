8e7cc554492efc5e9d65fd47115a20fc5ff4d95d
Stronlab - ARTIQ server <stronlab@ic.ac.uk>
2022-07-13 21:42:53 +0000

    Test ndscan

--- repository/test_ndscan.py
+++ repository/test_ndscan.py
@@ -1,13 +1,21 @@
-# from ndscan.experiment import ExpFragment
-# from artiq.experiment import EnvExperiment
-# class MyExpFragment(ExpFragment):
-#     def build_fragment(self):
-#         # ...
-#         self.setattr_result("foo")
-#     def run_once(self):
-#         # ...
-# class MyEnvExperiment(EnvExperiment):
-#     def run(self):
-#         results = create_and_run_once(self, MyExpFragment)
-#         print(results["foo"])
-# MyExpFragmentScan = make_fragment_scan_exp(MyExpFragment)
+from artiq.experiment import EnvExperiment
+from ndscan.experiment import ExpFragment
+from ndscan.experiment.entry_point import create_and_run_once
+from ndscan.experiment.entry_point import make_fragment_scan_exp
+
+
+class MyExpFragment(ExpFragment):
+    def build_fragment(self):
+        self.setattr_result("foo")
+
+    def run_once(self):
+        self.foo = 123
+
+
+class MyEnvExperiment(EnvExperiment):
+    def run(self):
+        results = create_and_run_once(self, MyExpFragment)
+        print(results["foo"])
+
+
+MyExpFragmentScan = make_fragment_scan_exp(MyExpFragment)
