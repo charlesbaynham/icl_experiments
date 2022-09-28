9f85baf215aad82b1919b6d1060eff13b74ae83c
Stronlab - ARTIQ server <stronlab@ic.ac.uk>
2022-09-28 16:10:03 +0000

    Handle dicts as well as floats

--- repository/monitors/monitor_master.py
+++ repository/monitors/monitor_master.py
@@ -8,14 +8,28 @@ logger = logging.getLogger(__name__)
 
 
 def my_db_logger(self, name, state, data):
-    l: InfluxController = self.influx_logger
-    # l.write(something)
 
-    assert l
+    if isinstance(data, dict):
+        fields = data
+    elif isinstance(data, float):
+        fields = {"value": data}
+    else:
+        raise ValueError(
+            "Data type %s not supported - only floats and dicts are accepted", data
+        )
 
-    print("(not writing to db) {} - {} - {}".format(name, state, data))
+    logger.debug("Writing to database: type = %s, fields = %s", name, data)
+
+    self.influx_logger: InfluxController
+    self.influx_logger.write(
+        tags={"type": name},
+        fields=fields,
+    )
 
 
 MyMonitorMaster = make_monitor_controller(
-    "MyMonitorMaster", monitors=[MonitorWeather], devices=["influx_logger"]
+    "MyMonitorMaster",
+    monitors={"weather": MonitorWeather},
+    devices=["influx_logger"],
+    data_logger=my_db_logger,
 )
