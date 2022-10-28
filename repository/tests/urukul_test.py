434f3080a5be730a0834e36ff81c61e890508223
Elizabeth Pasatembou <e.pasatembou21@imperial.ac.uk>
2022-10-28 14:30:58 +0100

    urukul test changes

--- repository/tests/urukul_test.py
+++ repository/tests/urukul_test.py
@@ -1,10 +1,7 @@
 import artiq
                                
 
-from artiq.experiment import* 
-from regex import D
-from artiq.coredevice.suservo import Channel
-from artiq.coredevice.suservo import SUServo                                  #imports everything from the artiq experiment library
+from artiq.experiment import*                                   #imports everything from the artiq experiment library
 
 #This code outputs a single frequency at a fixed amplitude on a single channel of the urukul
 #The following must be input from the dashboard:
@@ -14,24 +11,34 @@ from artiq.coredevice.suservo import SUServo                                  #i
 #pulse length(in s)
 
 class Urukul_Programmable(EnvExperiment):
-    """Urukul Selectable Frequency, Amplitude, Attenuation and Pulse Length"""
+    """Urukul Test"""
     def build(self): #This code runs on the host device
         
 
         self.setattr_device("core")                                                     #sets core device drivers as attributes
-        self.setattr_device("suservo0")  
+        self.setattr_device("suservo0")   
         
-        
-        dev_db = self.get_device_db()
-        check_array = [d for d in dev_db.keys() if re.match(r"suservo\d+_ch\d+", d)] # regex101.com
-                                                    #sets urukul0, channel 1 device drivers as attributes
+        check_array = [d for d in dev_db.keys() if re.match(r"suservo\d+_ch\d+", d)]
+        check_2 = [d for d in dev_db.keys() if re.match(r"urukul\d+_ch\d+", d)]
+                                                   #sets urukul0, channel 1 device drivers as attributes
         self.setattr_argument("freq", NumberValue(ndecimals=0, unit="MHz", step=1))     #instructs dashboard to take input in MHz and set it as an attribute called freq
         self.setattr_argument("amp", NumberValue(ndecimals=2, step=1))                  #instructs dashboard to take input and set it as an attribute called amp
         self.setattr_argument("atten", NumberValue(ndecimals=2, step=1))                #instructs dashboard to take input and set it as an attribute called atten
-        #self.setattr_argument("t_pulse", NumberValue(ndecimals=2, unit = "s", step=1))  #instructs dashboard to take input and set it as an attribute called t_pulse
+        self.setattr_argument("t_pulse", NumberValue(ndecimals=2, unit = "s", step=1))  #instructs dashboard to take input and set it as an attribute called t_pulse
         
     
     @kernel #This code runs on the FPGA
     def run(self):  
         self.core.reset()                                       #resets core device
-                            
+        self.urukul0_ch1.cpld.init()                            #initialises CPLD on channel 1
+        self.urukul0_ch1.init()                                 #initialises channel 1
+        delay(10 * ms)                                          #10ms delay
+        
+        
+        self.urukul0_ch1.set_att(self.atten)                    #writes attenuation to urukul channel
+        self.urukul0_ch1.sw.on()                                #switches urukul channel on
+           
+            
+        self.urukul0_ch1.set(self.freq, amplitude = self.amp)   #writes frequency and amplitude attributes to urukul channel thus outputting function
+        delay(self.t_pulse * s)                                 #delay determined by user input
+        self.urukul0_ch1.sw.off()                               #switches urukul channel off
