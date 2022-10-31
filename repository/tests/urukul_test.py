23b37c06c7c2e4078f6ef5a05e3f7654cb0e67e9
Elizabeth Pasatembou <e.pasatembou21@imperial.ac.uk>
2022-10-31 18:31:44 +0000

    Working urukul test

--- repository/tests/urukul_test.py
+++ repository/tests/urukul_test.py
@@ -7,7 +7,7 @@ from artiq.coredevice.fastino import Fastino
 from artiq.coredevice.suservo import Channel
 from artiq.coredevice.suservo import SUServo
 from artiq.experiment import *
-from regex import D                                #imports everything from the artiq experiment library
+                     #imports everything from the artiq experiment library
 
 #This code outputs a single frequency at a fixed amplitude on a single channel of the urukul
 #The following must be input from the dashboard:
@@ -19,64 +19,96 @@ from regex import D                                #imports everything from the 
 class Urukul_Programmable(EnvExperiment):
     """Urukul Test"""
     def build(self): #This code runs on the host device
-        
 
         self.setattr_device("core")               #sets core device drivers as attributes
-        self.setattr_device("suservo0") 
+        self.setattr_device("suservo0")
+        self.setattr_device("suservo0_ch0")
 
-        dev_db = self.get_device_db()
-        check_array = [d for d in dev_db.keys() if re.match(r"suservo\d+_ch\d+", d)]
+        self.setattr_device("suservo1")
+        self.setattr_device("suservo1_ch0")
+
+
+        self.suservo0 : SUServo
+        self.suservo0_ch0 : Channel
         
-        for val in check_array:
-            self.setattr_device(val)
-                                                   #sets urukul0, channel 1 device drivers as attributes
-        self.setattr_argument("freq", NumberValue(ndecimals=0, unit="MHz", step=1))     #instructs dashboard to take input in MHz and set it as an attribute called freq                 #instructs dashboard to take input and set it as an attribute called amp
-        self.setattr_argument("att", NumberValue(default=0, unit="dB", min=0, max=31.5, ndecimals=1))  # instructs dashboard to take input and set it as an attribute called amp
-        self.setattr_argument("phase", NumberValue(default=0, min=0, max=1, ndecimals=2))
-        self.setattr_argument("DDS", EnumerationValue(check_array, default=check_array[0]))
-    
-
-     #This code runs on the FPGA
-    def run(self): 
-        dds = self.get_device(self.DDS)
-        print(dds)
-        self.fpga(dds)
         
     @kernel
-    def fpga(self, dds):
-        # type:(Channel) -> None
-
-        
-        self.core.reset()  # resets core device
+    def run(self):
         
         cpld = self.suservo0.cplds[0]
         self.core.reset()
 
-        self.suservo0.init()
+
+        self.suservo1.init()
         delay(1 * us)
 
         # ADC PGIA gain
-        for i in range(8):
-            self.suservo0.set_pgia_mu(i, 0)
-            delay(10 * us)
+        # for i in range(8):
+        #     self.suservo0.set_pgia_mu(i, 0)
+        #     delay(10 * us)
 
         # DDS attenuator
-        cpld.set_att(0, 10.0)
+        cpld.set_att(0, 0.)
         delay(1 * us)
 
-        # Servo is done and disabled
-        assert self.suservo0.get_status() & 0xFF == 2
+
 
         # set up profile 0 on channel 0:
         delay(120 * us)
-        dds.set_y(profile=0, y=0.0)  # clear integrator
-        dds.set_dds(
+        self.suservo0_ch0.set_y(profile=0, y=1.0)  # clear integrator
+        
+        self.suservo0_ch0.set_dds(
             profile=0,
             offset=-0.5,  # 5 V with above PGIA settings
-            frequency=self.freq,
-            phase=self.phase,
+            frequency=100e6,
+            phase=0.0,
         )
         # enable RF, IIR updates and profile 0
-        dds.set(en_out=1, en_iir=0, profile=0)
+        self.suservo0_ch0.set(en_out=1, en_iir=0, profile=0)
         # enable global servo iterations
         self.suservo0.set_config(enable=1)
+
+
+    # def run(self): 
+    #     dds = self.get_device(self.DDS)
+    #     print(dds)
+    #     self.fpga(dds)
+        
+    # @kernel
+    # def fpga(self, dds):
+    #     # type:(Channel) -> None
+
+        
+    #     self.core.reset()  # resets core device
+        
+    #     cpld = self.suservo0.cplds[0]
+    #     self.core.reset()
+
+    #     self.suservo0.init()
+    #     delay(1 * us)
+
+    #     # ADC PGIA gain
+    #     for i in range(8):
+    #         self.suservo0.set_pgia_mu(i, 0)
+    #         delay(10 * us)
+
+    #     # DDS attenuator
+    #     cpld.set_att(0, 10.0)
+    #     delay(1 * us)
+
+    #     # Servo is done and disabled
+    #     assert self.suservo0.get_status() & 0xFF == 2
+
+    #     # set up profile 0 on channel 0:
+    #     delay(120 * us)
+    #     dds.set_y(profile=0, y=0.0)  # clear integrator
+    #     dds.set_dds(
+    #         profile=0,
+    #         offset=-0.5,  # 5 V with above PGIA settings
+    #         frequency=self.freq,
+    #         phase=self.phase,
+    #     )
+    #     # enable RF, IIR updates and profile 0
+    #     dds.set(en_out=1, en_iir=0, profile=0)
+    #     # enable global servo iterations
+    #     self.suservo0.set_config(enable=1)
