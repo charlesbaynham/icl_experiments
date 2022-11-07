bdc65079a323719854257f3bb99fd8ae94639ea5
Charles Baynham <c.baynham@imperial.ac.uk>
2022-11-07 15:32:15 +0000

    Sketch of AOM turning-on calibration dep

--- device_db_config/_aliases.py
+++ device_db_config/_aliases.py
@@ -1,13 +1,4 @@
 # List of aliases for the hardware devices present in _device_db.py.
 # These should be friendly names that describe the purpose of a connection.
 # These are just examples, add your own!
-aliases = {
-    "dds_transfer_cavity_aom": "urukul0_ch2",
-    "ttl_transfer_cavity_trigger": "ttl0",
-    # It's also possible to store generic information here about the hardware
-    # configuration of your experiment. For example, let's imagine that I need
-    # to record the polarity of a coil driver's connection to a coil, I could use:
-    "coil_polarity": "positive",
-    # and could retrieve that information with
-    #     self.get_device_db()["coil_polarity"]
-}
+aliases = {"suservo_injection_aom": "suservo0_ch3"}
--- repository/calibrations/blue_system.py
+++ repository/calibrations/blue_system.py
@@ -1,30 +1,49 @@
 import logging
 
-import requests
 from artiq.coredevice.suservo import Channel
-from ndscan.experiment import Fragment
-from ndscan.experiment import StringParam
+from artiq.coredevice.suservo import SUServo
+from artiq.experiment import kernel
+from artiq.experiment import portable
+from artiq.experiment import RTIOUnderflow
 from qbutler.calibration import Calibration
 from qbutler.calibration import CalibrationResult
 
 logger = logging.getLogger(__name__)
 
 
-class BlueInjectedDiode(Calibration):
+class BlueInjectionAOM(Calibration):
     """
-    Calibration for ensuring a blue diode is injected
+    Ensure that the double-pass AOM which injects the blue diodes has been set
+    up and turned on
     """
 
     def build_calibration(self):
-        pass
+        self.setup_completed = False
 
+        self.setattr_device("core")
+        self.suservo_injection_aom: Channel = self.get_device("suservo_injection_aom")
+
+    @kernel
     def device_setup(self) -> None:
         self.device_setup_subfragments()
 
-        # Ensure AOM is on and active
-        self.aom_suservo_channel: Channel
+        try:
+            self.suservo_injection_aom.servo.init()
+        except RTIOUnderflow:
+            self.core.break_realtime()
+            self.suservo_injection_aom.servo.init()
 
-        self.aom_suservo_channel
-
+    @portable
     def run_once(self):
-        pass
+        if self.setup_completed:
+            self.status.push(CalibrationResult.OK)
+        else:
+            self.status.push(CalibrationResult.BAD_EXPIRED)
+
+        self.data.push(None)
+
+    def fix_own_state(self):
+        self.setup_completed = True
+
+        # TODO: turn on the AOM here
+        raise NotImplementedError
