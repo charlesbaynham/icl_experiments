230538f2a5c6c956d35e7852163bb0552ecd65f8
Strontium lab <stronlab@ic.ac.uk>
2022-11-09 16:33:05 +0000

    Plug in blue AOMs

--- device_db_config/_aliases.py
+++ device_db_config/_aliases.py
@@ -1,4 +1,8 @@
 # List of aliases for the hardware devices present in _device_db.py.
 # These should be friendly names that describe the purpose of a connection.
 # These are just examples, add your own!
-aliases = {"suservo_injection_aom": "suservo0_ch3"}
+aliases = {
+    "suservo_aom_doublepass_461_injection": "suservo0_ch3",
+    "suservo_aom_singlepass_461_spectroscopy": "suservo0_ch7",
+    "suservo_aom_singlepass_461_push": "suservo1_ch3",
+}
