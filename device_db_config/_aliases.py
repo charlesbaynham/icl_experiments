fa9ec247b2c8c8e949a34d3b77605cc9f6043fd3
stronlab <stronlab@ARTIQcontrol.myguest.virtualbox.org>
2023-02-01 17:49:00 +0000

    Move blue DDS channels around

--- device_db_config/_aliases.py
+++ device_db_config/_aliases.py
@@ -5,4 +5,7 @@ aliases = {
     "suservo_aom_doublepass_461_injection": "suservo0_ch3",
     "suservo_aom_singlepass_461_spectroscopy": "suservo0_ch7",
     "suservo_aom_singlepass_461_push": "suservo1_ch3",
+    "suservo_aom_singlepass_461_IJD1_out": "suservo0_ch2",
+    "suservo_aom_singlepass_461_IJD2_out": "suservo0_ch0",
+    "suservo_aom_singlepass_461_IJD3_out": "suservo0_ch1",
 }
