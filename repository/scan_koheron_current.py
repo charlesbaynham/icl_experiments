b8c4f85fdbf07537a9c60c505e86b80e0f8e6abf
Charles Baynham <c.baynham@imperial.ac.uk>
2023-02-13 18:51:04 +0000

    Add control of temperature

--- repository/scan_koheron_current.py
+++ repository/scan_koheron_current.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from artiq.coredevice.core import Core
 from artiq.coredevice.sampler import Sampler
@@ -44,6 +45,30 @@ class ScanKoheronCurrentFrag(ExpFragment):
             step=0.1,
         )
         self.current: FloatParamHandle
+
+        self.setattr_param(
+            "temperature",
+            FloatParam,
+            description="Temperature",
+            default=0,
+            min=250,
+            max=400,
+            unit="K",
+            step=0.1,
+        )
+        self.temperature: FloatParamHandle
+
+        self.setattr_param(
+            "temperature_waittime",
+            FloatParam,
+            description="Time to wait after a temperature change",
+            default=30,
+            min=0,
+            max=1000,
+            unit="s",
+            step=1,
+        )
+        self.temperature_waittime: FloatParamHandle
 
         # Choose the controller to set:
         controller_names = [
@@ -102,9 +127,29 @@ class ScanKoheronCurrentFrag(ExpFragment):
 
     @kernel
     def run_once(self):
+        self.set_temperature(self.temperature.get())
         self.set_current(self.current.get())
         voltage = self.suservo_reader.read_adc()
         self.voltage.push(voltage)
+
+    @rpc
+    def set_temperature(self, temperature):
+        current_temperature_sp = round(self.controller.get_temperature_setpoint())
+        current_temperature_actual = round(self.controller.get_temperature_actual())
+
+        if (
+            # if the setpoint is already correct...
+            round(current_temperature_sp, 2) == round(temperature, 2)
+            and
+            # and we're within 100mK of the right temperature
+            abs(current_temperature_actual - temperature) < 0.1
+        ):
+            # ... then assume everything is fine and do nothing
+            return
+
+        # Otherwise, set the temperature and wait for it
+        self.controller.set_temperature_setpoint(round(temperature, 2))
+        time.sleep(self.temperature_waittime.get())
 
     @rpc
     def set_current(self, current):
