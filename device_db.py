bb4374f9c638a648ecff282fb1c5de3bf5e20f93
Charles Baynham <c.baynham@imperial.ac.uk>
2023-03-03 09:48:41 +0000

    Always reload when refreshing the device_db

--- device_db.py
+++ device_db.py
@@ -1,7 +1,3 @@
-import importlib
-
 import device_db_config
 
-# Force reload of the module in case it has been updated
-importlib.reload(device_db_config)
 device_db = device_db_config.get_device_db()
