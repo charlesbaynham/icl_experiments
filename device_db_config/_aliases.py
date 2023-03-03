630621be186d55ff0a07edfbea008fa3ba345650
Charles Baynham <c.baynham@imperial.ac.uk>
2023-03-03 09:35:57 +0000

    Relabel blue AOMs

--- device_db_config/_aliases.py
+++ device_db_config/_aliases.py
@@ -2,9 +2,9 @@
 # These should be friendly names that describe the purpose of a connection.
 # These are just examples, add your own!
 aliases = {
-    "suservo_aom_doublepass_461_injection": "suservo0_ch3",
-    "suservo_aom_singlepass_461_spectroscopy": "suservo0_ch7",
-    "suservo_aom_singlepass_461_pushbeam": "suservo0_ch2",
-    "suservo_aom_singlepass_461_2dmot_a": "suservo0_ch0",
-    "suservo_aom_singlepass_461_2dmot_b": "suservo0_ch1",
+    "suservo_aom_doublepass_461_injection": "suservo1_ch3",
+    "suservo_aom_singlepass_461_spectroscopy": "suservo1_ch7",
+    "suservo_aom_singlepass_461_pushbeam": "suservo1_ch2",
+    "suservo_aom_singlepass_461_2dmot_a": "suservo1_ch0",
+    "suservo_aom_singlepass_461_2dmot_b": "suservo1_ch1",
 }
