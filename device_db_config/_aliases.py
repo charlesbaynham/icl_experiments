a8b361d2821746dcce349613c6b13baac6ee287a
Charles Baynham <c.baynham@imperial.ac.uk>
2023-03-30 18:35:07 +0000

    Add IJD monitor

--- device_db_config/_aliases.py
+++ device_db_config/_aliases.py
@@ -7,4 +7,9 @@ aliases = {
     "suservo_aom_singlepass_461_pushbeam": "suservo1_ch2",
     "suservo_aom_singlepass_461_2dmot_a": "suservo1_ch0",
     "suservo_aom_singlepass_461_2dmot_b": "suservo1_ch1",
+    "sampler_IJD_monitor": "sampler0",
+    # These are not devices, but can still be retrieved from the device_db
+    "sampler_channel_IJD1": 0,
+    "sampler_channel_IJD2": 1,
+    "sampler_channel_IJD3": 2,
 }
