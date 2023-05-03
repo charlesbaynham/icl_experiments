0f2dc85665524cae44bb5031800579ba13fa8b3c
Elizabeth Pasatembou <e.pasatembou21@imperial.ac.uk>
2023-05-03 11:47:23 +0100

    changes to urukul code

--- repository/urukul.py
+++ repository/urukul.py
@@ -1,15 +1,8 @@
-from artiq.experiment import*                                   #imports everything from the artiq experiment library
-
-#This code outputs a single frequency at a fixed amplitude on a single channel of the urukul
-#The following must be input from the dashboard:
-#frequency(in MHz)
-#amplitude(as amplitude scale factor so between 0 and 1)
-#attenuation(in db, between 0 and 31.5)
-#pulse length(in s)
+from artiq.experiment import*                                
 
 class Urukul_Programmable(EnvExperiment):
-    """Urukul Selectable Frequency, Amplitude, Attenuation and Pulse Length"""
-    def build(self): #This code runs on the host device
+    """Urukul frequency, amplitude and attenuation"""
+    def build(self): 
         
 
         self.setattr_device("core")                                                     #sets core device drivers as attributes
@@ -17,10 +10,10 @@ class Urukul_Programmable(EnvExperiment):
         self.setattr_argument("freq", NumberValue(ndecimals=0, unit="MHz", step=1))     #instructs dashboard to take input in MHz and set it as an attribute called freq
         self.setattr_argument("amp", NumberValue(ndecimals=2, step=1))                  #instructs dashboard to take input and set it as an attribute called amp
         self.setattr_argument("atten", NumberValue(ndecimals=2, step=1))                #instructs dashboard to take input and set it as an attribute called atten
-        self.setattr_argument("t_pulse", NumberValue(ndecimals=2, unit = "s", step=1))  #instructs dashboard to take input and set it as an attribute called t_pulse
+        
         
     
-    @kernel #This code runs on the FPGA
+    @kernel 
     def run(self):  
         self.core.reset()                                       #resets core device
         self.urukul0_ch1.cpld.init()                            #initialises CPLD on channel 1
@@ -32,6 +25,5 @@ class Urukul_Programmable(EnvExperiment):
         self.urukul0_ch1.sw.on()                                #switches urukul channel on
            
             
-        self.urukul0_ch1.set(self.freq, amplitude = self.amp)   #writes frequency and amplitude attributes to urukul channel thus outputting function
-        delay(self.t_pulse * s)                                 #delay determined by user input
+        self.urukul0_ch1.set(self.freq, amplitude = self.amp)   #writes frequency and amplitude attributes to urukul channel thus outputting function                             #delay determined by user input
         self.urukul0_ch1.sw.off()                               #switches urukul channel off
