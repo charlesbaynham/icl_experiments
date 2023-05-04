e9c64ff09b3245e63bfaa1e7521ccd31ca10c7e6
Elizabeth Pasatembou <e.pasatembou21@imperial.ac.uk>
2023-05-04 09:56:50 +0100

    urukul changes

--- repository/urukul.py
+++ repository/urukul.py
@@ -1,32 +1,38 @@
 from artiq.experiment import*                                
-
+from artiq.coredevice.ad9910 import AD9910
 class Urukul_Programmable(EnvExperiment):
     """Urukul frequency, amplitude and attenuation"""
     def build(self): 
         
         urukuls = []
-        for i in range(8):
+        for i in range(4):
             urukuls.append("urukul0_ch{a}".format(a = i))
 
-        self.setattr_device("core")                                                                             #sets core device drivers as attributes
-        self.setattr_device("urukul0") 
+        self.setattr_device("core") 
+        #sets core device drivers as attributes
+        self.setattr_device("urukul0")
+        self.urukul0 = self.get_device("urukul0")
+        
        
         self.setattr_argument("freq", NumberValue(ndecimals=0, unit="MHz", step=1, min=0))     #instructs dashboard to take input in MHz and set it as an attribute called freq
-        self.setattr_argument("amp", NumberValue(ndecimals=2, step=0.5, min = 0, max = 1))                                          #instructs dashboard to take input and set it as an attribute called amp
+        self.setattr_argument("amp", NumberValue(ndecimals=2, step=0.5, min = 0, max = 1))     #instructs dashboard to take input and set it as an attribute called amp
         self.setattr_argument("atten", NumberValue(ndecimals=2, unit="dB", step=0.1, min=0))                                        #instructs dashboard to take input and set it as an attribute called atten
-        self.setattr_argument("urukul channel", EnumerationValue(urukuls, default = urukuls[0]))
+        self.setattr_argument("urukul_channel", EnumerationValue(urukuls, default = urukuls[0]))
+        
+        self.my_urukul : AD9910 = self.get_device(self.urukul_channel)
+
         
     
     @kernel 
     def run(self):  
         self.core.reset()                                      
-        self.urukul0_ch1.cpld.init()                            
-        self.urukul0_ch1.init()                                                                        
+        self.my_urukul.cpld.init()                            
+        self.my_urukul.init()                                                                        
         
         
-        self.urukul0_ch1.set_att(self.atten)                    #writes attenuation to urukul channel
-        self.urukul0_ch1.sw.on()                                #switches urukul channel on
+        self.my_urukul.set_att(self.atten)                    #writes attenuation to urukul channel
+        self.my_urukul.sw.on()                                #switches urukul channel on
            
             
-        self.urukul0_ch1.set(self.freq, amplitude = self.amp)   #writes frequency and amplitude attributes to urukul channel thus outputting function                             #delay determined by user input
-        self.urukul0_ch1.sw.off()                               #switches urukul channel off
+        self.my_urukul.set(self.freq, amplitude = self.amp)   #writes frequency and amplitude attributes to urukul channel thus outputting function                             #delay determined by user input
+        self.my_urukul.sw.off()                               #switches urukul channel off
