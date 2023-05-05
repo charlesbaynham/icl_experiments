3ba713485c310d4f2e9f46b10d9f5ffbaf0ff3d6
Charles Baynham <c.baynham@imperial.ac.uk>
2023-05-05 10:55:57 +0100

    Generalise the beam turner-onner

---
+++ repository/lib/fragments/beam_setter.py
@@ -0,0 +1,62 @@
+import logging
+from typing import List
+
+from artiq.coredevice.core import Core
+from artiq.coredevice.ttl import TTLOut
+from artiq.experiment import kernel
+from ndscan.experiment import Fragment
+from pyaion.fragments.suservo import LibSetSUServoStatic
+
+import repository.lib.constants as constants
+
+
+logger = logging.getLogger(__name__)
+
+
+
+
+class SetBeamsToDefaults(Fragment):
+    """
+    Turn on a list of suservoed beams, possibly with shutters, to their default settings
+    """
+
+    def build_fragment(self, beams_to_enable : List[str]):
+        self.beams_to_enable = beams_to_enable
+
+        self.setattr_device("core")
+        self.core: Core
+
+        self.suservo_setters: List[LibSetSUServoStatic] = []
+        self.beam_info = [constants.AOM_BEAMS[beam] for beam in self.beams_to_enable]
+        self.ttls: List[TTLOut] = []
+
+        for beam, beam_info in zip(self.beams_to_enable, self.beam_info):
+            self.setattr_fragment(beam, LibSetSUServoStatic, beam_info.suservo_device)
+            self.suservo_setters.append(getattr(self, beam))
+            if beam_info.shutter_device:
+                self.ttls.append(self.get_device(beam_info.shutter_device))
+
+        self.setattr_device("core")
+
+
+
+    @kernel
+    def turn_on_all(self):
+        """
+        Turn on the pre-configured beams to their default values
+
+        This method does not advance the timeline and does not respect
+        shutter delays - it just turns everything on immediately.
+        """
+        for i in range(len(self.beams_to_enable)):
+            setter = self.suservo_setters[i]
+            beam_info = self.beam_info[i]
+
+            setter.set_suservo(
+                float(beam_info.frequency), 1.0, float(beam_info.attenuation)
+            )
+
+        for ttl in self.ttls:
+            ttl.on()
+
+
--- repository/single_purpose/blue_system_on.py
+++ repository/single_purpose/blue_system_on.py
@@ -1,65 +1,41 @@
 import logging
 import re
-from typing import List
 
 from artiq.coredevice.core import Core
-from artiq.coredevice.ttl import TTLOut
 from artiq.experiment import kernel
 from ndscan.experiment import ExpFragment
 from ndscan.experiment.entry_point import make_fragment_scan_exp
-from pyaion.fragments.suservo import LibSetSUServoStatic
+
 
 import repository.lib.constants as constants
-
+from repository.lib.fragments.beam_setter import SetBeamsToDefaults
 
 logger = logging.getLogger(__name__)
 
 
-SUSERVOED_BEAMS = [k for k in constants.AOM_BEAMS.keys() if re.match(r"^blue_", k)]
+BLUE_BEAMS = [k for k in constants.AOM_BEAMS.keys() if re.match(r"^blue_", k)]
 
 
 class BlueSystemOn(ExpFragment):
     """
-    Turn the blue system AOMs and shutters on to default settings
+    Turn the blue system AOMs and shutters on to their default settings
     """
 
     def build_fragment(self):
         self.setattr_device("core")
         self.core: Core
 
-        self.suservo_setters: List[LibSetSUServoStatic] = []
-        self.beam_info = [constants.AOM_BEAMS[beam] for beam in SUSERVOED_BEAMS]
-        self.ttls: List[TTLOut] = []
-
-        for beam, beam_info in zip(SUSERVOED_BEAMS, self.beam_info):
-            self.setattr_fragment(beam, LibSetSUServoStatic, beam_info.suservo_device)
-            self.suservo_setters.append(getattr(self, beam))
-            if beam_info.shutter_device:
-                self.ttls.append(self.get_device(beam_info.shutter_device))
-
-        self.setattr_device("core")
+        self.setattr_fragment("SetBeamsToDefaults", SetBeamsToDefaults, beams_to_enable=BLUE_BEAMS)
+        self.SetBeamsToDefaults: SetBeamsToDefaults
+        
 
     @kernel
     def run_once(self):
         logger.info("Enabling AOMS:")
-        logger.info(SUSERVOED_BEAMS)
+        logger.info(BLUE_BEAMS)
 
         self.core.break_realtime()
-        self.turn_on()
-
-    @kernel
-    def turn_on(self):
-        # Set the outputs
-        for i in range(len(SUSERVOED_BEAMS)):
-            setter = self.suservo_setters[i]
-            beam_info = self.beam_info[i]
-
-            setter.set_suservo(
-                float(beam_info.frequency), 1.0, float(beam_info.attenuation)
-            )
-
-        for ttl in self.ttls:
-            ttl.on()
+        self.SetBeamsToDefaults.turn_on_all()
 
 
 BlueSystemOnExp = make_fragment_scan_exp(BlueSystemOn)
