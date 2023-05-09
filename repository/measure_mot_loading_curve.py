a82f87a541b891a48968482353bdef1d9044e05e
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-05-09 14:42:33 +0000

    Update loading curve

--- repository/measure_mot_loading_curve.py
+++ repository/measure_mot_loading_curve.py
@@ -73,23 +73,14 @@ class MeasureMOTLoadingCurveFrag(ExpFragment):
         self.repumper_707_shutter.on()
         self.repumper_679_shutter.on()
         delay(25 * ms)
-        self.mot_controller.turn_off_push_beam()
-        delay(20 * ns)
-        self.mot_controller.turn_off_2d_mot_beams()
-        delay(25 * ms)
-        self.mot_controller.turn_off_3d_mot_beams()
-        delay(20 * ns)
+        self.mot_controller.turn_off_3d_and_2d_beams()
 
         delay(
             100 * ms
         )  # Wait to allow atoms to disperse if there were any hanging around
 
         # Load MOT and start measuring signal immediately
-        self.mot_controller.turn_on_3d_mot_beams()
-        delay(20 * ns)
-        self.mot_controller.turn_on_push_beam()
-        delay(25 * ms)
-        self.mot_controller.turn_on_2d_mot_beams()
+        self.mot_controller.turn_on_3d_and_2d_beams()
 
         num_points = int(
             self.mot_loading_time.get() / self.delay_between_trace_points.get()
