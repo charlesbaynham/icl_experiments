5d6495b7841946abfa0236175cbf6ab95a591cc6
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-05-09 13:35:47 +0000

    Break realtime

--- repository/measure_mot_loading_curve.py
+++ repository/measure_mot_loading_curve.py
@@ -65,6 +65,7 @@ class MeasureMOTLoadingCurveFrag(ExpFragment):
 
     @kernel
     def run_once(self):
+        self.core.break_realtime()
         # Turn on the 2D/3D beams & AOMs,
         # but block the important ones, leaving the repumpers on
         self.mot_controller.enable_mot_beams()
