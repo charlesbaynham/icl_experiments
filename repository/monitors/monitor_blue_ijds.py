dac9f0c5ad42296f7b9ba9c160414ab45fff5537
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-05-13 08:01:43 +0000

    Add "settling" status for IJDs

--- repository/monitors/monitor_blue_ijds.py
+++ repository/monitors/monitor_blue_ijds.py
@@ -4,6 +4,8 @@ from koheron_ctl200_laser_driver import CTL200
 from qbutler.calibration import Calibration
 from qbutler.calibration import CalibrationResult
 
+AWAY_FROM_TEMPERATURE_SETPOINT_THRESHOLD = 0.05  # k
+
 logger = logging.getLogger(__name__)
 
 
@@ -22,7 +24,17 @@ class MonitorIJD1(Calibration):
         out["temperature_setpoint"] = self.controller.get_temperature_setpoint()
         out["current"] = 1e-3 * self.controller.get_current_mA()
         out["voltage"] = self.controller.get_voltage()
-        out["status"] = "ON" if self.controller.status() else "OFF"
+
+        laser_is_on = self.controller.status()
+        if not laser_is_on:
+            out["status"] = "OFF"
+        elif (
+            abs(out["temperature_actual"] - out["temperature_setpoint"])
+            > AWAY_FROM_TEMPERATURE_SETPOINT_THRESHOLD
+        ):
+            out["status"] = "SETTLING"
+        else:
+            out["status"] = "ON"
 
         result = CalibrationResult.OK if out["status"] else CalibrationResult.BAD_DATA
 
