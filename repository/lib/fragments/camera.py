ec6c232909204b1c3fad08aa8d58658bc70ad3bb
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-05-14 14:09:58 +0000

    Try to fix the aravis import in my code

--- repository/lib/fragments/camera.py
+++ repository/lib/fragments/camera.py
@@ -3,8 +3,10 @@ import time
 from typing import List
 from typing import Tuple
 
+from numpy.typing import ArrayLike
+
 import numpy as np
-from aravis import Camera
+
 from artiq.coredevice.core import Core
 from artiq.experiment import host_only
 from artiq.experiment import rpc
@@ -20,6 +22,12 @@ logger = logging.getLogger(__name__)
 
 class Chamber2Camera:
     def __init__(self, num_images, exposure_us, delay_ms):
+        # This import happens here because, for some reason, importing the
+        # gi.repository Aravis (which happens in python-aravis) breaks if you do
+        # it from multiple processes at the same time, which ARTIQ will trigger
+        # when scanning for experiments
+        from aravis import Camera
+
         if delay_ms < 20:
             raise ValueError(
                 "The camera cannot support a frame rate faster than 20ms per frame"
@@ -35,16 +43,18 @@ class Chamber2Camera:
         self._buffer_depth = num_images + 10
         self._per_image_time = max([1e-6 * exposure_us, 1e-3 * delay_ms])
 
-    def capture_frames(self) -> List[Tuple[int, np.array]]:
+    def capture_frames(self) -> List[Tuple[int, ArrayLike]]:
         self.cam.start_acquisition_continuous(nb_buffers=self._buffer_depth)
 
         out = []
         while True:
             time.sleep(self._per_image_time)
 
-            ts, frame = self.cam.try_pop_frame(True)
-            if ts is not None:
-                out.append((ts, frame))
+            new_frame =self.cam.try_pop_frame(True)
+            if new_frame is not None:
+                ts, frame = new_frame
+                if ts is not None:
+                    out.append((ts, frame))
 
             if len(out) == self._num_images:
                 break
