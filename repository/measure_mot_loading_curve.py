bdbd7f62b250fab7a292569281850c1249855fb0
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-05-19 21:00:07 +0000

    Remove debug print

--- repository/measure_mot_loading_curve.py
+++ repository/measure_mot_loading_curve.py
@@ -1,4 +1,3 @@
-import numpy as np
 from artiq.coredevice.core import Core
 from artiq.coredevice.ttl import TTLOut
 from artiq.experiment import delay
@@ -100,8 +99,6 @@ class MeasureMOTFrag(ExpFragment):
             ),
             data=trace_data,
         )
-
-        print(np.array(trace_data))
 
         self.photodiode_voltage.push(trace_data)
         mean_voltage = 0.0
