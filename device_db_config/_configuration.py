bdc402ef25e4f5730fa26d17137fe321a944a8cf
Charles Baynham <c.baynham@imperial.ac.uk>
2023-05-19 15:36:21 +0100

    Docs

--- device_db_config/_configuration.py
+++ device_db_config/_configuration.py
@@ -8,7 +8,12 @@ NOT be used to represent information like "which power we usually run this AOM
 at" or "what current this coil typically needs" - that information belongs in
 `constants.py`.
 
-To retrieve information from this file in an experiment, use `
+To retrieve information from this file in an experiment, use
+:meth:`.get_configuration_from_db` like so::
+
+    from device_db_config import get_configuration_from_db
+
+    data = get_configuration_from_db("my_data_item")
 
 """
 
