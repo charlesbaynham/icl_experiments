728dbe50fb2ea673da5b3e14ea4781b1eca62c98
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-05-20 10:58:54 +0000

    Test camera

---
+++ repository/tests/test_camera.py
@@ -0,0 +1,23 @@
+import logging
+
+from artiq.experiment import EnvExperiment
+from ndscan.experiment import ExpFragment
+from ndscan.experiment.entry_point import make_fragment_scan_exp
+
+from repository.lib.fragments.camera import Chamber2Camera
+
+
+class TestFLIRCamera(EnvExperiment):
+    def run(self):
+        # This import happens here because, for some reason, importing the
+        # gi.repository Aravis (which happens in python-aravis) breaks if you do
+        # it from multiple processes at the same time, which ARTIQ will trigger
+        # when scanning for experiments
+        from aravis import Camera
+
+        cam = Camera("FLIR-Blackfly S BFS-PGE-50S5M-22018873", loglevel=logging.INFO)
+        cam.start_acquisition_trigger()
+
+        frame = cam.pop_frame()
+
+        print(frame)
