dde1d82b16e0bd785ef4d2fbdb342134669af4dd
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-06-08 17:21:29 +0000

    debug

--- repository/tests/ad9910_ramper.py
+++ repository/tests/ad9910_ramper.py
@@ -211,7 +211,7 @@ class AD9910Ramper(EnvExperiment):
 
         self.set_ramp_limits(freq_low, freq_high)
 
-        max_step_mu = 0x0000FFFF
+        max_step_mu = 0x00FFFFFF
         min_wait_mu = 1
 
         # max_step_mu = 0x8FFFFFFF
