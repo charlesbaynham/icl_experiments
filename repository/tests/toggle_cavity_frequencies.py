7fb2a7dc099683a80ba5f626f9062be55fcd9f6c
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-06-09 18:03:17 +0000

    Add cavity tester

---
+++ repository/tests/toggle_cavity_frequencies.py
@@ -0,0 +1,31 @@
+from artiq.coredevice.core import Core
+from artiq.experiment import delay
+from artiq.experiment import kernel
+from artiq.experiment import now_mu
+from ndscan.experiment import ExpFragment
+from ndscan.experiment.entry_point import make_fragment_scan_exp
+
+from repository.lib.fragments.cavity_control import LaserStabilisationSystem
+
+
+class ToggleCavityFreqs(ExpFragment):
+    def build_fragment(self, *args, **kwargs) -> None:
+        self.setattr_device("core")
+        self.core: Core
+        self.setattr_fragment("laser_stab", LaserStabilisationSystem)
+        self.laser_stab: LaserStabilisationSystem
+
+    @kernel
+    def run_once(self) -> None:
+        self.core.break_realtime()
+
+        for _ in range(10):
+            self.laser_stab.offset_689(0.0)
+            delay(5.0)
+            self.laser_stab.offset_689(1e3)
+            delay(5.0)
+
+        self.core.wait_until_mu(now_mu())
+
+
+ToggleCavityFreqs = make_fragment_scan_exp(ToggleCavityFreqs)
