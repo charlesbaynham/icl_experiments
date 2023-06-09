80772ab290ddd817ccfb36c30cd37970b7103be3
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-06-09 14:55:36 +0000

    Add shutters for red table

--- device_db_config/_aliases.py
+++ device_db_config/_aliases.py
@@ -17,6 +17,7 @@ information like "optimal drive power for this AOM": that information belongs in
 
 
 aliases = {
+    # %% SUServos
     # Blue
     "suservo_aom_doublepass_461_injection": "suservo1_ch3",
     "suservo_aom_singlepass_461_spectroscopy": "suservo1_ch7",
@@ -37,12 +38,21 @@ aliases = {
     "suservo_aom_singlepass_707": "suservo2_ch0",
     "suservo_aom_singlepass_679": "suservo2_ch1",
     "suservo_aom_singlepass_1379": "suservo2_ch2",
+    # %% TTLs
     "TTL_shutter_461_pushbeam": "ttl16",
     "TTL_shutter_461_2dmot_is_it_a": "ttl17",
     "TTL_shutter_461_2dmot_is_it_b": "ttl18",
     "TTL_shutter_461_3dmot": "ttl19",
     "TTL_shutter_679_temporary_shutter": "ttl22",
     "TTL_shutter_707_temporary_shutter": "ttl23",
+    "ttl_shutter_repump_707": "ttl24",
+    "ttl_shutter_repump_679": "ttl25",
+    "ttl_shutter_red_sigmaplus": "ttl26",
+    "ttl_shutter_red_sigmaminus": "ttl27",
+    "ttl_shutter_red_axial_mox": "ttl28",
+    "ttl_shutter_red_up": "ttl29",
+    "ttl_shutter_red_diagonal_mot": "ttl30",
+    "ttl_shutter_red_axial_spin_pol": "ttl31",
     "zotino_plant_room": "zotino0",
     "ttl_transfer_cavity_trigger": "ttl1",
     "dds_transfer_cavity_aom": "urukul2_ch0",
