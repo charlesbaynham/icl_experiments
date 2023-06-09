8b3ad281efd281cdd530edfeea4d1f7b4a11909b
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-06-09 18:06:23 +0000

    Allow configurable step size

--- repository/tests/toggle_cavity_frequencies.py
+++ repository/tests/toggle_cavity_frequencies.py
@@ -3,6 +3,7 @@ from artiq.experiment import delay
 from artiq.experiment import kernel
 from artiq.experiment import now_mu
 from ndscan.experiment import ExpFragment
+from ndscan.experiment import FloatParam
 from ndscan.experiment.entry_point import make_fragment_scan_exp
 
 from repository.lib.fragments.cavity_control import LaserStabilisationSystem
@@ -15,14 +16,17 @@ class ToggleCavityFreqs(ExpFragment):
         self.setattr_fragment("laser_stab", LaserStabilisationSystem)
         self.laser_stab: LaserStabilisationSystem
 
+        self.setattr_param("stepsize", FloatParam, "Step size", default=1e3, unit="Hz")
+
     @kernel
     def run_once(self) -> None:
-        self.core.break_realtime()
+        self.core.wait_until_mu(now_mu())
+        self.core.reset()
 
         for _ in range(10):
             self.laser_stab.offset_689(0.0)
             delay(5.0)
-            self.laser_stab.offset_689(1e3)
+            self.laser_stab.offset_689(self.stepsize.get())
             delay(5.0)
 
         self.core.wait_until_mu(now_mu())
