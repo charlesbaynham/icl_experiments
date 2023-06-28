17ff3dd01817e7d2b8045f57ab2fa97d787d4fc6
Charles Baynham <c.baynham@imperial.ac.uk>
2023-06-28 13:56:59 +0100

    Allow including 461 in lock

--- repository/single_purpose/set_wavemeter_lock.py
+++ repository/single_purpose/set_wavemeter_lock.py
@@ -13,10 +13,12 @@ class EnableWavemeterLock(EnvExperiment):
         self.setattr_device("wand_server")
         self.wand_server: WandServer
 
+        self.setattr_argument("include_461", BooleanValue(default=False))
+
     def run(self):
 
         for laser in self.wand_server.get_laser_db().keys():
-            if self.enable and laser != "461":
+            if self.enable and (laser != "461" or self.include_461):
                 self.wand_server.lock(laser, set_point=0.0, timeout=None)
             else:
                 self.wand_server.unlock(laser, name="")
