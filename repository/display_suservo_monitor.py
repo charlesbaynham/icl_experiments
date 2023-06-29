fec4905c84c1d90465d630fd99be371852c7b425
Charles Baynham <c.baynham@imperial.ac.uk>
2023-06-29 11:14:27 +0100

    Tidy

--- repository/display_suservo_monitor.py
+++ repository/display_suservo_monitor.py
@@ -1,32 +1,17 @@
 import logging
 import re
-import time
 
-import numpy as np
 from artiq.coredevice.core import Core
-from artiq.coredevice.sampler import Sampler
-from artiq.coredevice.suservo import Channel
-from artiq.coredevice.suservo import Channel as SUServoChannel
 from artiq.coredevice.suservo import SUServo
-from artiq.coredevice.urukul import CPLD
-from artiq.experiment import BooleanValue
 from artiq.experiment import delay
 from artiq.experiment import EnumerationValue
 from artiq.experiment import kernel
 from artiq.experiment import ms
-from artiq.experiment import NumberValue
-from artiq.experiment import rpc
-from artiq.experiment import RTIOUnderflow
-from artiq.experiment import TFloat
-from koheron_ctl200_laser_driver import CTL200
 from ndscan.experiment import ExpFragment
 from ndscan.experiment import FloatParam
 from ndscan.experiment import ResultChannel
 from ndscan.experiment.entry_point import make_fragment_scan_exp
 from ndscan.experiment.parameters import FloatParamHandle
-from ndscan.experiment.parameters import IntParam
-from ndscan.experiment.parameters import IntParamHandle
-from pyaion.lib.utils import get_local_devices
 
 from repository.lib.fragments.read_adc import ReadSUServoADC
 
