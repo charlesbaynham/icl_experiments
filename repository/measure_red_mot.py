9396058336143b1d10798783e730d6b228f45c89
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-06-29 14:47:02 +0000

    Implement red mot photos

--- repository/measure_red_mot.py
+++ repository/measure_red_mot.py
@@ -7,9 +7,11 @@ from artiq.experiment import now_mu
 from ndscan.experiment import ExpFragment
 from ndscan.experiment.entry_point import make_fragment_scan_exp
 from ndscan.experiment.parameters import FloatParam
+from ndscan.experiment.parameters import FloatParamHandle
 
 from repository.lib.fragments.blue_3d_mot import Blue3DMOTFrag
 from repository.lib.fragments.dual_camera_measurer import BGCorrectedMeasurement
+from repository.lib.fragments.magnetic_fields import SetMagneticFields
 from repository.lib.fragments.red_3d_mot import Red3DMOTFrag
 
 logger = logging.getLogger(__name__)
@@ -26,6 +28,12 @@ class MeasureRedMOTFrag(ExpFragment):
         self.setattr_fragment("red_mot_controller", Red3DMOTFrag)
         self.red_mot_controller: Red3DMOTFrag
 
+        self.setattr_fragment(
+            "chamber_2_field_setter",
+            SetMagneticFields,
+        )
+        self.chamber_2_field_setter: SetMagneticFields
+
         self.setattr_fragment("camera_bg_corrected", BGCorrectedMeasurement)
         self.camera_bg_corrected: BGCorrectedMeasurement
 
@@ -36,6 +44,7 @@ class MeasureRedMOTFrag(ExpFragment):
             default=200e-3,
             unit="ms",
         )
+        self.red_loading_time: FloatParamHandle
 
     @kernel
     def run_once(self):
@@ -51,15 +60,26 @@ class MeasureRedMOTFrag(ExpFragment):
         delay(10e-9)
         self.blue_mot_controller.turn_off_3d_and_2d_beams()
 
-        # Wait then take a photo
+        # Wait with atoms hopefully in the red mot
+        delay(self.red_loading_time.get())
+
+        # Flash on the blue light and take a photo
+        self.blue_mot_controller.turn_on_3d_beams()
+
+        self.core.wait_until_mu(now_mu())
+        self.camera_bg_corrected.trigger_signal()
+
+        # Discard the MOT and take a background photo, allowing enough time for
+        # the gradient currents to dissipate
+        self.chamber_2_field_setter.set_mot_gradient(0.0)
+        delay(20e-3)
+        self.blue_mot_controller.enable_mot_fields()
+
+        # Take a background photo
         self.core.wait_until_mu(now_mu())
         self.camera_bg_corrected.trigger_background()
 
-        delay(500e-3)
-        self.core.wait_until_mu(now_mu())
-
-        self.camera_bg_corrected.trigger_signal()
-
+        # Save the photos
         self.camera_bg_corrected.save_data()
 
 
