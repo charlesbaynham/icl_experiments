fbb4312dc15b820458aab98212b985c3d51ddcc0
Charles Baynham <c.baynham@imperial.ac.uk>
2023-06-29 11:55:38 +0100

    Tidy

--- repository/measure_red_mot.py
+++ repository/measure_red_mot.py
@@ -1,26 +1,12 @@
 import logging
 
-import numpy as np
 from artiq.coredevice.core import Core
-from artiq.experiment import delay
 from artiq.experiment import kernel
-from artiq.experiment import ms
-from artiq.experiment import now_mu
-from artiq.experiment import rpc
 from ndscan.experiment import ExpFragment
-from ndscan.experiment import ResultChannel
 from ndscan.experiment.entry_point import make_fragment_scan_exp
 from ndscan.experiment.parameters import FloatParam
-from ndscan.experiment.parameters import FloatParamHandle
-from ndscan.experiment.result_channels import FloatChannel
-from ndscan.experiment.result_channels import IntChannel
-from ndscan.experiment.result_channels import OpaqueChannel
 
 from repository.lib.fragments.blue_3d_mot import Blue3DMOTFrag
-from repository.lib.fragments.chamber_photodiode import MOTPhotodiodeMeasurement
-from repository.lib.fragments.dual_camera_measurer import BGCorrectedMeasurement
-from repository.lib.fragments.flir_camera import Chamber2HorizontalCamera
-from repository.lib.fragments.flir_camera import Chamber2VerticalCamera
 from repository.lib.fragments.red_3d_mot import Red3DMOTFrag
 
 logger = logging.getLogger(__name__)
