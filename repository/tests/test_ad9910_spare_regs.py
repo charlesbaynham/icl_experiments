b892051ade2c1169415cc253b9bc71f53d1a2fd1
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-07-03 23:17:57 +0000

    Make new

--- repository/tests/test_ad9910_spare_regs.py
+++ repository/tests/test_ad9910_spare_regs.py
@@ -10,6 +10,7 @@ from artiq.coredevice.ad9910 import _AD9910_REG_AUX_DAC
 from artiq.coredevice.ad9910 import _AD9910_REG_CFR2
 from artiq.coredevice.ad9910 import _AD9910_REG_PROFILE7
 from artiq.coredevice.ad9910 import AD9910
+from artiq.coredevice.core import Core
 from artiq.experiment import EnvExperiment
 from artiq.experiment import kernel
 from artiq.experiment import NumberValue
@@ -21,6 +22,7 @@ REG_ADDR = 0x05
 class WriteToAD9910SpareRegistry(EnvExperiment):
     def build(self):
         self.setattr_device("core")
+        self.core: Core
 
         self.urukul: AD9910 = self.get_device(
             "urukul9910_aom_doublepass_689_red_injection"
@@ -47,10 +49,14 @@ class WriteToAD9910SpareRegistry(EnvExperiment):
 
         logger.info("address_step_rate = 0x%X", address_step_rate)
 
-        # logger.info("Writing val = 0x%X", aux_val)
+        address_step_rate_new = 0xABCD
+
+        profile_7_new = (profile_7 & (~mask)) | (address_step_rate_new << 40)
+
+        logger.info("Writing asr = 0x%X", profile_7_new)
 
         # self.core.break_realtime()
-        # self.urukul.write32(_AD9910_REG_PROFILE7, aux_val)
+        # self.urukul.write64(_AD9910_REG_PROFILE7, profile_7_new)
 
         # self.core.break_realtime()
         # aux_val = self.urukul.read32(_AD9910_REG_PROFILE7)
