f818088d06d71f06c0bf4ab30b3d9961af1c1a5c
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-07-04 10:38:18 +0000

    Even longer wait

--- repository/tests/test_ad9910_spare_regs.py
+++ repository/tests/test_ad9910_spare_regs.py
@@ -40,7 +40,7 @@ class WriteToAD9910SpareRegistry(EnvExperiment):
         dds.cfg_write(dds.cfg_reg | (1 << CFG_RST))
         delay(100e-3)
         dds.cfg_write(dds.cfg_reg & ~(1 << CFG_RST))
-        delay(100e-3)
+        delay(2000e-3)
 
     @kernel
     def read_freq(self):
