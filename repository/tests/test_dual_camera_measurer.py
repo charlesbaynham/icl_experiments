780f3a5dac88f3846fbeffb0fcb4588a0d1850b9
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-07-08 10:52:40 +0000

    Test dual camera interface

---
+++ repository/tests/test_dual_camera_measurer.py
@@ -0,0 +1,63 @@
+import logging
+
+from artiq.coredevice.core import Core
+from artiq.coredevice.ttl import TTLOut
+from artiq.experiment import at_mu
+from artiq.experiment import delay
+from artiq.experiment import kernel
+from artiq.experiment import now_mu
+from artiq.experiment import parallel
+from artiq.experiment import sequential
+from ndscan.experiment import ExpFragment
+from ndscan.experiment.entry_point import make_fragment_scan_exp
+from ndscan.experiment.parameters import FloatParam
+from ndscan.experiment.parameters import FloatParamHandle
+
+from repository.lib.fragments.blue_3d_mot import Blue3DMOTFrag
+from repository.lib.fragments.dual_camera_measurer import BGCorrectedMeasurement
+from repository.lib.fragments.magnetic_fields import SetMagneticFields
+from repository.lib.fragments.red_3d_mot import Red3DMOTFrag
+
+logger = logging.getLogger(__name__)
+
+
+class TestDualCameraMeasurerHardwareTrigger(ExpFragment):
+    def build_fragment(self):
+        self.setattr_device("core")
+        self.core: Core
+
+        self.setattr_fragment(
+            "camera_bg_corrected", BGCorrectedMeasurement, hardware_trigger=True
+        )
+        self.camera_bg_corrected: BGCorrectedMeasurement
+
+        self.camera_trigger_h: TTLOut = self.get_device("ttl_camera_trigger_horizontal")
+        self.camera_trigger_v: TTLOut = self.get_device("ttl_camera_trigger_vertical")
+
+    @kernel
+    def run_once(self):
+        self.core.break_realtime()
+
+        with parallel:
+            self.camera_trigger_h.pulse(1e-3)
+            self.camera_trigger_v.pulse(1e-3)
+
+        delay(20e-3)
+
+        with parallel:
+            self.camera_trigger_h.pulse(1e-3)
+            self.camera_trigger_v.pulse(1e-3)
+
+        # Wait for all RTIO events to complete
+        self.core.break_realtime()
+        self.core.wait_until_mu(now_mu())
+
+        # End of RTIO sequencing. Now we are in real-time.
+
+        # Save the photos
+        self.camera_bg_corrected.save_data()
+
+
+TestDualCameraMeasurerHardwareTrigger = make_fragment_scan_exp(
+    TestDualCameraMeasurerHardwareTrigger
+)
