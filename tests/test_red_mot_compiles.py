ebba46fd87ba899cacb279a9922331f72ac1cd3e
Charles Baynham <c.baynham@imperial.ac.uk>
2023-07-12 20:43:41 +0100

    Fix tests

--- tests/test_red_mot_compiles.py
+++ tests/test_red_mot_compiles.py
@@ -1,10 +1,10 @@
 import pytest
 
-from repository.measure_red_mot import MeasureRedMOTFrag
-
 
 @pytest.mark.xfail
 def test_red_mot(fragment_factory):
+    from repository.measure_red_mot import MeasureRedMOTFrag
+
     def precompile(self):
         precompiled_setup = self.core.precompile(self.device_setup)
         precompiled_run = self.core.precompile(self.run_once)
