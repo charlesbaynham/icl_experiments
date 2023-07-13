ca2111e81220cb7e122efde36cc3bf320ae9efac
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-07-13 14:11:55 +0000

    With a reset?

--- repository/tests/test_3dmot_fragment.py
+++ repository/tests/test_3dmot_fragment.py
@@ -18,7 +18,7 @@ class TestLoadBlueMOT(ExpFragment):
 
     @kernel
     def run_once(self):
-        self.core.break_realtime()
+        self.core.reset()
         self.blue_mot_controller.load_mot(clearout=True)
 
 
