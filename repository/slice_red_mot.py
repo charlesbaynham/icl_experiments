e2200c83c48c529dd830204f1eaa25f5848bc13c
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-07-13 12:02:50 +0000

    Debug

--- repository/slice_red_mot.py
+++ repository/slice_red_mot.py
@@ -82,7 +82,7 @@ class SliceRedMOTFrag(ExpFragment):
         # self.blue_mot_controller.init()
         self.red_mot_controller.init()
 
-        # self.red_mot_controller.turn_off_mot_beams()
+        self.red_mot_controller.turn_off_mot_beams()
 
         # Clear the camera buffer in case we quit a previous sequence midway
         # self.camera_bg_corrected.clear()
