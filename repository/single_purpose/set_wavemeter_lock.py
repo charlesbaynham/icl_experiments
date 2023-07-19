ae36f90bf825480378267523ac1a5f78ab2c92e0
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-07-19 11:41:42 +0000

    Allow WAND setter to set offsets

--- repository/single_purpose/set_wavemeter_lock.py
+++ repository/single_purpose/set_wavemeter_lock.py
@@ -1,8 +1,17 @@
 from artiq.experiment import BooleanValue
 from artiq.experiment import EnvExperiment
+from artiq.experiment import NumberValue
 from wand.server import ControlInterface as WandServer
 
 LASERS = ["461", "689", "707", "679"]
+
+
+def _laser_name_to_enabled_param(laser):
+    return f"{laser}_enabled"
+
+
+def _laser_name_to_offset_param(laser):
+    return f"{laser}_offset"
 
 
 class EnableWavemeterLock(EnvExperiment):
@@ -15,14 +24,25 @@ class EnableWavemeterLock(EnvExperiment):
         self.wand_server: WandServer
 
         for laser in LASERS:
-            self.setattr_argument(laser, BooleanValue(default=True))
+            self.setattr_argument(
+                _laser_name_to_enabled_param(laser),
+                BooleanValue(default=True),
+                group=laser,
+            )
+            self.setattr_argument(
+                _laser_name_to_offset_param(laser),
+                NumberValue(
+                    default=0.0, unit="MHz", ndecimals=1, type="float", step=1e5
+                ),
+                group=laser,
+            )
 
     def run(self):
-
         for laser in LASERS:
-            laser_enabled = getattr(self, laser)
+            laser_enabled = getattr(self, _laser_name_to_enabled_param(laser))
+            laser_offset = getattr(self, _laser_name_to_offset_param(laser))
 
             if laser_enabled:
-                self.wand_server.lock(laser, set_point=0.0, timeout=None)
+                self.wand_server.lock(laser, set_point=laser_offset, timeout=None)
             else:
                 self.wand_server.unlock(laser, name="")
