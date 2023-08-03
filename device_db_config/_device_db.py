a010f78af14df73619b50e8a9126ffc1f826d75c
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-08-03 18:29:49 +0000

    Reinstate core log

--- device_db_config/_device_db.py
+++ device_db_config/_device_db.py
@@ -8,12 +8,12 @@ device_db = {
         "class": "Core",
         "arguments": {"host": core_addr, "ref_period": 1e-09, "target": "rv32g"},
     },
-    # "core_log": {
-    #     "type": "controller",
-    #     "host": "::1",
-    #     "port": 1068,
-    #     "command": "aqctl_corelog -p {port} --bind {bind} " + core_addr,
-    # },
+    "core_log": {
+        "type": "controller",
+        "host": "::1",
+        "port": 1068,
+        "command": "aqctl_corelog -p {port} --bind {bind} " + core_addr,
+    },
     "core_moninj": {
         "type": "controller",
         "host": "10.137.1.252",
