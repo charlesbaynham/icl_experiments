c10a3b521b6112487f1130181e5086d9977b78f1
Richard Hobson <r.hobson@imperial.ac.uk>
2023-08-08 14:35:44 +0100

    Added comment

--- repository/measure_red_mot.py
+++ repository/measure_red_mot.py
@@ -293,6 +293,9 @@ class _RampingPhase(Fragment):
 
     @kernel
     def device_setup(self):
+        """
+        Records the ramps to DMA, creating self.dma_handle for later playback. Write events are staggered by 4 ns (self.core.ref_multiplier) to use only one lane
+        """
         self.device_setup_subfragments()
 
         # Compute grid for writes
