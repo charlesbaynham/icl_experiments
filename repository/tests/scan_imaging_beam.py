0ac3914600cfcf5c595aa9a0b6cbc4f574298696
Charles Baynham <c.baynham@imperial.ac.uk>
2023-10-24 19:18:39 +0100

    Complete test imaging frag

--- repository/tests/scan_imaging_beam.py
+++ repository/tests/scan_imaging_beam.py
@@ -1,21 +1,36 @@
+from repository.lib.fragments.dual_camera_measurer import DualCameraMeasurement
 from artiq.coredevice.suservo import Channel as SUServoChannel
-from artiq.experiment import delay
-from artiq.experiment import kernel
+from artiq.experiment import delay,now_mu
+from artiq.experiment import kernel,parallel,sequential
 from ndscan.experiment import ExpFragment
+from ndscan.experiment.parameters import FloatParam, FloatParamHandle
 
 from repository.lib.fragments.blue_3d_mot import Blue3DMOTFrag
 
 
 class ImageBlueMOT(ExpFragment):
-    def build_fragment(self, *args, **kwargs) -> None:
+    def build_fragment(self) -> None:
         self.setattr_device("core")
 
         self.setattr_fragment("blue_mot", Blue3DMOTFrag)
         self.blue_mot: Blue3DMOTFrag
 
+        self.setattr_fragment(
+            "dual_cameras", DualCameraMeasurement, hardware_trigger=True
+        )
+        self.dual_cameras: DualCameraMeasurement
+
         self.suservo: SUServoChannel = self.get_device(
             "suservo_aom_singlepass_461_imaging_switch"
         )
+
+        self.setattr_param("expansion_time", FloatParam, "Expansion time of blue MOT", default=10e-3, unit="ms")
+        self.expansion_time : FloatParamHandle
+
+        self.setattr_param("image_time", FloatParam, "Expansion time of blue MOT", default=200e-6, unit="us"))
+        self.image_time : FloatParamHandle    
+
+
 
     @kernel
     def run_once(self) -> None:
@@ -24,4 +39,18 @@ class ImageBlueMOT(ExpFragment):
 
         delay(1.0)
 
-        self.suservo.set()
+        self.blue_mot.turn_off_3d_and_2d_beams()
+
+        delay(self.expansion_time.get())
+
+        with parallel:
+            with sequential:
+                self.suservo.set(en_out=True)
+                delay(self.image_time.get())
+                self.suservo.set(en_out=False)
+            with sequential:
+                self.dual_cameras.trigger()
+            
+        self.core.wait_until_mu(now_mu())
+
+        self.dual_cameras.save_data()
