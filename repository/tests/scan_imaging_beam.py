3ebdacec629f3605a631f703744474d8fd35ab31
Charles Baynham <c.baynham@imperial.ac.uk>
2023-10-24 19:24:04 +0100

    Use setter

--- repository/tests/scan_imaging_beam.py
+++ repository/tests/scan_imaging_beam.py
@@ -1,10 +1,9 @@
 from repository.lib.fragments.dual_camera_measurer import DualCameraMeasurement
-from artiq.coredevice.suservo import Channel as SUServoChannel
-from artiq.experiment import delay,now_mu
-from artiq.experiment import kernel,parallel,sequential
+from artiq.experiment import delay, now_mu
+from artiq.experiment import kernel, parallel, sequential
 from ndscan.experiment import ExpFragment
 from ndscan.experiment.parameters import FloatParam, FloatParamHandle
-
+from repository.lib.fragments.suservo import LibSetSUServoStatic
 from repository.lib.fragments.blue_3d_mot import Blue3DMOTFrag
 
 
@@ -20,17 +19,29 @@ class ImageBlueMOT(ExpFragment):
         )
         self.dual_cameras: DualCameraMeasurement
 
-        self.suservo: SUServoChannel = self.get_device(
-            "suservo_aom_singlepass_461_imaging_switch"
+        self.suservo_setter: LibSetSUServoStatic = self.setattr_fragment(
+            "suservo_setter",
+            LibSetSUServoStatic,
+            "suservo_aom_singlepass_461_imaging_switch",
         )
 
-        self.setattr_param("expansion_time", FloatParam, "Expansion time of blue MOT", default=10e-3, unit="ms")
-        self.expansion_time : FloatParamHandle
+        self.setattr_param(
+            "expansion_time",
+            FloatParam,
+            "Expansion time of blue MOT",
+            default=10e-3,
+            unit="ms",
+        )
+        self.expansion_time: FloatParamHandle
 
-        self.setattr_param("image_time", FloatParam, "Expansion time of blue MOT", default=200e-6, unit="us"))
-        self.image_time : FloatParamHandle    
-
-
+        self.setattr_param(
+            "image_time",
+            FloatParam,
+            "Expansion time of blue MOT",
+            default=200e-6,
+            unit="us",
+        )
+        self.image_time: FloatParamHandle
 
     @kernel
     def run_once(self) -> None:
@@ -45,12 +56,16 @@ class ImageBlueMOT(ExpFragment):
 
         with parallel:
             with sequential:
-                self.suservo.set(en_out=True)
+                self.suservo_setter.set_channel_state(
+                    rf_switch_state=True, enable_iir=False
+                )
                 delay(self.image_time.get())
-                self.suservo.set(en_out=False)
-            with sequential:
-                self.dual_cameras.trigger()
-            
+                self.suservo_setter.set_channel_state(
+                    rf_switch_state=False, enable_iir=False
+                )
+
+            self.dual_cameras.trigger()
+
         self.core.wait_until_mu(now_mu())
 
         self.dual_cameras.save_data()
