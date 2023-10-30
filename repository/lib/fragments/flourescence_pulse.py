6eb3177da3e3271bfc24c2601b7133e8d3bbe336
Charles Baynham <c.baynham@imperial.ac.uk>
2023-10-30 14:29:34 +0000

    Add another test case

--- repository/lib/fragments/flourescence_pulse.py
+++ repository/lib/fragments/flourescence_pulse.py
@@ -129,3 +129,38 @@ class FlourescencePulseWithShutter(ExpFragment):
                 ),
             ],
         )
+
+
+class FlourescencePulseWithBoth(ExpFragment):
+    @kernel
+    def run_once(self) -> None:
+        self.core.break_realtime()
+        self.frag.do_imaging_pulse()
+
+    def build_fragment(self) -> None:
+        self.setattr_device("core")
+        self.setattr_fragment(
+            "frag",
+            FlourescencePulse,
+            beam_infos=[
+                SUServoedBeam(
+                    "blue_3dmot_axialminus",
+                    150e6,
+                    20,
+                    "suservo_aom_singlepass_461_3DMOT_axialminus",
+                    servo_enabled=True,
+                    setpoint=5.0,
+                ),
+                SUServoedBeam(
+                    "blue_3dmot_axialplus",
+                    150e6,
+                    20,
+                    "suservo_aom_singlepass_461_3DMOT_axialplus",
+                    "TTL_shutter_461_3dmot",
+                    shutter_delay=10e-3,
+                    servo_enabled=True,
+                    setpoint=3.2,
+                ),
+            ],
+        )
+        self.frag: FlourescencePulse
