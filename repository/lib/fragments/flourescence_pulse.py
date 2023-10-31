669a1b384ae99acbfae120580aa2244d2b1d91ce
Charles Baynham <c.baynham@imperial.ac.uk>
2023-10-31 08:02:12 +0000

    But not with both

--- repository/lib/fragments/flourescence_pulse.py
+++ repository/lib/fragments/flourescence_pulse.py
@@ -40,13 +40,13 @@ class FlourescencePulse(Fragment):
         self.setattr_fragment("all_beam_toggler", ToggleListOfBeams, beam_infos)
         self.all_beam_toggler: ToggleListOfBeams
 
-        # # Also set up the flourescence delivery AOM, regardless of which beams we're flashing
-        # self.setattr_fragment(
-        #     "delivery_beam_setter",
-        #     SetBeamsToDefaults,
-        #     [constants.AOM_BEAMS["blue_imaging_delivery"]],
-        # )
-        # self.delivery_beam_setter: SetBeamsToDefaults
+        # Also set up the flourescence delivery AOM, regardless of which beams we're flashing
+        self.setattr_fragment(
+            "delivery_beam_setter",
+            SetBeamsToDefaults,
+            [constants.AOM_BEAMS["blue_imaging_delivery"]],
+        )
+        self.delivery_beam_setter: SetBeamsToDefaults
 
         self.setattr_param(
             "flourescence_pulse_duration",
@@ -66,7 +66,7 @@ class FlourescencePulse(Fragment):
 
         # # Configure and enable the SUServos for all configured beams, and also the delivery beam
         self.all_beam_default_setter.turn_on_all(light_enabled=False)
-        # self.delivery_beam_setter.turn_on_all(light_enabled=True)
+        self.delivery_beam_setter.turn_on_all(light_enabled=True)
 
     @kernel
     def do_imaging_pulse(self):
