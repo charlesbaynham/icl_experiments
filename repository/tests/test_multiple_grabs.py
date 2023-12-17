99f7b0745090f4da8ab9ccbce5cee8903273bd9f
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-12-17 15:01:56 +0000

    Bug

--- repository/tests/test_multiple_grabs.py
+++ repository/tests/test_multiple_grabs.py
@@ -15,19 +15,29 @@ class MultipleGrabs(ExpFragment):
         self.setattr_fragment("andor_interface", AndorCameraControl)
         self.andor_interface: AndorCameraControl
 
-        self.setattr_param("delay", FloatParam, default=10e-3)
+        self.setattr_param(
+            "delay",
+            FloatParam,
+            default=10e-3,
+            description="Delay between images",
+            unit="ms",
+        )
 
+    @kernel
     def run_once(self) -> None:
         self.core.break_realtime()
 
-        self.andor_interface.trigger()
+        self.andor_interface.trigger(exposure=1e-6, control_shutter=False)
         delay(self.delay.get())
-        self.andor_interface.trigger()
+        self.andor_interface.trigger(exposure=1e-6, control_shutter=False)
 
         sums = [0, 0]
         means = [0.0, 0.0]
         self.andor_interface.readout_images(
-            sums, means, timeout_mu=now_mu() + self.core.seconds_to_mu(1.0)
+            sums,
+            means,
+            timeout_mu=now_mu() + self.core.seconds_to_mu(1.0),
+            num_images=2,
         )
 
         print(sums)
