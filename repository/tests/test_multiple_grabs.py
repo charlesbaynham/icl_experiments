23c36ae860b7c74565323d5bd871e60f5807e163
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2023-12-17 16:01:53 +0000

    Control reads separately from triggers

--- repository/tests/test_multiple_grabs.py
+++ repository/tests/test_multiple_grabs.py
@@ -3,6 +3,8 @@ from artiq.experiment import *
 from ndscan.experiment import *
 from ndscan.experiment import FloatParam
 from ndscan.experiment.entry_point import make_fragment_scan_exp
+from ndscan.experiment.parameters import FloatParamHandle
+from ndscan.experiment.parameters import IntParamHandle
 
 from repository.lib.fragments.andor_camera import AndorCameraControl
 
@@ -23,7 +25,13 @@ class MultipleGrabs(ExpFragment):
             unit="ms",
         )
 
-        self.setattr_param("num", IntParam, default=2, description="Num images")
+        self.setattr_param(
+            "num_triggers", IntParam, default=2, description="Num triggers"
+        )
+        self.num_triggers: IntParamHandle
+
+        self.setattr_param("num_reads", IntParam, default=2, description="Num reads")
+        self.num_reads: IntParamHandle
 
     @kernel
     def run_once(self) -> None:
@@ -31,17 +39,17 @@ class MultipleGrabs(ExpFragment):
 
         delay(1.0)
 
-        for _ in range(self.num.get()):
+        for _ in range(self.num_triggers.get()):
             self.andor_interface.trigger(exposure=1e-6, control_shutter=False)
             delay(self.delay.get())
 
-        sums = [0] * self.num.get()
-        means = [0.0] * self.num.get()
+        sums = [0] * self.num_reads.get()
+        means = [0.0] * self.num_reads.get()
         self.andor_interface.readout_images(
             sums,
             means,
             timeout_mu=now_mu() + self.core.seconds_to_mu(1.0),
-            num_images=self.num.get(),
+            num_images=self.num_reads.get(),
         )
 
         print(sums)
