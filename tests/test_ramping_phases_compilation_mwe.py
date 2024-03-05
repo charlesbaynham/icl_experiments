3e531aeb21f9ec688be4a5550a57a8e254174617
Charles Baynham <c.baynham@imperial.ac.uk>
2024-03-05 13:18:10 +0000

    More

--- tests/test_ramping_phases_compilation_mwe.py
+++ tests/test_ramping_phases_compilation_mwe.py
@@ -34,83 +34,16 @@ logger = logging.getLogger(__name__)
 
 
 class GeneralRampingPhase(Fragment):
-    general_setter_names: List[str] = []
-    general_setter_param_options: List[Dict] = []
-    general_setter_default_starts: List[float] = []
-    general_setter_default_ends: List[float] = []
-
-    duration = 100e-3
-    time_step = 500e-6
 
     def build_fragment(self, *args, general_setter: Optional[Callable] = None):
         self.setattr_device("core")
         self.core: Core
 
-        self.setattr_device("core_dma")
-        self.core_dma: CoreDMA
-
         self.general_setter = general_setter or self._do_nothing
-        self.general_setter_param_handles = self.build_general_setter_param_handles(
-            general_setter
-        )
 
     @kernel
-    def _do_nothing(self, num: TList(TFloat)):
+    def _do_nothing(self, val):
         pass
-
-    def build_general_setter_param_handles(self, general_setter):
-        setter_was_passed = general_setter is not None
-
-        general_setter_param_handles: List[
-            Tuple[
-                FloatParamHandle,
-                FloatParamHandle,
-            ]
-        ] = []
-
-        if setter_was_passed:
-            for name, options, start, end in zip(
-                self.general_setter_names,
-                self.general_setter_param_options,
-                self.general_setter_default_starts,
-                self.general_setter_default_ends,
-            ):
-                # For each passed parameter to the general setter, make an NDScan parameter
-                start_handle = self.setattr_param(
-                    f"{name}_start",
-                    FloatParam,
-                    f"Start value for {name}",
-                    default=start,
-                    **options,
-                )
-
-                end_handle = self.setattr_param(
-                    f"{name}_end",
-                    FloatParam,
-                    f"End value for {name}",
-                    default=end,
-                    **options,
-                )
-
-                general_setter_param_handles.append((start_handle, end_handle))
-
-        else:
-            # ARTIQ doesn't like empty lists because it doesn't know what type they are.
-            # Rather than work around this, I'll just make a general setter that does nothing.
-            # This costs us 8ns per step of wasted time.
-            general_setter = self._do_nothing
-
-            # I also need to loop over parameter handles, so I must make a dummy
-            # parameter to pass. I'll override it so that it doesn't appear in
-            # the parameter listing
-            dummy_handle = self.setattr_param(
-                "dummy_param", FloatParam, "Dummy parameter - ignore me", default=0.0
-            )
-            self.override_param("dummy_param", 0.0)
-
-            general_setter_param_handles.append((dummy_handle, dummy_handle))
-
-        return general_setter_param_handles
 
     @kernel
     def device_setup(self):
@@ -122,9 +55,6 @@ class GeneralRampingPhase(Fragment):
 
 
 class RedRampingPhaseWithFieldsAndSUServoBindings(GeneralRampingPhase):
-    # The general ramp here ramps the chamber 2 MOT coils in amps
-    general_setter_names = ["chamber_2_mot_current"]
-    general_setter_param_options = [{"min": 0, "max": 150, "unit": "A"}]
 
     def build_fragment(self):
         # Register self.set_fields as the recipient of general ramps
@@ -136,19 +66,11 @@ class RedRampingPhaseWithFieldsAndSUServoBindings(GeneralRampingPhase):
 
 
 class NarrowRedCapturePhase(RedRampingPhaseWithFieldsAndSUServoBindings):
-    duration_default = 50e-3
-
-    # Chamber 2 MOT coils in amps
-    general_setter_default_starts = [5.0]
-    general_setter_default_ends = [1.0]
+    pass
 
 
 class NarrowRedCompressionPhase(RedRampingPhaseWithFieldsAndSUServoBindings):
-    duration_default = 100e-3
-
-    # Chamber 2 MOT coils in amps
-    general_setter_default_starts = [1.0]
-    general_setter_default_ends = [1.0]
+    pass
 
 
 class RedPhaseUser(ExpFragment):
