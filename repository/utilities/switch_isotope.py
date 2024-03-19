3d461bc37190ecd074e285460f0b639d82503c1a
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-03-19 15:44:59 +0000

    No wand timeout

--- repository/utilities/switch_isotope.py
+++ repository/utilities/switch_isotope.py
@@ -58,7 +58,7 @@ class SwitchIsotopeFrag(ExpFragment):
                 self.wand_server.unlock(laser=laser, name="")
             else:
                 logger.info("Setting laser %s to %.6f MHz", laser, 1e-6 * offset)
-                self.wand_server.lock(laser=laser, set_point=offset)
+                self.wand_server.lock(laser=laser, set_point=offset, timeout=None)
 
         initial_laser_db = self.wand_server.get_laser_db()
 
