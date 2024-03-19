c2c7b15cd87069554c9783662912bd6da0238608
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-03-19 19:11:50 +0000

    Revert "Changed _calc_step_size to avoid divide by zero when num_points == 1"
    This reverts commit 85de276c3be9c3663f327249c7bb2407cebe3226.

--- repository/lib/fragments/ramping_phase.py
+++ repository/lib/fragments/ramping_phase.py
@@ -483,7 +483,7 @@ class GeneralRampingPhase(Fragment):
 
     @portable
     def _calc_step_size(self, start: TFloat, end: TFloat, num_points: TInt32) -> TFloat:
-        return (end - start) / float(num_points)
+        return (end - start) / float(num_points - 1)
 
     @kernel
     def device_setup(self):
