99bb9b389c9a7b0e51df9f3dbcbf77139895bcfc
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-03-20 11:33:53 +0000

    new alias for 698

--- device_db_config/_aliases.py
+++ device_db_config/_aliases.py
@@ -45,6 +45,7 @@ aliases = {
     "suservo_aom_singlepass_707": "suservo2_ch0",
     "suservo_aom_singlepass_679": "suservo2_ch1",
     "suservo_aom_singlepass_1379": "suservo2_ch2",
+    "suservo_aom_698_up_switch": "suservo0_ch7",  # TODO: This should be on an Urukul, but our SUServoedBeam model doesn't support nice specification for this
     # %% TTLs
     "TTL_shutter_461_pushbeam": "ttl16",
     "TTL_shutter_461_2dmot_is_it_a": "ttl17",
