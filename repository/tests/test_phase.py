3f68fe551d7c8bbbdb5d4f72f555772ac5c8ef76
Charles Baynham <c.baynham@imperial.ac.uk>
2024-04-19 12:40:26 +0100

    Turn on the beam for the test phase!

--- repository/tests/test_phase.py
+++ repository/tests/test_phase.py
@@ -3,13 +3,17 @@ Test a simple phase
 """
 import logging
 
+from artiq.coredevice.core import Core
 from artiq.experiment import *
+from artiq.experiment import delay
 from artiq.experiment import kernel
+from artiq.experiment import now_mu
 from artiq.experiment import TFloat
-from artiq.experiment import TList
 from ndscan.experiment import *
 
 from repository.lib import constants
+from repository.lib.fragments.beams.beam_setters import make_set_beams_to_default
+from repository.lib.fragments.beams.beam_setters import SetBeamsToDefaults
 from repository.lib.fragments.ramping_phase import GeneralRampingPhase
 
 logger = logging.getLogger(__name__)
@@ -25,17 +29,25 @@ class TestPhase(GeneralRampingPhase):
     default_suservo_setpoint_multiples_end = [0.1]
 
     duration_default = 100e-3
+    time_step_default = 10e-3
 
 
-class ExpFragWithPhase(ExpFragment):
+class ExpFragWithPhaseFrag(ExpFragment):
     def build_fragment(self):
         self.setattr_device("core")
+        self.core: Core
 
         self.setattr_fragment(
             "test_phase",
             TestPhase,
         )
         self.test_phase: GeneralRampingPhase
+
+        self.setattr_fragment(
+            "diagonal_beam_setter",
+            make_set_beams_to_default([constants.AOM_BEAMS["red_mot_diagonal"]]),
+        )
+        self.diagonal_beam_setter: SetBeamsToDefaults
 
         self.setattr_param(
             "delay_between_phases",
@@ -59,8 +71,11 @@ class ExpFragWithPhase(ExpFragment):
         logger.info("Precomputing handle")
         self.test_phase.precalculate_dma_handle()
 
-        logger.info("Starting test phase")
+        logger.info("Enabling diagonal beam")
+        self.core.break_realtime()
+        self.diagonal_beam_setter.turn_on_all()
 
+        logger.info("Starting test phase")
         self.core.break_realtime()
 
         for _ in range(self.num_repeats.get()):
@@ -81,4 +96,4 @@ class ExpFragWithPhase(ExpFragment):
         logger.info("Phase output completed")
 
 
-ExpFragWithPhase = make_fragment_scan_exp(ExpFragWithPhase)
+ExpFragWithPhase = make_fragment_scan_exp(ExpFragWithPhaseFrag)
