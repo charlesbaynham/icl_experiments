fe865d5839604254a8cde5dffec7bebd6461eb4c
Charles Baynham <c.baynham@imperial.ac.uk>
2024-04-23 19:28:54 +0100

    Add AD9912 test cases

--- tests/test_default_beam_setter_with_urukul.py
+++ tests/test_default_beam_setter_with_urukul.py
@@ -1,3 +1,6 @@
+from abc import ABC
+from abc import abstractmethod
+
 from artiq.coredevice.core import Core
 from artiq.experiment import kernel
 from ndscan.experiment import ExpFragment
@@ -19,75 +22,105 @@ BEAM_INFO_SUSERVO_WITH_SHUTTER = SUServoedBeam(
     shutter_device="ttl12",  # This is our first ttl output
 )
 
-BEAM_INFO_URUKUL_NO_SHUTTER = UrukuledBeam(
-    "no_shutter_urukul", 100e6, 1.0, 0.0, "urukul2_ch0"
+BEAM_INFO_AD9910_NO_SHUTTER = UrukuledBeam(
+    "no_shutter_urukul",
+    100e6,
+    1.0,
+    0.0,
+    urukul_device="urukul5_ch0",  # This is an AD9910
 )
-BEAM_INFO_URUKUL_WITH_SHUTTER = UrukuledBeam(
+BEAM_INFO_AD9910_WITH_SHUTTER = UrukuledBeam(
     "shutter_urukul",
     100e6,
     0,
     0,
-    "urukul2_ch0",
+    urukul_device="urukul5_ch0",  # This is an AD9910
     shutter_device="ttl12",  # This is our first ttl output
 )
 
 
-class SetupSingleSUServoedBeam(ExpFragment):
+BEAM_INFO_AD9912_WITH_SHUTTER = UrukuledBeam(
+    "shutter_ad9912",
+    100e6,
+    0,
+    0,
+    urukul_device="urukul2_ch0",  # This is an AD9912
+    shutter_device="urukul5_ch0",  # This is our first ttl output
+)
+
+
+class _Base(ExpFragment, ABC):
     def build_fragment(self) -> None:
         self.setattr_device("core")
         self.core: Core
 
+        self.add_setter()
+        self.default_setter: SetBeamsToDefaults
+
+    @abstractmethod
+    def add_setter(self):
+        pass
+
+    @kernel
+    def run_once(self):
+        self.core.break_realtime()
+        self.default_setter.turn_on_all()
+
+
+class SetupSingleSUServoedBeam(_Base):
+    def add_setter(self):
         self.setattr_fragment(
             "default_setter",
             make_set_beams_to_default(
                 suservo_beam_infos=[BEAM_INFO_SUSERVO_WITH_SHUTTER]
             ),
         )
-        self.default_setter: SetBeamsToDefaults
-
-    @kernel
-    def run_once(self):
-        self.core.break_realtime()
-        self.default_setter.turn_on_all()
 
 
-class SetupSingleUrukulledBeam(ExpFragment):
-    def build_fragment(self) -> None:
-        self.setattr_device("core")
-        self.core: Core
-
+class SetupSingleUrukulledBeam(_Base):
+    def add_setter(self):
         self.setattr_fragment(
             "default_setter",
             make_set_beams_to_default(
-                urukul_beam_infos=[BEAM_INFO_URUKUL_WITH_SHUTTER]
+                urukul_beam_infos=[BEAM_INFO_AD9910_WITH_SHUTTER]
             ),
         )
-        self.default_setter: SetBeamsToDefaults
-
-    @kernel
-    def run_once(self):
-        self.core.break_realtime()
-        self.default_setter.turn_on_all()
 
 
-class SetupOneOfEach(ExpFragment):
-    def build_fragment(self) -> None:
-        self.setattr_device("core")
-        self.core: Core
-
+class SetupOneOfEach(_Base):
+    def add_setter(self):
         self.setattr_fragment(
             "default_setter",
             make_set_beams_to_default(
                 suservo_beam_infos=[BEAM_INFO_SUSERVO_WITH_SHUTTER],
-                urukul_beam_infos=[BEAM_INFO_URUKUL_WITH_SHUTTER],
+                urukul_beam_infos=[BEAM_INFO_AD9910_WITH_SHUTTER],
             ),
         )
-        self.default_setter: SetBeamsToDefaults
 
-    @kernel
-    def run_once(self):
-        self.core.break_realtime()
-        self.default_setter.turn_on_all()
+
+class SetupAD9912(_Base):
+    def add_setter(self):
+        self.setattr_fragment(
+            "default_setter",
+            make_set_beams_to_default(
+                suservo_beam_infos=[],
+                urukul_beam_infos=[BEAM_INFO_AD9912_WITH_SHUTTER],
+            ),
+        )
+
+
+class SetupAD9910_AD9912_mix(_Base):
+    def add_setter(self):
+        self.setattr_fragment(
+            "default_setter",
+            make_set_beams_to_default(
+                suservo_beam_infos=[],
+                urukul_beam_infos=[
+                    BEAM_INFO_AD9912_WITH_SHUTTER,
+                    BEAM_INFO_AD9910_WITH_SHUTTER,
+                ],
+            ),
+        )
 
 
 def test_single_suservo(fragment_precompiler):
@@ -100,3 +133,11 @@ def test_single_urukul(fragment_precompiler):
 
 def test_two_beam_setup(fragment_precompiler):
     fragment_precompiler(SetupOneOfEach)
+
+
+def test_ad9912(fragment_precompiler):
+    fragment_precompiler(SetupAD9912)
+
+
+def test_ad9910_ad9912(fragment_precompiler):
+    fragment_precompiler(SetupAD9910_AD9912_mix)
