5b1acb19d9398afb6a997f0bf8c135315ffa712d
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-04-30 18:54:30 +0100

    Finish merge

--- repository/lib/dummy_devices.py
+++ repository/lib/dummy_devices.py
@@ -4,7 +4,6 @@ exist solely for the purpose of working around ARTIQ's inability to infer the
 type of an empty list. You can therefore add one of these into your list in
 build() if it's empty, and it won't do anything when called.
 """
-from artiq.coredevice.ad9910 import AD9910
 from artiq.experiment import kernel
 from artiq.experiment import TBool
 from artiq.experiment import TFloat
@@ -21,11 +20,6 @@ def create_dummy_class(original_class) -> type:
             setattr(dummy_class, method_name, lambda self, *args, **kwargs: None)
     setattr(dummy_class, "__init__", lambda self: None)
     return dummy_class
-
-
-from artiq.experiment import kernel
-from artiq.experiment import TBool
-from artiq.experiment import TFloat
 
 
 class DummySUServoFrag:
@@ -84,8 +78,8 @@ class DummyAD9910:
         return 0.0
 
     @kernel
-    def set(self, frequency: TFloat = 0.0, amplitude: TFloat = 1.0) -> TFloat:
-        return 0.0
+    def set_att(self, att: TFloat):
+        pass
 
     @kernel
     def cfg_sw(self, state: TBool):
