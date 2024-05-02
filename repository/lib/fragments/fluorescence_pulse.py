a03ac4174b7d950c79fd73c5b9621092fe258c4f
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-05-02 17:41:20 +0100

    Fluorescence pulse works fine

--- repository/lib/fragments/fluorescence_pulse.py
+++ repository/lib/fragments/fluorescence_pulse.py
@@ -26,8 +26,6 @@ from repository.lib.fragments.beams.toggling_beam_setter import ToggleListOfBeam
 
 logger = logging.getLogger(__name__)
 
-# FIXME: The fluorescence pulse (not MOT beams) appears to be broken still
-
 
 class FluorescencePulseBase(Fragment):
     """
