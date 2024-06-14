7157fa298556c4a921b6b6041f49f5aad4d63c41
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-06-14 11:56:33 +0100

    Use new lattice mixin

--- repository/red_mot/measure_red_mot.py
+++ repository/red_mot/measure_red_mot.py
@@ -8,10 +8,12 @@ from repository.lib.fragments.red_mot.red_mot_experiment import RedMOTWithExperi
 from repository.lib.fragments.red_mot.red_mot_mixins.bg_corrected_andor_image import (
     BGCorrectedAndorImage,
 )
+from repository.lib.fragments.red_mot.red_mot_mixins.constant_lattice import (
+    ConstantLatticeMixin,
+)
 from repository.lib.fragments.red_mot.red_mot_mixins.single_andor_image import (
     SingleAndorImage,
 )
-
 
 logger = logging.getLogger(__name__)
 
@@ -39,13 +41,23 @@ class _MeasureNarrowbandMOTFrag(RedMOTWithExperiment):
         pass
 
 
-class MeasureNarrowbandMOTFrag(SingleAndorImage, _MeasureNarrowbandMOTFrag):
+class MeasureNarrowbandMOTFrag(
+    SingleAndorImage, ConstantLatticeMixin, _MeasureNarrowbandMOTFrag
+):
+    """
+    Make a narrowband MOT, image with the ANDOR and leave lattice light on
+    """
+
     pass
 
 
 class MeasureNarrowbandMOTBGCorrectedFrag(
-    BGCorrectedAndorImage, _MeasureNarrowbandMOTFrag
+    BGCorrectedAndorImage, ConstantLatticeMixin, _MeasureNarrowbandMOTFrag
 ):
+    """
+    Make a narrowband MOT, image twice for BG subtraction with the ANDOR and leave lattice light on
+    """
+
     pass
 
 
