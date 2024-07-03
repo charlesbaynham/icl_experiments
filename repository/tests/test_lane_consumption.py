24c01d539918cd7c8b3d702ed5a003c247ca9f5b
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-07-03 11:36:44 +0100

    Be dumb

--- repository/tests/test_lane_consumption.py
+++ repository/tests/test_lane_consumption.py
@@ -2,6 +2,7 @@ import logging
 
 from artiq.coredevice.core import Core
 from artiq.experiment import *
+from artiq.experiment import at_mu
 from artiq.experiment import delay
 from artiq.experiment import delay_mu
 from artiq.experiment import now_mu
@@ -29,9 +30,10 @@ class TestAD9910LaneUsage(EnvExperiment):
 
         self.core.break_realtime()
 
-        with parallel:
-            for ttl in self.ttls:
-                ttl.on()
+        t_now_mu = now_mu()
+        for ttl in self.ttls:
+            at_mu(t_now_mu)
+            ttl.on()
 
         self.core.wait_until_mu(now_mu())
         logger.info("Test done, resetting TTLs")
