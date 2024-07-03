b879de1c022a8b2e81073fdd3fece0e43e5916ca
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-07-03 12:16:21 +0100

    Not working..

--- repository/tests/test_lane_consumption.py
+++ repository/tests/test_lane_consumption.py
@@ -1,6 +1,10 @@
 import logging
+from typing import List
 
+from artiq.coredevice.ad9910 import AD9910
 from artiq.coredevice.core import Core
+from artiq.coredevice.suservo import Channel as SUServoChannel
+from artiq.coredevice.ttl import TTLOut
 from artiq.experiment import *
 from artiq.experiment import at_mu
 from artiq.experiment import delay
@@ -14,30 +18,45 @@ logger = logging.getLogger(__name__)
 # Hypothesis: the AD9910 code is consuming more than one lane in the latest ARTIQ update. That's a regression.
 # TODO: Write a test here to check this. Use "test_lanes_during_ramps.py" as inspiration.
 
-NUM_LANES = 16
-
 
 class TestAD9910LaneUsage(EnvExperiment):
     def build(self):
         self.setattr_device("core")
         self.core: Core
 
-        self.ttls = [self.get_device(f"ttl{i}") for i in range(NUM_LANES)]
+        self.urukul_channels: List[AD9910] = [
+            self.get_device(f"urukul5_ch{i}") for i in range(4)
+        ] + [self.get_device(f"urukul8_ch{i}") for i in range(4)]
+
+        self.setattr_device("ttl0")
+        self.ttl0: TTLOut
+
+        for uc in self.urukul_channels:
+            print(uc)
+
+        logger.warning("This test does not work! Do not trust it")
 
     @kernel
     def run(self):
         logger.info("Starting test")
 
-        self.core.break_realtime()
-
-        t_now_mu = now_mu()
-        for ttl in self.ttls:
-            at_mu(t_now_mu)
-            ttl.on()
-
-        self.core.wait_until_mu(now_mu())
-        logger.info("Test done, resetting TTLs")
-
-        for ttl in self.ttls:
+        for _ in range(1):
             self.core.break_realtime()
-            ttl.off()
+
+            t_now_mu = now_mu()
+            for i in range(8):
+                urukul_channel = self.urukul_channels[i]
+
+                at_mu(t_now_mu)
+                urukul_channel.set(100e6, 0.0, amplitude=0.0)
+
+            at_mu(t_now_mu)
+            self.ttl0.on()
+
+            delay(1e-3)
+            self.ttl0.off()
+
+            logger.info("Test done")
+
+            delay(1.0)
+            self.core.wait_until_mu(now_mu())
