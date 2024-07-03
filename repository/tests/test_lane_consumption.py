34b73ac53922e68af2dc4bd6b1414101af0d6648
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-07-03 11:15:41 +0100

    Add lane tester

---
+++ repository/tests/test_lane_consumption.py
@@ -0,0 +1,38 @@
+import logging
+
+from artiq.coredevice.core import Core
+from artiq.experiment import *
+from artiq.experiment import delay
+from artiq.experiment import delay_mu
+from artiq.experiment import now_mu
+from artiq.experiment import parallel
+from artiq.experiment import sequential
+
+logger = logging.getLogger(__name__)
+
+# Hypothesis: the AD9910 code is consuming more than one lane in the latest ARTIQ update. That's a regression.
+# TODO: Write a test here to check this. Use "test_lanes_during_ramps.py" as inspiration.
+
+NUM_LANES = 8
+
+
+class TestAD9910LaneUsage(EnvExperiment):
+    def build(self):
+        self.setattr_device("core")
+        self.core: Core
+
+        self.ttls = [self.get_device(f"ttl{i}") for i in range(NUM_LANES)]
+
+    @kernel
+    def run(self):
+        logger.info("Starting test")
+
+        self.core.break_realtime()
+
+        # This should cause an RTIO sequence error unless we're only using a single lane
+        with parallel:
+            for ttl in self.ttls:
+                ttl.on()
+
+        self.core.wait_until_mu(now_mu())
+        logger.info("Test done")
