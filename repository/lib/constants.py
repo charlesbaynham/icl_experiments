e0753dfa81d79ea26c2d09606b327d487864771e
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-08-29 16:36:17 +0100

    Put red IJD back to real value

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -40,7 +40,7 @@ USE_LATTICE_MODE = False
 URUKULED_BEAMS = [
     UrukuledBeam(
         name="red_doublepass_injection",
-        frequency=367.0e6,
+        frequency=365.0e6,
         amplitude=1.0,
         attenuation=0.0,
         urukul_device="urukul9910_aom_doublepass_689_red_injection",
