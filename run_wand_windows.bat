a06ef66895cad3c7d1974a44d91514ec1cba2821
StrontiumLab <stronlab@ph-cb2409.ic.ac.uk>
2024-09-04 10:38:02 +0100

    Add Windows dashboard scripts

---
+++ run_wand_windows.bat
@@ -0,0 +1,4 @@
+@REM Launch an ARTIQ dashboard on Windows
+@REM You must have manually set up a poetry environment and have kept it up to date using "poetry install"
+
+poetry run wand_gui -n icl_aion
