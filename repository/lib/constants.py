d50ad41a13a821abb40e1e1bb7b055a81bd78da7
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-09-04 11:44:03 +0100

    Tweak red IJD window upwards

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -134,7 +134,7 @@ IJD_DEFAULTS = {
     ),
     "blue_IJD3_controller": IJDSettings(8850, 358e-3, 348e-3, 3e-3),
     "red_IJD1_controller": IJDSettings(
-        9460, 190.0e-3, 187.0e-3, 3e-3, associated_beams=["red_doublepass_injection"]
+        9460, 191.0e-3, 188.0e-3, 3e-3, associated_beams=["red_doublepass_injection"]
     ),
 }
 "Injected diode default settings"
