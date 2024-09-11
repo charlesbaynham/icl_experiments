a73e77d20d0266e34c14c016266dde37e713bf0e
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-09-11 09:57:14 +0100

    undo

--- repository/lib/fragments/red_mot/red_mot_mixins/triple_imaging_kinetics.py
+++ repository/lib/fragments/red_mot/red_mot_mixins/triple_imaging_kinetics.py
@@ -135,15 +135,15 @@ class TripleImageFastKineticsMixin(RedMOTWithExperiment):
 
     @kernel
     def do_first_pulse(self, andor_exposure):
-        self.fluorescence_pulse.do_imaging_pulse(andor_exposure)
+        self.do_pulse(andor_exposure)
 
     @kernel
     def do_second_pulse(self, andor_exposure):
-        self.fluorescence_pulse.do_imaging_pulse(andor_exposure)
+        self.do_pulse(andor_exposure)
 
     @kernel
     def do_third_pulse(self, andor_exposure):
-        self.fluorescence_pulse.do_imaging_pulse(andor_exposure)
+        self.do_pulse(andor_exposure)
 
     @kernel
     def save_data_hook(self):
