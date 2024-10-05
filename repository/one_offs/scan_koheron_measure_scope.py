464ccee925ee05adcc8968b6925be804913174a1
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-10-05 19:44:51 +0100

    Squashed commit of the following:
    commit cd6ad7152412744693c9136ef4d418300a6af3b0
    Author: Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
    Date:   Sat Oct 5 19:44:24 2024 +0100
    
        More delay
    
    commit 32257cfc367a740058c55a6ae8a038623904a371
    Author: Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
    Date:   Sat Oct 5 19:38:43 2024 +0100
    
        Set current
    
    commit 34ff6a7732bf8767c6b0883b583c0f7e5b654530
    Author: Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
    Date:   Sat Oct 5 19:34:28 2024 +0100
    
        Debug
    
    commit e028fb5d32f3c12e851d14bfb8e164e3f3628546
    Author: Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
    Date:   Sat Oct 5 19:32:56 2024 +0100
    
        Debug
    
    commit d2cea854bd8d4fee524658fab2e4e02e24de0d99
    Author: Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
    Date:   Sat Oct 5 19:31:57 2024 +0100
    
        Record data
    
    commit 8d941f915110078c4fc8e2a41a5d73b148796e2d
    Author: Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
    Date:   Sat Oct 5 19:26:30 2024 +0100
    
        Raw data
    
    commit a8c1ae1953304a933edf197dc74d6e096d37f367
    Author: Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
    Date:   Sat Oct 5 19:23:33 2024 +0100
    
        Data
    
    commit 660c4baa5f7c81790f953d1cf514e1e37beb1559
    Author: Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
    Date:   Sat Oct 5 19:20:23 2024 +0100
    
        Scale
    
    commit 05099a7a888c809cfe8f0a04d1bdf6e848cd6f49
    Author: Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
    Date:   Sat Oct 5 19:16:52 2024 +0100
    
        Tweak
    
    commit cd70a7512bc8c8025d53ab19f1dc39801a1cee8d
    Author: Charles Baynham <c.baynham@imperial.ac.uk>
    Date:   Sat Oct 5 19:15:16 2024 +0100
    
        Start scope setup
    
    commit 580a0e2b70e0aa69deb535d505b1886a44fe49ca
    Author: Charles Baynham <c.baynham@imperial.ac.uk>
    Date:   Sat Oct 5 18:57:37 2024 +0100
    
        Add RS package

---
+++ repository/one_offs/scan_koheron_measure_scope.py
@@ -0,0 +1,96 @@
+import time
+
+import numpy as np
+from artiq.experiment import *
+from koheron_ctl200_laser_driver import CTL200
+from ndscan.experiment import *
+from ndscan.experiment.parameters import FloatParamHandle
+from RsInstrument import *
+
+# See https://www.rohde-schwarz.com/webhelp/RTB_HTML_UserManual_en/Content/6f1f81df55074fde.htm
+SETUP_CMDS = """
+*RST
+CHAN:TYPE HRES
+CHAN:DATA:POIN DMAX
+CHANnel1:SCALe 1e-3
+CHANnel2:SCALe 10e-3
+CHANnel1:COUPling ACLimit
+CHANnel2:COUPling ACLimit
+TIMebase:RANGe 0.000006
+TRIGger:A:SOURce CH2
+TRIGger:A:TYPE EDGE
+TRIGger:A:LEVel1:VALue 0.0
+WGENerator:FUNCtion SINusoid
+WGENerator:VOLTage 0.1
+WGENerator:VOLTage:OFFSet 0.0
+WGENerator:FREQuency 500000
+WGENerator:OUTPut ON
+CHANnel1:STATe ON
+CHANnel2:STATe ON
+ACQuire:POINts 20000
+"""
+
+
+class ScanKoheronMeasureScopeFrag(ExpFragment):
+    def build_fragment(self):
+        self.setattr_device("core")
+
+        self.setattr_param(
+            "current",
+            FloatParam,
+            description="IJD1 current",
+            default=0.350,
+            unit="mA",
+            max=0.38,
+            min=0,
+        )
+        self.current: FloatParamHandle
+
+        self.laser_driver: CTL200 = self.get_device("blue_IJD1_controller")
+
+        self.setattr_result("time", OpaqueChannel)
+        self.setattr_result("current_modulation", OpaqueChannel)
+        self.setattr_result("photodiode_signal", OpaqueChannel)
+
+        self.time: ResultChannel
+        self.current_modulation: ResultChannel
+        self.photodiode_signal: ResultChannel
+
+    def host_setup(self):
+        self.scope = RsInstrument("TCPIP::scope2.lan::INSTR", id_query=True, reset=True)
+
+        for cmd in SETUP_CMDS.split("\n"):
+            self.scope.write(cmd)
+
+    def host_cleanup(self):
+        self.scope.close()
+
+    def get_data(self, chan):
+        self.scope.write("SING")
+        self.scope.query("*OPC?")
+
+        data_raw = self.scope.query_bin_or_ascii_float_list(
+            "FORM ASC;:CHAN{}:DATA?".format(chan)
+        )
+        header = self.scope.query_bin_or_ascii_float_list(
+            "CHANnel{}:DATA:HEADer?".format(chan)
+        )
+
+        x_vals = np.linspace(header[0], header[1], int(header[2])).tolist()
+
+        return x_vals, data_raw
+
+    def run_once(self):
+        self.laser_driver.set_current_mA(1e3 * self.current.get())
+
+        time.sleep(1)
+
+        t, data_ch1 = self.get_data("1")
+        t, data_ch2 = self.get_data("2")
+
+        self.time.push(t)
+        self.photodiode_signal.push(data_ch1)
+        self.current_modulation.push(data_ch2)
+
+
+ScanKoheronMeasureScope = make_fragment_scan_exp(ScanKoheronMeasureScopeFrag)
