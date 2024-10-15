508196e9de24027859e87c0bd5f36f06efbfb2f2
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-10-15 13:30:28 +0100

    Correct bg subtraction with exponential

--- repository/lib/experiment_templates/mixins/ndscan_analysis_exponential_decay.py
+++ repository/lib/experiment_templates/mixins/ndscan_analysis_exponential_decay.py
@@ -1,20 +1,28 @@
 from ndscan.experiment import *
 
-from repository.lib.experiment_templates.mixins.single_andor_image import (
-    SingleAndorImage,
-)
+from repository.lib.experiment_templates.red_mot_experiment import RedMOTWithExperiment
 
 
-class ExponentialDecayMixin(SingleAndorImage):
+class ExponentialDecayMixin(RedMOTWithExperiment):
     def get_default_analyses(self):
         super_analysis = super().get_default_analyses()
+
+        # Valid output channels for exponential decay:
+        possible_channels = [
+            "andor_mean",
+            "andor_mean_bg_corrected",
+            "andor_sum",
+            "image_horizontal_mean",
+            "image_vertical_mean",
+        ]
+
+        # Filter by whether they exist or not for this fragment
+        valid_channels = [c for c in possible_channels if hasattr(self, c)]
 
         return super_analysis + [
             OnlineFit(
                 "exponential_decay",
-                data={
-                    "x": self.expansion_time,
-                    "y": self.andor_mean,
-                },
+                data={"x": self.expansion_time, "y": getattr(self, c)},
             )
+            for c in valid_channels
         ]
