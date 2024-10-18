3388159adabda85253a0c55265595dbbf987f27f
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-10-18 10:43:39 +0100

    Default ROIs

--- repository/lib/experiment_templates/mixins/andor_imaging/double_trap_basic.py
+++ repository/lib/experiment_templates/mixins/andor_imaging/double_trap_basic.py
@@ -1,5 +1,7 @@
 import logging
 
+from repository.lib import constants
+
 from .single_andor_image import SingleAndorImage
 
 logger = logging.getLogger(__name__)
@@ -21,3 +23,49 @@ class DoubleTrapImagingBasic(SingleAndorImage):
     num_andor_images = 1
     num_grabber_readouts = 1
     num_grabber_rois = 2
+
+    def build_fragment(self):
+        super().build_fragment()
+
+        # Set default ROIs
+        self.setattr_param_rebind(
+            "roi_0_x0",
+            self.andor_camera_control,
+            default=constants.ANDOR_ROI_DIPOLE_TRAP_FORWARD_X0,
+        )
+        self.setattr_param_rebind(
+            "roi_0_x1",
+            self.andor_camera_control,
+            default=constants.ANDOR_ROI_DIPOLE_TRAP_FORWARD_X1,
+        )
+        self.setattr_param_rebind(
+            "roi_0_y0",
+            self.andor_camera_control,
+            default=constants.ANDOR_ROI_DIPOLE_TRAP_FORWARD_Y0,
+        )
+        self.setattr_param_rebind(
+            "roi_0_y1",
+            self.andor_camera_control,
+            default=constants.ANDOR_ROI_DIPOLE_TRAP_FORWARD_Y1,
+        )
+
+        self.setattr_param_rebind(
+            "roi_1_x0",
+            self.andor_camera_control,
+            default=constants.ANDOR_ROI_DIPOLE_TRAP_BACKWARD_X0,
+        )
+        self.setattr_param_rebind(
+            "roi_1_x1",
+            self.andor_camera_control,
+            default=constants.ANDOR_ROI_DIPOLE_TRAP_BACKWARD_X1,
+        )
+        self.setattr_param_rebind(
+            "roi_1_y0",
+            self.andor_camera_control,
+            default=constants.ANDOR_ROI_DIPOLE_TRAP_BACKWARD_Y0,
+        )
+        self.setattr_param_rebind(
+            "roi_1_y1",
+            self.andor_camera_control,
+            default=constants.ANDOR_ROI_DIPOLE_TRAP_BACKWARD_Y1,
+        )
