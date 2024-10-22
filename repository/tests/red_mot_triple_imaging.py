7132e025a3b2f1e34f29ffff918e4d4599764aa6
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-10-22 15:56:12 +0100

    Test exp

--- repository/tests/red_mot_triple_imaging.py
+++ repository/tests/red_mot_triple_imaging.py
@@ -6,16 +6,28 @@ from ndscan.experiment import make_fragment_scan_exp
 from repository.lib.experiment_templates.mixins.andor_imaging.triple_imaging_basic import (
     TripleImageBasicMixin,
 )
+
+from repository.lib.experiment_templates.mixins.andor_imaging.triple_imaging_kinetics import (
+    TripleImageFastKineticsMixin,
+)
 from repository.lib.experiment_templates.red_mot_experiment import RedMOTWithExperiment
 
 logger = logging.getLogger(__name__)
 
 
-class TestTripleImaging(TripleImageBasicMixin, RedMOTWithExperiment):
+class TestTripleImagingBasic(TripleImageBasicMixin, RedMOTWithExperiment):
     @kernel
     def do_experiment_after_red_mot_hook(self):
         # No spectroscopy needed - just do nothing and move straight to imaging
         pass
 
 
-TestTripleImagingExp = make_fragment_scan_exp(TestTripleImaging)
+class TestTripleImagingKinetics(TripleImageFastKineticsMixin, RedMOTWithExperiment):
+    @kernel
+    def do_experiment_after_red_mot_hook(self):
+        # No spectroscopy needed - just do nothing and move straight to imaging
+        pass
+
+
+TestTripleImagingBasicExp = make_fragment_scan_exp(TestTripleImagingBasic)
+TestTripleImagingKineticsExp = make_fragment_scan_exp(TestTripleImagingKinetics)
