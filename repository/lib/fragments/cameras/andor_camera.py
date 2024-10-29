3a577a45d7f825835615a0a38ec42a17cd86e0cd
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-10-29 11:21:49 +0000

    Remove fixmes

--- repository/lib/fragments/cameras/andor_camera.py
+++ repository/lib/fragments/cameras/andor_camera.py
@@ -284,9 +284,7 @@ class AndorCameraControl(Fragment):
                 self.cam.set_external_start_trigger()
             else:
                 logger.debug("Setting external exposure mode")
-                self.cam.setup_run_till_abort_mode(exposure_time=0.001)  # FIXME
-                # self.cam.set_external_exposure_trigger() # FIXME
-                self.cam.set_external_trigger()  # FIXME
+                self.cam.set_external_exposure_trigger()
                 logger.debug("Setting continuous acquisition mode")
                 self.cam.set_run_till_abort_mode()
                 # Start the acquisition here: it'll run forever and we just
@@ -498,7 +496,6 @@ class AndorCameraControl(Fragment):
         if img is None:
             raise AndorNoImageAvailable("There was no image to read out")
 
-        img_array = np.array(img)
         img_array = np.rot90(img_array, axes=(1, 0))
 
         return img_array
