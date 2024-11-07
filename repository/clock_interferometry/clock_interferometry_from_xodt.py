425ba0636414291a571a60e11343651e151446a5
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-11-07 17:47:34 +0000

    Bodge in EM gain... We promised we wouldn't do this

--- repository/clock_interferometry/clock_interferometry_from_xodt.py
+++ repository/clock_interferometry/clock_interferometry_from_xodt.py
@@ -37,11 +37,23 @@ class DifferentialClockInterferometryFrag(
     Clock interferometry from a double XODT
     """
 
+    def host_setup(self):
+        super().host_setup()
+
+        # TODO: Make this not a horrible hack
+        logger.warning("Setting EMCCD gain to 30. BEWARE!!!")
+        self.andor_camera_control.cam.set_EMCCD_gain(30)
+
     @kernel
     def before_start_hook(self):
         self.before_start_hook_clockspec()
         self.before_start_hook_xodt_molasses()
         self.before_start_hook_clockshelving()
+
+    def host_cleanup(self):
+        logger.warning("EM gain turned off again")
+        self.andor_camera_control.cam.set_EMCCD_gain(0)
+        return super().host_cleanup()
 
 
 DifferentialClockInterferometry = make_fragment_scan_exp(
