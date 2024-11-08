d624b2857adc53d4ece6e33042883e840dece2ca
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-11-08 10:39:02 +0000

    oops image lists broke things, back to arrays

--- repository/lib/experiment_templates/mixins/andor_imaging/imaging_base.py
+++ repository/lib/experiment_templates/mixins/andor_imaging/imaging_base.py
@@ -66,9 +66,6 @@ class AndorImagingBase(RedMOTWithExperiment):
         self.kernel_invariants.add("num_grabber_rois")
         self.kernel_invariants.add("num_grabber_readouts")
 
-        # sometimes we have multiple acquisitions and need to store the images
-        self.image_store = []
-
     def hook_setup_andor(self):
         """
         Setup the Andor camera
@@ -129,6 +126,8 @@ class AndorImagingBase(RedMOTWithExperiment):
             self.andor_images.append(image)
 
     def host_setup(self):
+        # sometimes we have multiple acquisitions and need to store the images
+        self.image_store = np.array([])
         if self.use_andor_driver.get():
             self.ccb.issue(
                 "create_applet",
@@ -191,7 +190,7 @@ class AndorImagingBase(RedMOTWithExperiment):
     @rpc(flags={"async"})
     def _call_camera_rpc(self):
         # Get new images and add them to any images we got earlier
-        images = self.image_store + self.get_andor_images()
+        images = np.append(self.image_store, self.get_andor_images())
         # Update detailed images
         for i, image in enumerate(images):
             dataset_name = ANDOR_DETAILED_MONITOR_DATASETS.format(i=i)
@@ -215,7 +214,7 @@ class AndorImagingBase(RedMOTWithExperiment):
             num_images=self.num_images_per_series
         )
 
-        return imgs_array.tolist()
+        return imgs_array
 
     @host_only
     @staticmethod
@@ -251,7 +250,6 @@ class AndorImagingBase(RedMOTWithExperiment):
 
     @kernel
     def get_grabber_data(self):
-
         # Arrays to hold all the ROIs
         sums = [0] * self.num_grabber_rois * self.num_grabber_readouts
         means = [0.0] * self.num_grabber_rois * self.num_grabber_readouts
