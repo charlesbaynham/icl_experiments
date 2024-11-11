cdf99eb2261911201884b05b80aa4fc8b597209b
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-11-11 18:39:18 +0000

    Test EM gain

--- repository/clock_interferometry/clock_interferometry_from_mot.py
+++ repository/clock_interferometry/clock_interferometry_from_mot.py
@@ -3,6 +3,7 @@ import logging
 from artiq.experiment import kernel
 from ndscan.experiment.entry_point import make_fragment_scan_exp
 
+from repository.lib.experiment_templates.mixins.andor_imaging.em_gain import EMGain
 from repository.lib.experiment_templates.mixins.andor_imaging.normalised_fast_kinetics import (
     NormalisedRedMOTFastKineticsMixin,
 )
@@ -21,7 +22,7 @@ logger = logging.getLogger(__name__)
 
 class MOTClockInterferometryNormalizedExp(
     ClockInterferometryRedMOTMixin,
-    # ClockShelvingAndClearoutRedMOTMixin, c
+    # ClockShelvingAndClearoutRedMOTMixin, # FIXME
     FLIRBlueMOTMeasurementMixin,
     NormalisedRedMOTFastKineticsMixin,
 ):
@@ -41,6 +42,7 @@ class MOTClockInterferometrySUServoExp(
     # ClockShelvingAndClearoutRedMOTMixin,  # FIXME
     FLIRBlueMOTMeasurementMixin,
     NormalisedRedMOTFastKineticsMixin,
+    EMGain,  # FIXME
 ):
     """
     Clock interferometry from red MOT using SUServo
