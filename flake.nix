33ac3d3e3e8914017af78bfec58faab13baebc4b
Charles Baynham <c.baynham@imperial.ac.uk>
2024-11-13 17:02:35 +0000

    Update ARTIQ - still using non-pyaion version for now, this needs cleaning up

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   # TODO: Go back to pyaion artiq. This is currently hard because we're getting
   # sequence errors coming from somewhere in the red MOT sequence when we
   # update. It's not clear why
-  inputs.alt_artiq.url = "git+https://gitlab.com/aion-physics/code/artiq/forks/artiq_fork.git?ref=make-event-spreading-optional";
+  inputs.alt_artiq.url = "git+https://gitlab.com/aion-physics/code/artiq/forks/artiq_fork.git";
   inputs.alt_artiq.inputs.nixpkgs.follows = "nixpkgs";
   inputs.pyaion.inputs.artiq.follows = "alt_artiq";
 
