68d04a498183467c6a9701dbf375d849a06f0f0f
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-11-14 11:35:56 +0000

    get it in seconds

--- repository/lib/experiment_templates/mixins/clock_interferometry.py
+++ repository/lib/experiment_templates/mixins/clock_interferometry.py
@@ -107,13 +107,14 @@ class ClockInterferometryBase(ClockSpectroscopyBase):
         delay_mu(int64(self.core.ref_multiplier))
         self.stark_689_dds.sw.on()
 
-        logger.info(now_mu())
+        start = now_mu()
         # Phase step
         self.clock_dds.set(
             frequency=CLOCK_BEAM_INFO.frequency,
             phase=self.phase_constant + 1.0 * self.phase_step.get(),
         )
-        logger.info(now_mu())
+        end = now_mu()
+        logger.debug("Phase step took %d mu", self.core.mu_to_seconds(end - start))
 
         # PI PULSE
         at_mu(
