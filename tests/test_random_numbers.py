09acc325fcb1591e90abe6ecafbc79771ef1d1e1
Charles Baynham <c.baynham@imperial.ac.uk>
2024-11-18 17:12:44 +0000

    Test randomness of random numbers

--- tests/test_random_numbers.py
+++ tests/test_random_numbers.py
@@ -1,6 +1,27 @@
 from artiq.experiment import *
+from repository.lib.utils import SimpleRandom
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+OUTFILE = Path(__file__, "..", "random_numbers.png").resolve()
 
 
 class RandomNumbers(EnvExperiment):
     def build(self):
-        pass
+        seed = 12345  # Any integer seed
+        self.rng = SimpleRandom(self, seed)
+
+    @portable
+    def run(self):
+        nums = []
+        for _ in range(1000):
+            x = self.rng.next()
+            nums.append(x)
+            print(x)
+
+        plt.hist(nums, bins=20)
+        plt.savefig(OUTFILE)
+
+
+def test_random_numbers(build_and_run_experiment):
+    build_and_run_experiment(RandomNumbers)
