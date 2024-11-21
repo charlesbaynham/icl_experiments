69a5e98abe26cd071fee77e659a615b5cb340dd7
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-11-21 14:48:13 +0000

    Expose freq

--- repository/blue_mot/plug_beam_test.py
+++ repository/blue_mot/plug_beam_test.py
@@ -25,7 +25,6 @@ PlugBeamSetter = make_set_beams_to_default(
     ],
     urukul_beam_infos=[],
     name="PlugBeamSetter",
-    use_automatic_setup=True,
 )
 
 
@@ -33,15 +32,6 @@ class ScanPlugBeamParamsFrag(ExpFragment):
     def build_fragment(self) -> None:
         self.setattr_device("core")
         self.core: Core
-
-        # self.get_device("suservo_aom_doublepass_461_plug")
-
-        # self.blue_aom = self.setattr_fragment(
-        #     "blue_aom",
-        #     LibSetSUServoStatic,
-        #     constants.SUSERVOED_BEAMS["blue_plug_beam"].suservo_device,
-        # )
-        # self.blue_aom: LibSetSUServoStatic
 
         self.setattr_fragment("blue_mot", Blue3DMOTFrag)
         self.blue_mot: Blue3DMOTFrag
@@ -67,62 +57,26 @@ class ScanPlugBeamParamsFrag(ExpFragment):
         )
         self.delay_between_points: FloatParamHandle
 
-        # self.setattr_param(
-        #     "plug_aom_attenuation",
-        #     FloatParam,
-        #     description="Attenuation on Urukul's variable attenuator",
-        #     default=30,
-        #     unit="dB",
-        #     min=0,
-        #     max=31.5,
-        # )
-        # self.plug_aom_attenuation: FloatParamHandle
+        self.setattr_param_rebind(
+            "plug_beam_setpoint",
+            self.plug_beam_default_setter,
+            "setpoint_blue_plug_beam",
+            description="Setpoint",
+        )
+        self.plug_beam_setpoint: FloatParamHandle
 
-        # self.setattr_param_rebind(
-        #     "plug_beam_setpoint",
-        #     self.plug_beam_default_setter,
-        #     "setpoint_blue_plug_beam",
-        #     description="Setpoint",
-        # )
-        # self.plug_beam_setpoint: FloatParamHandle
-
-        # self.setattr_param_rebind(
-        #     "plug_beam_frequency",
-        #     self.plug_beam_default_setter,
-        #     "frequency_blue_plug_beam",
-        #     description="Frequency",
-        # )
-        # self.plug_beam_frequency: FloatParamHandle
-
-        # self.setattr_param(
-        #     "plug_aom_frequency",
-        #     FloatParam,
-        #     description="Frequency of plug beam AOM",
-        #     default=165e6,
-        #     unit="MHz",
-        #     min=0,
-        #     #    max=185e6,
-        # )
-        # self.plug_aom_frequency: FloatParamHandle
-
-        # self.attenuation = constants.SUSERVOED_BEAMS["blue_plug_beam"].attenuation
-        # self.initial_amplitude = constants.SUSERVOED_BEAMS[
-        #     "blue_plug_beam"
-        # ].initial_amplitude
+        self.setattr_param_rebind(
+            "plug_beam_frequency",
+            self.plug_beam_default_setter,
+            "frequency_blue_plug_beam",
+            description="Frequency",
+        )
+        self.plug_beam_frequency: FloatParamHandle
 
     @kernel
     def run_once(self) -> None:
         # Turn on the plug beam and set its amplitude, setpoint and frequency
         self.plug_beam_default_setter.turn_on_all()
-
-        # # Override its frequency. We must also set its setpoint again
-        # self.blue_aom.set_suservo(
-        #     freq=self.plug_aom_frequency.get(),
-        #     amplitude=self.initial_amplitude,
-        #     attenuation=self.attenuation,
-        #     rf_switch_state=True,
-        #     setpoint_v=self.plug_beam_setpoint.get(),
-        # )
 
         self.blue_mot.load_mot()  # This turns on MOT coils, "clears out" for 100ms, then turns on MOT beams, and waits for loading time
 
