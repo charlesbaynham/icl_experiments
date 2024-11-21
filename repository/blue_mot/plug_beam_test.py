2aa72937d3a573ea2c186c145d05aadd2c9f87c6
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2024-11-21 13:25:30 +0000

    Test

--- repository/blue_mot/plug_beam_test.py
+++ repository/blue_mot/plug_beam_test.py
@@ -101,10 +101,10 @@ class ScanPlugBeamParamsFrag(ExpFragment):
     def run_once(self) -> None:
         self.plug_beam_default_setter.turn_on_all()
 
-        self.blue_aom.set(1, 0, self.blue_aom.servo_channel)
-        self.blue_aom.set_dds(
-            self.blue_aom.servo_channel, self.plug_aom_frequency.get(), offset=0.0
-        )
+        # self.blue_aom.set(1, 0, self.blue_aom.servo_channel)
+        # self.blue_aom.set_dds(
+        #     self.blue_aom.servo_channel, self.plug_aom_frequency.get(), offset=0.0
+        # )
 
         # self.blue_mot.load_mot()  # This turns on MOT coils, "clears out" for 100ms, then turns on MOT beams, and waits for loading time
 
