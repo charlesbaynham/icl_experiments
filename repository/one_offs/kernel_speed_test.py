3e7663fa43d526e4121cddd898708369ff3fda75
Charles Baynham <c.baynham@imperial.ac.uk>
2024-12-09 14:29:59 +0000

    Convert to frag

--- repository/one_offs/kernel_speed_test.py
+++ repository/one_offs/kernel_speed_test.py
@@ -3,44 +3,57 @@ import logging
 import numpy as np
 from artiq.coredevice.core import Core
 from artiq.experiment import *
+from ndscan.experiment import *
 
 logger = logging.getLogger(__name__)
 
 
-class KernelSpeedTest(EnvExperiment):
-    def build(self):
+class KernelSpeedTestFrag(ExpFragment):
+    def build_fragment(self):
         self.setattr_device("core")
         self.core: Core
 
-        self.setattr_argument(
+        self.n = self.setattr_param(
             "n",
-            NumberValue(
-                default=1000, unit="", scale=1, step=1, precision=0, type="int"
-            ),
+            IntParam,
+            default=1000,
+            description="Number of random numbers to generate",
         )
 
+        self.time_taken = self.setattr_result("time_taken", FloatChannel)
+        self.flops = self.setattr_result("flops", FloatChannel)
+
     @rpc
-    def get_random_numbers(self) -> list[float]:
-        return np.random.rand(self.n).tolist()
+    def get_random_numbers(self, n) -> list[float]:
+        return np.random.rand(n).tolist()
 
     @kernel
-    def run(self):
-        output = [0.0] * self.n
+    def run_once(self):
+        n = self.n.get()
+        output = [0.0] * n
 
-        numbers_a = self.get_random_numbers()
-        numbers_b = self.get_random_numbers()
+        numbers_a = self.get_random_numbers(n)
+        numbers_b = self.get_random_numbers(n)
 
         # Do a non-trivial calculation on the code
         t_start_mu = self.core.get_rtio_counter_mu()
-        for i in range(self.n):
+        for i in range(n):
             output[i] = numbers_a[i] * numbers_b[i]
 
         t_end_mu = self.core.get_rtio_counter_mu()
 
+        time_taken = self.core.mu_to_seconds(t_end_mu - t_start_mu)
+
         logger.info(
             "Kernel time: %.3f us for %i FLOP",
-            (1e6 * self.core.mu_to_seconds(t_end_mu - t_start_mu)),
-            self.n,
+            1e6 * time_taken,
+            n,
         )
 
         logger.debug("Random numbers: %s", output)
+
+        self.time_taken.push(time_taken)
+        self.flops.push(float(n) / time_taken)
+
+
+KernelSpeedTest = make_fragment_scan_exp(KernelSpeedTestFrag)
