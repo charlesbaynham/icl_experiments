8f6e005916887948c4748c49e47ab2b1f97e1867
Charles Baynham <c.baynham@imperial.ac.uk>
2024-12-18 14:08:44 +0000

    Test on kernel

--- repository/tests/test_checkpoints.py
+++ repository/tests/test_checkpoints.py
@@ -2,9 +2,51 @@ from repository.lib.fragments.checkpoint_fragment import CheckpointFragment
 from ndscan.experiment import *
 
 
-# class # FIXME
+class DoInPostNarrowbandA(CheckpointFragment):
+    def build_fragment(self):
+        pass
+
+    @kernel
+    def post_narrowband_hook(self):
+        self.post_narrowband_hook_subfragments()
+        print("Hello from post_narrowband_hook, I am fragment A")
 
 
-# class TestCheckpoints(CheckpointFragment, ExpFragment):
-#     def build_fragment(self):
-#         sss
+class DoInPostNarrowbandB(CheckpointFragment):
+    def build_fragment(self):
+        pass
+
+    @kernel
+    def post_narrowband_hook(self):
+        self.post_narrowband_hook_subfragments()
+        print("Hello from post_narrowband_hook, I am fragment B")
+
+
+class DoInPostNarrowbandC(CheckpointFragment):
+    def build_fragment(self):
+        self.setattr_fragment("sub", DoInPostNarrowbandB)
+
+    @kernel
+    def post_narrowband_hook(self):
+        self.post_narrowband_hook_subfragments()
+        print("Hello from post_narrowband_hook, I am fragment C")
+
+
+class TestCheckpointsDefaultFrag(CheckpointFragment, ExpFragment):
+    def build_fragment(self):
+        self.setattr_fragment("subA", DoInPostNarrowbandA)
+        self.setattr_fragment("subC", DoInPostNarrowbandC)
+
+
+class TestCheckpointsOverriddenFrag(CheckpointFragment, ExpFragment):
+    def build_fragment(self):
+        self.setattr_fragment("subA", DoInPostNarrowbandA)
+        self.setattr_fragment("subC", DoInPostNarrowbandC)
+
+    def post_narrowband_hook(self):
+        self.post_narrowband_hook_subfragments()
+        print("Hello from post_narrowband_hook, I am TestCheckpointsOverridden")
+
+
+TestCheckpointsDefault = make_fragment_scan_exp(TestCheckpointsDefaultFrag)
+TestCheckpointsOverridden = make_fragment_scan_exp(TestCheckpointsOverriddenFrag)
