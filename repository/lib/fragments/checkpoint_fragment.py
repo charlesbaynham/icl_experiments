759689bbff7dd206fdf08887bcebddc1d8e1c60b
Charles Baynham <c.baynham@imperial.ac.uk>
2024-12-18 16:15:26 +0000

    Split

--- repository/lib/fragments/checkpoint_fragment.py
+++ repository/lib/fragments/checkpoint_fragment.py
@@ -17,8 +17,114 @@ class CheckpointFragment(Fragment):
     happens, all the subfragments' implementations of the checkpoint will be
     run.
 
-    For now, this directly defines the checkpoints we'll use in the AION code.
-    Later, if this proves useful, we can make this more general.
+    To use this you should subclass from this class and:
+
+    * Choose some checkpoint names and list them in `checkpoint_method_names`.
+    * Write default implementations for each of these. Probably just calling
+      `self.<your-hook-name>_subfragments()`.
+    * Optionally, write a stub method for
+      `self.<your-hook-name>_subfragments()`. This does nothing, but means that
+      users will get autocompletion in their IDE.
+
+    See :class:`~RedMOTExperimentCheckpoints` for an example.
+    """
+
+    checkpoint_method_names = None
+
+    @classmethod
+    def _checkpoint_is_trivial(cls, frag, checkpoint_name: str):
+        """
+        Check if the passed fragment or its children use this checkpoint
+
+        There are two ways that this checkpoint could be not used:
+
+        a) The (sub)fragment is a CheckpointFragment but which has not
+           implemented this checkpoint. The method will therefore exist but do
+           nothing.
+        b) The sub(fragment) is a plain Fragment and has no concept of this
+           Checkpoint. The method will not exist.
+
+        Anything other than these two scenarios == a non-trivial checkpoint
+        implementation.
+
+        This is written as a classmethod instead of a normal bound method so
+        that it can be called passing Fragments as well as CheckpointFragments.
+        """
+        assert not frag._building
+        we_have_trivial_checkpoint = not hasattr(frag, checkpoint_name) or getattr(
+            frag, checkpoint_name
+        ).__func__ is getattr(CheckpointFragment, checkpoint_name)
+
+        children_have_trivial_checkpoints = all(
+            [cls._checkpoint_is_trivial(f, checkpoint_name) for f in frag._subfragments]
+        )
+        return we_have_trivial_checkpoint and children_have_trivial_checkpoints
+
+    def build(self, *args, **kwargs):
+        super().build(*args, **kwargs)
+
+        if self.checkpoint_method_names is None:
+            raise TypeError(
+                f"You must define `checkpoint_method_names`. See the following docstring for this class:\n\n{CheckpointFragment.__doc__}"
+            )
+
+        for checkpoint_name in self.checkpoint_method_names:
+            # Check that we have a default implementation defined. This is done
+            # manually so that type checkers can read it: see above.
+            assert hasattr(
+                self, checkpoint_name
+            ), f"You must provide a default implementation of {checkpoint_name}"
+
+            # Check that there's a `*_subfragments`` method defined. This isn't
+            # essential, but means that type checkers and IDE type hints will
+            # work.
+            if not hasattr(self, f"{checkpoint_name}_subfragments"):
+                logger.warning(
+                    textwrap.dedent(
+                        """
+                        No %s_subfragments method statically defined
+
+                        This is not an error since this method will be defined
+                        dynamically, but means that type annotation and IDE type
+                        hints will not work. Consider adding one to help people use
+                        your checkpoint.
+                        """
+                    ).strip(),
+                    checkpoint_name,
+                )
+
+            # Build an implementation kernel that calls all this fragments'
+            # subfragments' checkpoints
+            code = ""
+            for s in self._subfragments:
+                if s in self._detached_subfragments:
+                    continue
+                if CheckpointFragment._checkpoint_is_trivial(
+                    s, checkpoint_name=checkpoint_name
+                ):
+                    continue
+                code += f"self.{s._fragment_path[-1]}.{checkpoint_name}()\n"
+
+            code += "pass\n"
+
+            implementation_kernel = kernel_from_string(["self"], code[:-1], portable)
+            implementation_kernel_name = f"{checkpoint_name}_subfragments"
+
+            # Overwrite the "*_subfragment" stub defined above with this
+            # implementation, binding it to the instance (see
+            # https://docs.python.org/3/howto/descriptor.html#functions-and-methods
+            # for an interesting look at how python treats class methods vs.
+            # functions)
+            setattr(
+                self,
+                implementation_kernel_name,
+                implementation_kernel.__get__(self),
+            )
+
+
+class RedMOTExperimentCheckpoints(CheckpointFragment):
+    """
+    Checkpoints for the RedMOTExperiment experiment template
     """
 
     checkpoint_method_names = [
@@ -30,8 +136,6 @@ class CheckpointFragment(Fragment):
         "post_sequence_cleanup_hook",
         "after_data_saved_checkpoint",
     ]
-
-    # %% Begin type annotations
 
     # Default implementations of checkpoints. These could be dynamically
     # generated easily, but we write them out manually so that type checkers can
@@ -98,90 +202,3 @@ class CheckpointFragment(Fragment):
     @portable
     def after_data_saved_checkpoint_subfragments(self):
         pass
-
-    # %% End type annotations
-
-    @classmethod
-    def _checkpoint_is_trivial(cls, frag, checkpoint_name: str):
-        """
-        Check if the passed fragment or its children use this checkpoint
-
-        There are two ways that this checkpoint could be not used:
-
-        a) The (sub)fragment is a CheckpointFragment but which has not
-           implemented this checkpoint. The method will therefore exist but do
-           nothing.
-        b) The sub(fragment) is a plain Fragment and has no concept of this
-           Checkpoint. The method will not exist.
-
-        Anything other than these two scenarios == a non-trivial checkpoint
-        implementation.
-
-        This is written as a classmethod instead of a normal bound method so
-        that it can be called passing Fragments as well as CheckpointFragments.
-        """
-        assert not frag._building
-        we_have_trivial_checkpoint = not hasattr(frag, checkpoint_name) or getattr(
-            frag, checkpoint_name
-        ).__func__ is getattr(CheckpointFragment, checkpoint_name)
-
-        children_have_trivial_checkpoints = all(
-            [cls._checkpoint_is_trivial(f, checkpoint_name) for f in frag._subfragments]
-        )
-        return we_have_trivial_checkpoint and children_have_trivial_checkpoints
-
-    def build(self, *args, **kwargs):
-        super().build(*args, **kwargs)
-
-        for checkpoint_name in self.checkpoint_method_names:
-            # Check that we have a default implementation defined. This is done
-            # manually so that type checkers can read it: see above.
-            assert hasattr(
-                self, checkpoint_name
-            ), f"You must provide a default implementation of {checkpoint_name}"
-
-            # Check that there's a `*_subfragments`` method defined. This isn't
-            # essential, but means that type checkers and IDE type hints will
-            # work.
-            if not hasattr(self, f"{checkpoint_name}_subfragments"):
-                logger.warning(
-                    textwrap.dedent(
-                        """
-                        No %s_subfragments method statically defined
-
-                        This is not an error since this method will be defined
-                        dynamically, but means that type annotation and IDE type
-                        hints will not work. Consider adding one to help people use
-                        your checkpoint.
-                        """
-                    ).strip(),
-                    checkpoint_name,
-                )
-
-            # Build an implementation kernel that calls all this fragments'
-            # subfragments' checkpoints
-            code = ""
-            for s in self._subfragments:
-                if s in self._detached_subfragments:
-                    continue
-                if CheckpointFragment._checkpoint_is_trivial(
-                    s, checkpoint_name=checkpoint_name
-                ):
-                    continue
-                code += f"self.{s._fragment_path[-1]}.{checkpoint_name}()\n"
-
-            code += "pass\n"
-
-            implementation_kernel = kernel_from_string(["self"], code[:-1], portable)
-            implementation_kernel_name = f"{checkpoint_name}_subfragments"
-
-            # Overwrite the "*_subfragment" stub defined above with this
-            # implementation, binding it to the instance (see
-            # https://docs.python.org/3/howto/descriptor.html#functions-and-methods
-            # for an interesting look at how python treats class methods vs.
-            # functions)
-            setattr(
-                self,
-                implementation_kernel_name,
-                implementation_kernel.__get__(self),
-            )
