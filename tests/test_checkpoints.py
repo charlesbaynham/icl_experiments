1bcc19117f752b27d30e609037b2ea3e233e77b6
Charles Baynham <c.baynham@imperial.ac.uk>
2024-12-18 12:13:10 +0000

    Two levels

--- tests/test_checkpoints.py
+++ tests/test_checkpoints.py
@@ -14,7 +14,7 @@ class TestCheckpointExpFragBase(CheckpointFragment, ExpFragment):
 
 def test_can_run_with_default_implementations(fragment_precompiler):
     frag = fragment_precompiler(TestCheckpointExpFragBase)
-    result = run_fragment_once(frag)
+    run_fragment_once(frag)
 
 
 def test_default_implementations_call_subfragments(fragment_precompiler):
@@ -39,5 +39,34 @@ def test_default_implementations_call_subfragments(fragment_precompiler):
 
     frag = fragment_precompiler(TestCheckpointExpFrag)
     assert not herald
-    result = run_fragment_once(frag)
+    run_fragment_once(frag)
+    assert herald
+
+
+def test_default_implementations_call_subfragments_squared(fragment_precompiler):
+    herald = False
+
+    class Bottom(CheckpointFragment):
+        def build_fragment(self):
+            pass
+
+        def test_checkpoint(self):
+            nonlocal herald
+            herald = True
+
+            self.test_checkpoint_subfragments()
+
+    class Middle(CheckpointFragment):
+        def build_fragment(self):
+            self.setattr_fragment("subfrag", Bottom)
+
+    class Top(TestCheckpointExpFragBase):
+        def build_fragment(self):
+            super().build_fragment()
+
+            self.setattr_fragment("subfrag", Middle)
+
+    frag = fragment_precompiler(Top)
+    assert not herald
+    run_fragment_once(frag)
     assert herald
