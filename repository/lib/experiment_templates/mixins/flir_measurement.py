222744e17f0200f208c1cb0038e2ca3ba442b692
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-01-29 17:48:53 +0000

    Revert "this was not alice's fault"
    This reverts commit 3b7e908c18d4c33d7cd87d0d746c39e8103d8d88.

--- repository/lib/experiment_templates/mixins/flir_measurement.py
+++ repository/lib/experiment_templates/mixins/flir_measurement.py
@@ -24,7 +24,6 @@ class FLIRMeasurementMixin(RedMOTWithExperiment):
     """
 
     def build_fragment(self):
-        super().build_fragment()
 
         self.setattr_fragment(
             "camera_interface", DualCameraMeasurement, hardware_trigger=True
