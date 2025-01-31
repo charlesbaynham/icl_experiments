6cdaf0c9927dd8aabf20afe0410fc7a8cdff1b6f
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-01-31 16:55:11 +0000

    go back to previous small ROIs from the DAI data

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -336,8 +336,8 @@ else:
     ANDOR_ROI_Y1 = y + height / 2
 
 ANDOR_ROI_DIPOLE_HEIGHT_ABOVE = 10
-ANDOR_ROI_DIPOLE_HEIGHT_BELOW = 40
-ANDOR_ROI_DIPOLE_WIDTH = 50
+ANDOR_ROI_DIPOLE_HEIGHT_BELOW = 10
+ANDOR_ROI_DIPOLE_WIDTH = 16
 
 ANDOR_DIPOLE_TRAP_BACKWARD_X = 184
 # ~3 pixels below the center of the dipole trap to include falling atoms
