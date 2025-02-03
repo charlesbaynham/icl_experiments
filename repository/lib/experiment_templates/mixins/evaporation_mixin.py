748a05f8760093755cb72712d0bd73f8c5a097af
Alice Josset <acj320@ic.ac.uk>
2025-02-03 18:12:47 +0000

    evaporation experiment with linear ramping

---
+++ repository/lib/experiment_templates/mixins/evaporation_mixin.py
@@ -0,0 +1,41 @@
+import logging
+
+from artiq.experiment import kernel
+from ndscan.experiment.entry_point import make_fragment_scan_exp
+from ndscan.experiment.parameters import FloatParam
+from ndscan.experiment.parameters import FloatParamHandle
+import repository.lib.constants as constants
+
+from repository.lib.fragments.dipole_trap.dipole_trap_phases import (
+    XODTWithLinearRamp
+)
+from repository.lib.experiment_templates.dipole_trap_experiment import (
+    DipoleTrapWithExperiment,
+)
+
+
+class EvaporationMixin(DipoleTrapWithExperiment):
+    def build_fragment(self):
+        super().build_fragment()
+
+        self.setattr_fragment(
+            "linear_evap_ramp", XODTWithLinearRamp, enforce_binding_to_defaults=False
+        )
+        self.linear_evap_ramp: XODTWithLinearRamp
+
+        self.linear_evap_ramp.bind_suservo_setpoint_params_to_default_beam_setter(
+            [self.dipole_beam_controller.all_beam_default_setter]
+        )
+
+        #self.linear_evap_ramp.daisy_chain_with_previous_phase(self.narrow_red_compression_phase, suservos= suservos_XODT)
+        self.linear_evap_ramp.default_suservo_setpoint_multiples_start = (
+            constants.XODT_EVAP_START
+        )
+        self.linear_evap_ramp.default_suservo_setpoint_multiples_end = (
+            constants.XODT_EVAP_END
+        )
+        
+
+    @kernel
+    def dipole_trap_evaporation_hook(self):
+        self.linear_evap_ramp.do_phase()
--- repository/lib/fragments/dipole_trap/dipole_trap_phases.py
+++ repository/lib/fragments/dipole_trap/dipole_trap_phases.py
@@ -6,6 +6,9 @@ import repository.lib.constants as constants
 from repository.lib.fragments.ramping_phase_bound import (
     GeneralRampingPhaseWithBindingAndBiasField,
 )
+from repository.lib.fragments.ramping_phase_bound import (
+    GeneralRampingPhaseWithBinding
+)
 
 suservos_molasses = [
     "suservo_aom_singlepass_689_red_mot_sigmaplus",
@@ -118,3 +121,22 @@ class XODTWithFieldRamp(GeneralRampingPhaseWithBindingAndBiasField):
     # Chamber 2 bias coils in amps
     general_setter_default_starts = constants.XODT_EVAP_AND_FIELD_RAMP_FIELD_START
     general_setter_default_ends = constants.XODT_EVAP_AND_FIELD_RAMP_FIELD_END
+
+class XODTWithLinearRamp(GeneralRampingPhaseWithBinding):
+    """
+    A phase with linear ramps for 1064 and 813 nm XODT
+    """
+    
+    duration_default  = 150e-3
+    time_step_default = 1e-3
+
+    suservos = suservos_XODT
+
+    # self.bind_suservo_setpoint_params_to_default_beam_setter for this.
+    default_suservo_nominal_setpoints = [0.0] * len(suservos_XODT)
+    # The start setpoints must be overridden by daisy-chaining to previous phase
+    default_suservo_setpoint_multiples_start = [0] * len(suservos_XODT)
+    default_suservo_setpoint_multiples_end = (
+        constants.XODT_EVAP_AND_FIELD_RAMP_SUSERVOS_END
+    )
+
