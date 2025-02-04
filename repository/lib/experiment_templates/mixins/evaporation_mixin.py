4fc25e4e98e6e8bb245d3643b885f27a420bd44a
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-02-04 15:13:21 +0000

    Revert "test"
    This reverts commit 271427526fd88d1a42602093b77b196ff8949e0f.

--- repository/lib/experiment_templates/mixins/evaporation_mixin.py
+++ repository/lib/experiment_templates/mixins/evaporation_mixin.py
@@ -6,7 +6,9 @@ from ndscan.experiment.parameters import FloatParam
 from ndscan.experiment.parameters import FloatParamHandle
 import repository.lib.constants as constants
 
-from repository.lib.fragments.dipole_trap.dipole_trap_phases import XODTWithLinearRamp
+from repository.lib.fragments.dipole_trap.dipole_trap_phases import (
+    XODTWithLinearRamp
+)
 from repository.lib.experiment_templates.dipole_trap_experiment import (
     DipoleTrapWithExperiment,
 )
@@ -21,17 +23,18 @@ class EvaporationMixin(DipoleTrapWithExperiment):
         )
         self.linear_evap_ramp: XODTWithLinearRamp
 
-        # self.linear_evap_ramp.bind_suservo_setpoint_params_to_default_beam_setter(
-        #   [self.dipole_beam_controller.all_beam_default_setter]
-        # )
+        self.linear_evap_ramp.bind_suservo_setpoint_params_to_default_beam_setter(
+            [self.dipole_beam_controller.all_beam_default_setter]
+        )
 
-        # self.linear_evap_ramp.daisy_chain_with_previous_phase(self.narrow_red_compression_phase, suservos= suservos_XODT)
+        #self.linear_evap_ramp.daisy_chain_with_previous_phase(self.narrow_red_compression_phase, suservos= suservos_XODT)
         self.linear_evap_ramp.default_suservo_setpoint_multiples_start = (
             constants.XODT_EVAP_START
         )
         self.linear_evap_ramp.default_suservo_setpoint_multiples_end = (
             constants.XODT_EVAP_END
         )
+        
 
     @kernel
     def dipole_trap_evaporation_hook(self):
