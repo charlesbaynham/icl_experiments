e77e8799a5e09931ad14f2b914da49c9f1ecec9e
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-02-14 14:35:08 +0000

    test fastino control exp

---
+++ repository/tests/test_fastino.py
@@ -0,0 +1,37 @@
+import logging
+
+from artiq.coredevice.core import Core
+from artiq.coredevice.fastino import Fastino
+from ndscan.experiment import ExpFragment
+from ndscan.experiment.entry_point import make_fragment_scan_exp
+from ndscan.experiment.parameters import FloatParam
+from ndscan.experiment.parameters import FloatParamHandle
+from ndscan.experiment.parameters import IntParam
+from ndscan.experiment.parameters import IntParamHandle
+
+
+class FastinoControlFrag(ExpFragment):
+    def build_fragment(self):
+        self.setattr_device("core")
+        self.core: Core
+        self.setattr_device("fastino")
+        self.fastino: Fastino
+        self.setattr_param(
+            "voltage", FloatParam, description="Voltage", default=0.0, unit="V"
+        )
+        self.voltage: FloatParamHandle
+
+        self.setattr_param("channel", IntParam, description="Channel", default=0)
+        self.channel: IntParamHandle
+
+    def device_setup(self):
+        self.fastino.init()
+        self.device_setup_subfragments()
+
+    def run_once(self):
+        voltage_value = self.voltage.get()
+        self.core.break_realtime()
+        self.fastino.set_dac(0, voltage_value)
+
+
+FastinoControl = make_fragment_scan_exp(FastinoControlFrag)
