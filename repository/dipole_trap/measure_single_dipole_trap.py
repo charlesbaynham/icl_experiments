4b3272cc74edf139d1ee991bbdec56a34d4609e7
alice <alice.josset20@imperial.ac.uk>
2025-02-27 17:02:27 +0000

    molasses stage in single xodt

--- repository/dipole_trap/measure_single_dipole_trap.py
+++ repository/dipole_trap/measure_single_dipole_trap.py
@@ -3,6 +3,8 @@ import logging
 from artiq.experiment import kernel
 from ndscan.experiment.entry_point import make_fragment_scan_exp
 
+from repository.lib import constants
+
 from repository.lib.experiment_templates.mixins.andor_imaging.absorption_imaging import (
     AbsorptionDipoleTrapMixin,
 )
@@ -22,7 +24,7 @@ from repository.lib.experiment_templates.dipole_trap_experiment import (
     DipoleTrapWithExperiment,
 )
 from repository.lib.experiment_templates.mixins.XODT_molasses import (
-    XODTSingleMolassesMixin,
+    XODTSingleMolassesMixinBase,
 )
 
 logger = logging.getLogger(__name__)
@@ -31,11 +33,20 @@ logger = logging.getLogger(__name__)
 class MeasureSingleXODTBGCorrectedFrag(
     FLIRMeasurementMixin,
     BGCorrectedAndorImage,
-    XODTSingleMolassesMixin,
+    XODTSingleMolassesMixinBase,
 ):
     """
     Make Single XODT, image twice for BG subtraction
     """
+    def build_fragment(self):
+        super().build_fragment()   
+
+        self.setattr_param_rebind(
+            "delay_molasses",
+            self.delay_before_molasses,
+            original_name="delay_before_molasses",
+            default= 0.01e-3,
+        )    
 
     @kernel
     def do_experiment_after_dipole_trap_hook(self):
@@ -43,11 +54,20 @@ class MeasureSingleXODTBGCorrectedFrag(
 
 class MeasureSingleXODTAbsFrag(
     AbsorptionDipoleTrapMixin,
-    XODTSingleMolassesMixin,
+    XODTSingleMolassesMixinBase,
 ):
     """
     Measure a single XODT with absorption imaging
     """
+    def build_fragment(self):
+        super().build_fragment()   
+
+        self.setattr_param_rebind(
+            "delay_molasses",
+            self.delay_before_molasses,
+            original_name="delay_before_molasses",
+            default= 0.01e-3,
+        )  
 
     @kernel
     def do_experiment_after_dipole_trap_hook(self):
