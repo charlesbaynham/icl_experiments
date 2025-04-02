cd9dcf494dbe72db6dbc594a278fc9c7d33f6dc0
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-04-02 10:25:12 +0100

    Output a sum

--- repository/examples/demonstrate_fragments.py
+++ repository/examples/demonstrate_fragments.py
@@ -1,5 +1,5 @@
 from ndscan.experiment import ExpFragment, make_fragment_scan_exp
-from ndscan.experiment import FloatParam
+from ndscan.experiment import FloatParam, FloatChannel
 from ndscan.experiment.parameters import FloatParamHandle
 
 class Demo1(ExpFragment):
@@ -7,9 +7,15 @@ class Demo1(ExpFragment):
         self.setattr_param("num1", FloatParam, default=1.0, description="Number 1")
         self.num1 : FloatParamHandle
 
-    def run_once(self):
-        pass
+        self.setattr_param("num2", FloatParam, default=1.0, description="Number 2")
+        self.num2 : FloatParamHandle
 
+        self.setattr_result("sum", FloatChannel)
+        self.sum: FloatChannel
+
+    def run_once(self):
+        the_sum = self.num1.get() + self.num2.get()
+        print(f"Sum: {the_sum}")
 class Demo2(ExpFragment):
     def build_fragment(self, *args, **kwargs):
         pass
