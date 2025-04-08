a82f23e2d9a56d995226484d9e65e6ca62f5087f
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-04-08 16:12:07 +0100

    error in import

--- repository/tests/dds_amplitude_test.py
+++ repository/tests/dds_amplitude_test.py
@@ -1,4 +1,5 @@
 from ndscan.experiment.entry_point import make_fragment_scan_exp
-from utilities.set_suservo_static import AmplitudeTest
+
+from repository.utilities.set_suservo_static import AmplitudeTest
 
 AmplitudeTestExp = make_fragment_scan_exp(AmplitudeTest)
