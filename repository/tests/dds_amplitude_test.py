f07c906f2299953a7df886160aae8967d5f1cde2
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-04-08 15:58:43 +0100

    test to print dds amplitude

---
+++ repository/tests/dds_amplitude_test.py
@@ -0,0 +1,4 @@
+from ndscan.experiment.entry_point import make_fragment_scan_exp
+from utilities.set_suservo_static import AmplitudeTest
+
+AmplitudeTestExp = make_fragment_scan_exp(AmplitudeTest)
--- repository/utilities/set_suservo_static.py
+++ repository/utilities/set_suservo_static.py
@@ -1,1 +1,24 @@
+from artiq.coredevice.ad9910 import AD9910
+from pyaion.utilities.set_suservo_static import SetSUServoStatic
 from pyaion.utilities.set_suservo_static import SetSUServoStaticExp  # noqa
+
+
+class AmplitudeTest(SetSUServoStatic):
+    """Print the amplitude tuning word"""
+
+    @kernel
+    def run_once(self):
+        self.LibSetSUServoStatic.set_suservo(
+            self.frequency.get(),
+            self.amplitude.get(),
+            self.attenuation.get(),
+            self.rf_switch.get(),
+            self.setpoint_v.get(),
+            self.enable_iir.get(),
+        )
+
+        self.LibSetSUServoStatic.set_pgia_gain_mu(self.pgia_setting.get())
+
+        channel = self.LibSetSUServoStatic.suservo_channel
+        dds: AD9910 = self.LibSetSUServoStatic.suservo.ddses[channel[-1]]
+        print(dds.get_asf())
