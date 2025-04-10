92da58ca2ab8ff81786ce977483597a368825c5d
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-04-10 11:27:16 +0100

    Correct WAND setpoint for the clock laser

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -675,6 +675,7 @@ _default_461 = (
 _default_707 = 423_913_478e6
 _default_679 = 441_332_627e6
 _default_698 = 429_228_387.3e6  # Measured empirically
+_clock_laser_offset = -80e6
 
 # Calibrated empirically - I know it's not right but we seem to optimize here
 # for some reason
@@ -769,7 +770,7 @@ WAND_SETPOINTS_88 = {
     ),
     # "689_doubled1379": (_default_689, False),
     "698": (_default_698, False),
-    "Sirah": (_default_698, False),
+    "Sirah": (_default_698 + _clock_laser_offset, False),
 }
 
 
@@ -786,7 +787,7 @@ WAND_SETPOINTS_87 = {
     ),
     # "689_doubled1379": (_default_689, False),
     "698": (_default_698, False),
-    "Sirah": (_default_698, False),
+    "Sirah": (_default_698 + _clock_laser_offset, False),
 }
 
 TOPTICA_461_ANALOG_SCALE = 210e6 / (3.05)  # MHz/V # rough value # arc factor 0.15 V/V
