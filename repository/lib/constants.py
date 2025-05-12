035c99c2d6e1a4b2d368802af34e38ecb06567db
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-05-12 10:20:52 +0100

    Saving alice's work

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -517,7 +517,7 @@ SUSERVOED_BEAMS = [
         80e6,
         20,
         "suservo_aom_singlepass_487_transparency",
-        setpoint=0.4,
+        setpoint=0.1,
         servo_enabled=True,
     ),
     ### RED ###
