56d921eb2465af9952ffcb44c7d4d5e161a42b45
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-05-21 14:12:28 +0100

    Reinstate molasses

--- repository/dipole_trap/measure_single_dipole_trap.py
+++ repository/dipole_trap/measure_single_dipole_trap.py
@@ -35,7 +35,7 @@ class MeasureSingleXODTBGCorrectedFrag(
     FLIRMeasurementMixin,
     BGCorrectedAndorImageSingleXODT,
     LoadSingleXODTMixin,
-    # XODTSingleMolassesMixin, # FIXME
+    XODTSingleMolassesMixin,
 ):
     """
     Make Single XODT, image twice for BG subtraction
@@ -46,7 +46,7 @@ class MeasureSingleXODTBGCorrectedFrag(
         self.DMA_initialization_hook_default()
         # self.DMA_initialization_hook_linear_evap()
         self.DMA_initialization_hook_single_xodt_mot()
-        # self.DMA_initialization_hook_xodt_molasses() FIXME
+        self.DMA_initialization_hook_xodt_molasses()
 
     @kernel
     def do_experiment_after_dipole_trap_hook(self):
