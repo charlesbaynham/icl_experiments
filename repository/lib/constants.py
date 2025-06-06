9c9b3afab14a80d1696f62e0495ac2408405b924
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-06-06 17:25:45 +0100

    Moar power

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -80,7 +80,7 @@ URUKULED_BEAMS = [
     UrukuledBeam(
         name="clock_up",
         frequency=200e6,
-        attenuation=10.0,
+        attenuation=0.0,
         urukul_device="urukul9910_aom_698_up_switch",
     ),
     UrukuledBeam(
