db05ab446b9cf56cd68a5b23e7ec58fd8d6ba237
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-07-02 17:10:45 +0100

    Oops

--- flake.nix
+++ flake.nix
@@ -235,7 +235,7 @@
 
             # Automatic startup of database monitors
             monitor_launcher =
-              "sleep 200 && artiq_client submit -p monitors -P -10 -R --flush -c MonitorMaster repository/monitors/monitor_master.py && sleep infinity";
+              "sleep 200 && artiq_client -s 10.137.1.252 submit -p monitors -P -10 -R --flush -c MonitorMaster repository/monitors/monitor_master.py && sleep infinity";
 
           in overriddenOutputs.apps.full_stack.override (prev:
             {
