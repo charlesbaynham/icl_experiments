71de0d0b3497b4f325091d4cd56fa853cd3c5338
Charles Baynham <c.baynham@imperial.ac.uk>
2025-07-03 13:55:39 +0100

    Use variable

--- flake.nix
+++ flake.nix
@@ -236,7 +236,7 @@
             # Automatic startup of database monitors
             # FIXME I think this is broken now
             # monitor_launcher =
-            #   "sleep 200 && artiq_client -s 10.137.1.252 submit -p monitors -P -10 -R --flush -c MonitorMaster repository/monitors/monitor_master.py && sleep infinity";
+            #    "sleep 200 && artiq_client -s ${bind_settings.connection_ip} submit -p monitors -P -10 -R --flush -c MonitorMaster repository/monitors/monitor_master.py && sleep infinity";
 
           in overriddenOutputs.apps.full_stack.override (prev:
             {
