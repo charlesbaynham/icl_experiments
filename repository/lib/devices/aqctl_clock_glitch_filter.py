cd1e1c4ccca550765910eb29d58da73538937d6a
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-07-09 12:27:52 +0100

    Typo

--- repository/lib/devices/aqctl_clock_glitch_filter.py
+++ repository/lib/devices/aqctl_clock_glitch_filter.py
@@ -2,7 +2,7 @@ from generic_scpi_driver import get_controller_func
 
 from repository.lib.devices.clock_glitch_filter import ClockGlitchFilter
 
-main = get_controller_func("RelockerDriver", 8888, ClockGlitchFilter)
+main = get_controller_func("ClockGlitchFilter", 8888, ClockGlitchFilter)
 
 
 if __name__ == "__main__":
