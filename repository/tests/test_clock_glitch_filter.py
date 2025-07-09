6e35eeaa07cb333c906d20fb289f6c8cb89c8f44
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-07-09 11:32:26 +0100

    Add test for clock glitch filter

---
+++ repository/tests/test_clock_glitch_filter.py
@@ -0,0 +1,46 @@
+from artiq.coredevice.core import Core
+from artiq.experiment import rpc
+from artiq.language import delay
+from artiq.language import kernel
+from artiq.language import now_mu
+from ndscan.experiment import Fragment
+from ndscan.experiment import make_fragment_scan_exp
+
+from repository.lib.experiment_templates.mixins.clock_glitch_counting import (
+    ClockGlitchFilterFrag,
+)
+
+
+class TestClockGlitchFilter(Fragment):
+
+    def build_fragment(self):
+        self.setattr_device("core")
+        self.core: Core
+
+        self.setattr_fragment("clock_glitch_filter", ClockGlitchFilterFrag)
+        self.clock_glitch_filter: ClockGlitchFilterFrag
+
+    @kernel
+    def run_once(self):
+        self.count_glitches()
+
+        self.core.break_realtime()
+        self.core.wait_until_mu(now_mu())
+
+        self.clock_glitch_filter.start_counting_glitches()
+
+        delay(2.0)
+        self.core.wait_until_mu(now_mu())
+
+        self.clock_glitch_filter.stop_counting_glitches()
+        self.count_glitches()
+
+    @rpc(flags={"async"})
+    def count_glitches(self):
+        num_glitches = self.clock_glitch_filter.get_num_glitches()
+        print(f"Number of glitches counted: {num_glitches}")
+
+
+TestClockGlitchFilterExp = make_fragment_scan_exp(
+    TestClockGlitchFilter,
+)
