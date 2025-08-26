f918d3e7515d39219ec9c12b41a2d88cc051da58
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-08-26 19:36:59 +0100

    test

--- repository/tests/test_clock_opll_ramper.py
+++ repository/tests/test_clock_opll_ramper.py
@@ -42,7 +42,7 @@ class TestClockRamper(ExpFragment):
 
         end_time = now_mu()
 
-        new_freq = 1e6 * self.core.mu_to_seconds(end_time - start_time + 8e-9)
+        new_freq = 1e6 * self.core.mu_to_seconds(end_time - start_time + 1)
 
         self.clock_opll.clock_OPLL_offset.set(80e6 + new_freq)
 
