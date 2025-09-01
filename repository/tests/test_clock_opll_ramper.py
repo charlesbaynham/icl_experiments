5966c66c4d91656037b6c7b2df931b8bacc54732
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-09-01 16:11:45 +0100

    debug

--- repository/tests/test_clock_opll_ramper.py
+++ repository/tests/test_clock_opll_ramper.py
@@ -119,10 +119,6 @@ class TestLaunchFromXODTFrag(
         self.post_sequence_cleanup_hook_andor()
         self.post_sequence_cleanup_hook_shelving()
 
-    @kernel
-    def do_experiment_after_dipole_trap_hook(self):
-        pass
-
 
 TestClockRamperExp = make_fragment_scan_exp(TestClockRamper)
 TestLaunchFromXODTFExp = make_fragment_scan_exp(TestLaunchFromXODTFrag)
