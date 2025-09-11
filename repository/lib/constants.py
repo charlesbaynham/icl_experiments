1f60934c07b9c9363ae02d5dec329f70d258dbf7
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-09-11 14:48:04 +0100

    Update clock freq

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -674,7 +674,7 @@ SUSERVOED_BEAMS = [
     ),
     SUServoedBeam(
         "clock_delivery",
-        99.9410e6,
+        99.91e6,
         9,
         "suservo_aom_698_clock_delivery",
         servo_enabled=True,
