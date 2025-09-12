e68098aac6e9e201d1c782b2f7f680e662d739f0
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2025-09-12 17:26:59 +0100

    Revert "Test increasing width"
    This reverts commit ebae819a96ed98aa660d5d0ae0c6b10c32e1e643.

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -384,10 +384,10 @@ if USE_LATTICE_MODE:
 
 else:
     if USE_SR87:
-        x, y, width, height = 215, 273, 300, 100  # FIXME Put this back to 100 width
+        x, y, width, height = 215, 273, 100, 100
 
     else:
-        x, y, width, height = 215, 216, 300, 100  # FIXME Put this back to 100 width
+        x, y, width, height = 215, 216, 100, 100
 
     ANDOR_ROI_X0 = x - width / 2
     ANDOR_ROI_X1 = x + width / 2
