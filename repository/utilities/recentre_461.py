73c672efbec66ae159638ff8e775fe78cb3481b0
StrontiumLab <stronlab@ph-cb2409.ic.ac.uk>
2025-10-06 17:48:47 +0100

    Add 461 recentre utility

---
+++ repository/utilities/recentre_461.py
@@ -0,0 +1,55 @@
+import logging
+
+from ndscan.experiment import ExpFragment
+from ndscan.experiment import make_fragment_scan_exp
+from toptica_wrapper.driver import TopticaDLCPro
+
+from repository.lib.fragments.wand_steering import WandSteering
+
+logger = logging.getLogger(__name__)
+
+
+class Recentre461Frag(ExpFragment):
+    """
+    Recentre the 461 for the transfer cavity lock
+    """
+
+    def build_fragment(self):
+        self.set_default_scheduling(pipeline_name="wand")
+
+        self.setattr_fragment("wand_steering", WandSteering)
+        self.wand_steering: WandSteering
+
+        self.toptica_461: TopticaDLCPro = self.get_device("toptica_461")
+        self.raw_dlcpro = self.toptica_461.get_dlcpro()
+
+    def host_setup(self):
+        super().host_setup()
+
+        # Open a connection
+        self.raw_dlcpro.open()
+        self.laser = self.dlcpro.get_laser()
+
+    def run_once(self):
+
+        # Open a connection
+        self.raw_dlcpro.open()
+        self.laser = self.dlcpro.get_laser()
+
+        # Disable ARC
+        self.laser.dl.pc.external_input.set(False)
+
+        # Steer the 461 back to its nominal setpoint
+        self.wand_steering.steer_wand(
+            self.laser,
+            offset=0,
+            timeout=20.0,
+            required_accuracy=2e6,
+            leave_locked=False,
+        )
+
+        # Reenable ARC
+        self.laser.dl.pc.external_input.set(True)
+
+
+Recentre461 = make_fragment_scan_exp(Recentre461Frag)
