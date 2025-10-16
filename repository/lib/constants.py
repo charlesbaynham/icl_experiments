0fcc2381e394b22a6a2ee86a3143a5527fc3e375
Ultracold Strontium Laboratory <stronlab@imperial.ac.uk>
2025-10-16 19:22:26 +0100

    Tweak detuning

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -1088,7 +1088,7 @@ CLOCK_DELIVERY_PREEMPT_TIME = 200e-6
 DELAY_AFTER_CLOCK_SPECTROSCOPY = 250e-6
 DELAY_BETWEEN_INTERFEROMETRY_PULSES = 100e-6
 CLOCK_DELIVERY_SPECTROSCOPY_DETUNING = (
-    34.7e3  # Will need fine-tuning whenever velocity-selection pulse is changed
+    35.3e3  # Will need fine-tuning whenever velocity-selection pulse is changed
 )
 DURATION_OF_STARK_PULSE = 30e-6
 
