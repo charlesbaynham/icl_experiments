a82d9ec076d7ba5a0bde3f9b4cb3dc74223ba253
Charles Baynham <c.baynham@imperial.ac.uk>
2025-10-28 14:20:45 +0000

    Add mixin

--- repository/lib/experiment_templates/mixins/cavity_relocking.py
+++ repository/lib/experiment_templates/mixins/cavity_relocking.py
@@ -1,48 +1,50 @@
-# import logging
-# import logging
-# import time
+import logging
 
-# from artiq.master.scheduler import Scheduler
-# from ndscan.experiment import Fragment
-# from wand.server import ControlInterface as WANDControlInterface
-# from wand.tools import WLMMeasurementStatus
-# from artiq.coredevice.ad9910 import AD9910
-# from artiq.coredevice.core import Core
-# from artiq.language import at_mu
-# from artiq.language import delay
-# from artiq.language import delay_mu
-# from artiq.language import kernel
-# from artiq.language import now_mu
-# from ndscan.experiment import Fragment
-# from ndscan.experiment.parameters import FloatParam
-# from ndscan.experiment.parameters import FloatParamHandle
-# from numpy import int64
-# from pyaion.fragments.default_beam_setter import SetBeamsToDefaults
-# from pyaion.fragments.default_beam_setter import make_set_beams_to_default
-# from pyaion.fragments.suservo import LibSetSUServoStatic
-# from pyaion.models import SUServoedBeam
-# from pyaion.models import UrukuledBeam
+from artiq.language import kernel
+from ndscan.experiment import Fragment
 
-# from repository.lib import constants
-# from repository.lib.experiment_templates.dipole_trap_experiment import (
-#     DipoleTrapWithExperiment,
-# )
-# from repository.lib.experiment_templates.red_mot_experiment import RedMOTWithExperiment
-# from repository.lib.fragments
+from repository.lib.experiment_templates.red_mot_experiment import RedMOTWithExperiment
+from repository.lib.fragments.relock_689_and_698 import Relock689Frag
+from repository.lib.fragments.relock_689_and_698 import Relock698Frag
 
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
-# class MonitorAndRelock689and698Mixin(RedMOTWithExperiment):
-#     """
-#     Mixin to monitor the 689 and 698 cavity locks and relock them if required
+class MonitorAndRelock689and698Mixin(RedMOTWithExperiment):
+    """
+    Mixin to monitor the 689 and 698 cavity locks and relock them if required
 
-#     Kernel hooks used (multiple mixins cannot use the same hooks):
+    Kernel hooks used (multiple mixins cannot use the same hooks):
 
-#     * None
-#     """
+    * None
+    """
 
-#     def build_fragment(self):
-#         super().build_fragment()
+    def build_fragment(self):
+        super().build_fragment()
 
-#         self.setattr_fragment
+        class MonitorLocksInDeviceSetup(Fragment):
+            def build_fragment(self, outer_self):
+                self.outer_self: MonitorAndRelock689and698Mixin = outer_self
+
+                self.setattr_fragment("relock_689_frag", Relock689Frag)
+                self.relock_689_frag: Relock689Frag
+
+                self.setattr_fragment("relock_698_frag", Relock698Frag)
+                self.relock_698_frag: Relock698Frag
+
+            @kernel
+            def device_setup(self):
+                self.device_setup_subfragments()
+
+                if not self.relock_689_frag.is_cavity_locked():
+                    logger.warning("689 cavity unlocked, attempting relock")
+                    self.relock_689_frag.relock()
+
+                if not self.relock_698_frag.is_cavity_locked():
+                    logger.warning("698 cavity unlocked, attempting relock")
+                    self.relock_698_frag.relock()
+
+        self.setattr_fragment(
+            "monitor_locks_in_device_setup", MonitorLocksInDeviceSetup
+        )
+        self.monitor_locks_in_device_setup: MonitorLocksInDeviceSetup
