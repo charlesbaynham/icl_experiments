58d90ce0680aeb43c74d053677b5f52501d6aa4e
Charles Baynham <c.baynham@imperial.ac.uk>
2025-10-29 17:28:37 +0000

    Change freq

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -56,7 +56,7 @@ USE_LATTICE_MODE = False
 URUKULED_BEAMS = [
     UrukuledBeam(
         name="red_doublepass_injection",
-        frequency=365.0e6,
+        frequency=364.937e6,
         amplitude=1.0,
         attenuation=0.0,
         urukul_device="urukul9910_aom_doublepass_689_red_injection",
