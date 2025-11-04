5982fb487a131186f0752c5684d4870955d92211
Charles Baynham <c.baynham@imperial.ac.uk>
2025-11-04 17:09:03 +0000

    sneaky hidden var call

--- repository/tests/test_painted_pulse.py
+++ repository/tests/test_painted_pulse.py
@@ -50,7 +50,7 @@ class TestDiffractionCompensatedQuadraticFrag(ExpFragment):
 
         self.core.break_realtime()
         self.painter.start_output()
-        logger.warning("The pulse duration: ", self.painter.pulse_duration.get())
+        logger.warning("The pulse duration: ", self.painter._step_duration)
         logger.warning("Hey I'm starting minute")
         delay(10.0)
         self.painter.stop_output()
