85080665282bb793a484f64e171ddac43685e536
Ultracold Strontium Laboratory <stronlab@imperial.ac.uk>
2025-11-17 13:30:32 +0000

    no 689 clearout

--- repository/dipole_trap/measure_single_dipole_trap.py
+++ repository/dipole_trap/measure_single_dipole_trap.py
@@ -34,7 +34,6 @@ from repository.lib.experiment_templates.mixins.XODT_loading import LoadSingleXO
 from repository.lib.experiment_templates.mixins.XODT_loading import (
     LoadSingleXODTWithRampUpMixin,
 )
-from repository.lib.experiment_templates.mixins.XODT_molasses import ClearOut689Mixin
 from repository.lib.experiment_templates.mixins.XODT_molasses import (
     XODTSingleMolassesMixin,
 )
@@ -46,7 +45,7 @@ class MeasureSingleXODTBGCorrectedFrag(
     FLIRMeasurementMixin,
     BGCorrectedAndorImageSingleXODT,
     LoadSingleXODTWithRampUpMixin,
-    ClearOut689Mixin,
+    # ClearOut689Mixin,
 ):
     """
     Make Single XODT, clearout side atoms and image twice for BG subtraction
