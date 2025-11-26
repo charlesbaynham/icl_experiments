08ec67c0ed3379600bcc34267a786ed0ce59eb28
Dillen Lee <dillen.lee20@imperial.ac.uk>
2025-11-26 14:45:00 +0000

    tentative addition of the gravity and diffraction compensated quadratic shaped pulse

--- repository/lib/fragments/painted_pulse.py
+++ repository/lib/fragments/painted_pulse.py
@@ -1,6 +1,7 @@
 import logging
 
 import numpy as np
+from numpy import log, abs, sqrt
 from ndscan.experiment import *
 from ndscan.experiment.parameters import FloatParamHandle
 from scipy.optimize import root_scalar
@@ -119,5 +120,225 @@ class DiffractionCompensatedQuadraticShapedPulse(FrequencyShapedPulse):
 
         self._old_depth = self.mod_depth.get()
         self._old_epsilon = self.epsilon.get()
+
+        return return_value
+
+class GravityAndDiffractionCompensatedQuadraticShapedPulse(FrequencyShapedPulse):
+    """
+    A class of pulse that generates a gravity compensated quadratic pulse with diffraction correction.
+ 
+    """
+    def build_fragment(self, automatic_trigger: Optional[bool] = False, *args, **kwargs):
+        self.setattr_param(
+            "epsilon",
+            FloatParam,
+            description="Efficiency of the AOM at the edge of the pulse",
+            default=0.8,
+            min=0.0,
+            max=0.999,
+        )
+        self.epsilon: FloatParamHandle
+
+        self.setattr_param(
+            "AOM_eff_shift",
+            FloatParam,
+            description="Shift of the efficiency of the centre AOM frequency",
+            default=0.,
+            min=-0.999,
+            max=0.999,
+        )
+        self.aom_shift: FloatParamHandle
+
+        self.setattr_param(
+            "g",
+            FloatParam,
+            description="Gradient of gravity compensation",
+            default=1,
+            min = 0.0,
+            max = 1,
+        )
+        self.g : FloatParamHandle
+
+        self.setattr_param(
+            "curve_factor",
+            FloatParam,
+            description="Curvature of the trap",
+            default=1,
+            min = 0.0,
+            max = 1,
+        )
+        self.curve_factor : FloatParamHandle
+
+        self.setattr_param(
+            "int_k",
+            FloatParam,
+            description="Integrated time average power",
+            default=10,
+            min = 0.0,
+            max = 1000,
+        )
+        self.int_k : FloatParamHandle
+
+        self.setattr_param(
+            "cubic_correction",
+            FloatParam,
+            description="Cubic correction term",
+            default=0,
+            min = -1.0,
+            max = 1,
+        )
+        self.cubic_correction : FloatParamHandle
+        
+        self.setattr_param(
+            "quartic_correction",
+            FloatParam,
+            description="Quartic correction term",
+            default=0,
+            min = -1.0,
+            max = 1,
+        )
+        self.quartic_correction : FloatParamHandle
+
+        self.setattr_param(
+            "mod_depth",
+            FloatParam,
+            description="Modulation depth of the scan",
+            default=1e3,
+            unit="kHz",
+            min=1.0,
+            max=200e6,
+        )
+        self.mod_depth: FloatParamHandle
+
+        self.setattr_param(
+            "centre_frequency",
+            FloatParam,
+            description="Centre frequency of the shaped pulse",
+            default=100e6,
+            unit="MHz",
+            min=0.0,
+            max=4e8,
+        )
+        self.centre_frequency: FloatParamHandle
+
+        # Kernel params
+        self._old_frequency = -1.0
+        self._old_depth = -1.0
+        self._old_epsilon = -1.0
+        self._old_grav = -1.0
+        self._old_curve = -1.0
+        self._old_cubic = -1.0
+        self._old_quartic = -1.0
+        self._old_k = -1.0
+
+        # Do we want to automatically trigger the pulse in device_setup?
+        self.automatic_trigger = automatic_trigger
+
+        return super().build_fragment(
+            centre_frequency_param_handle=self.centre_frequency, *args, **kwargs
+        )
+
+    @kernel
+    def device_setup(self):
+        # Rewrite the device setup
+        self.device_setup_base()
+        self.core.break_realtime()
+        self.prepare_pulse()
+
+        if self.automatic_trigger is True:
+           self.start_output() 
+
+    def transform_coeffs(self):
+        """
+        This method transforms the physics parameters into qudratic coefficients  
+        """
+
+        grad = self.g.get()
+        mod_depth = self.mod_depth.get()
+        int_k = self.int_k.get()
+        curvature = self.curve_factor.get()
+
+        p = (grad * mod_depth - int_k / (2 * mod_depth)) * curvature + int_k / (2 * mod_depth)
+        coeff_a = 3 / (2 * mod_depth**2) * (p - int_k / (2 * mod_depth)) 
+        coeff_b = grad
+        coeff_c = 0.5 * (3 * int_k / (2 * mod_depth) - p)
+
+        # Ensure that we have a negative curvature in the shape of the trap.
+        assert coeff_a < 0
+
+        return coeff_a, coeff_b, coeff_c
+
+        
+
+    def generate_frequencies(self, n_words) -> np.ndarray:
+        """
+        Generate the gravity and diffraction compensating frequency modulated pulse.
+
+        The output will be normalized to 0 -> +1.
+        """
+
+        g = self.epsilon.get()
+        t_1 = self.cubic_correction.get()
+        t_2 = self.quartic_correction.get()
+        k_1, k_2, k_3 = self.transform_coeffs()
+
+        if (n_words % 2) != 0:
+            raise ValueError("n_words must be even")
+
+        # Generate a numpy array of the correct size
+        n_half = n_words // 2
+
+
+        relation = lambda x : ((t_2+(g**2-2*g+1)*k_2)*log(abs((g-1)*x**2+1)))/(2*g**3-6*g**2+6*g-2)+((t_1+(g**2-2*g+1)*k_3+(1-g)*k_1)*log(abs((2*g-2)*x-2*sqrt(1-g))/abs((2*g-2)*x+2*sqrt(1-g))))/(2*sqrt(1-g)*(g**2-2*g+1))+((3*g-3)*t_2*x**4+(4*g-4)*t_1*x**3-6*t_2*x**2+((12*g-12)*k_1-12*t_1)*x)/(12*g**2-24*g+12)
+
+
+        # This is a bit of a hack, we want to find the maximum value of t, so we can rearrange the above equation for t and f = 1.
+        # This expression is essentially the same but without the t component!
+        t_max = relation(-1)
+        t_min = relation(1)
+        calc_ts = np.linspace(-t_min, t_max, n_half)
+        roots = np.array(
+            list(
+                map(
+                    lambda t: root_scalar(lambda x : relation(x) - t, method="newton", x0=0).root,
+                    calc_ts,
+                )
+            )
+        )
+        detunings = np.zeros(2 * n_half)
+        detunings[:n_half] = roots
+        detunings[n_half:] = -1 * roots
+        detunings *= self.mod_depth.get()
+
+        logger.warning(detunings)
+
+        return detunings
+
+    @kernel
+    def is_recalc_needed(self) -> bool:
+        """
+        Check if any of the parameters have changed.
+        """
+
+        return_value = False
+
+        if (
+            self.mod_depth.get() != self._old_depth
+            or self.epsilon.get() != self._old_epsilon
+            or self.g.get() != self._old_grav
+            or self.curve_factor.get() != self._old_curve
+            or self.cubic_correction.get() != self._old_cubic
+            or self.quartic_correction.get() != self._old_quartic
+            or self.int_k.get() != self._old_k
+        ):
+            return_value = True
+
+        self._old_depth = self.mod_depth.get()
+        self._old_epsilon = self.epsilon.get()
+        self._old_grav = self.g.get()
+        self._old_curve = self.curve_factor.get()
+        self._old_cubic = self.cubic_correction.get()
+        self._old_quartic = self.quartic_correction.get()
+        self._old_k = self.int_k.get()
 
         return return_value
