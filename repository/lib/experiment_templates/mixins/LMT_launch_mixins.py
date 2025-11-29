1e190f854a148cff8db04e78afdb011f841ead68
Ultracold Strontium Laboratory <stronlab@imperial.ac.uk>
2025-11-29 14:50:16 +0000

    gives separation of the two arms

--- repository/lib/experiment_templates/mixins/LMT_launch_mixins.py
+++ repository/lib/experiment_templates/mixins/LMT_launch_mixins.py
@@ -97,6 +97,7 @@ class LMTBase(
             self.setattr_fragment("clock_opll", ClockOPLLController)
             self.clock_opll: ClockOPLLController
 
+    # use if we start in the excited state
     @kernel
     def lmt_series(self, offset_det, N):
 
@@ -138,13 +139,51 @@ class LMTBase(
             t_end_pulse = now_mu()
             total_ramp_time = self.core.mu_to_seconds(t_end_pulse - t_start_ramp)
 
+    # use if we start in the ground state
+    @kernel
+    def lmt_series_start_up(self, offset_det, N):
+        kick = self.momentum_kick.get()
+        total_ramp_time = 0.0
+
+        t_start_ramp = now_mu()
+        for i in range(N):
+
+            # start with up pulse
+            if i % 2 == 0:
+                down_offset = 0.0
+                pulse_type = "up"
+            else:
+                down_offset = self.down_offset_detuning.get()
+                pulse_type = "down"
+
+            f_i = (
+                start_opll_offset
+                + (-1) ** (i) * total_ramp_time * ramp_rate
+                + i * (-1) ** (i + 1) * kick
+                + (-1) ** (i + 1) * (offset_det + down_offset)
+            )
+
+            # fire the pulse
+            self.fire_lmt_pulse(f_i, pulse_type)
+
+            # Clear out the ground state
+            # if pulse_type == "down":
+            #     self.fluorescence_pulse.do_imaging_pulse(
+            #         duration=self.clearout_duration.get(),
+            #         ignore_final_shutters=True,
+            #     )
+            #     delay(8e-9)
+
+            delay(100e-6)
+            t_end_pulse = now_mu()
+            total_ramp_time = self.core.mu_to_seconds(t_end_pulse - t_start_ramp)
+
     @kernel
     def fire_lmt_pulse(self, start_freq, type):
         # stop the ramp
         self.clock_opll.clock_frequency_ramper.stop_ramp()
         # set the offset frequency
         self.clock_opll.clock_OPLL_offset.set(start_freq)
-        delay(8e-9)
 
         if type == "down":
             # ramp the offset downwards
@@ -270,6 +309,8 @@ class LMTInterferometryMixin(
 
     @kernel
     def do_clock_interferometry(self):
+        self.prepare_clock_delivery_aom()
+        delay_mu(16)
 
         N = self.lmt_pulses_number.get()
         bs1_lmt_offset = self.bs1_lmt_offset_detuning.get()
@@ -277,28 +318,30 @@ class LMTInterferometryMixin(
         lower_mirror_offset = self.lower_mirror_offset_detuning.get()
         t_pi_up = self.spectroscopy_pulse_time.get()
 
-        self.prepare_clock_delivery_aom()
-
         t_start_first_pulse_mu = now_mu()
         # PI/2 PULSE UP BEAM
         at_mu(t_start_first_pulse_mu)
         self.clock_up_dds.sw.on()
-        delay(t_pi_up / 2)
+        delay(t_pi_up / 2)  # FIXME should be t_pi_up / 2
         self.clock_up_dds.sw.off()
         t_end_pi_by_2_mu = now_mu()
-        delay(25e-6)
 
-        # LMT sequence on upper arm
-        self.lmt_series(bs1_lmt_offset, (N - 1))
+        # LMT sequence on upper arm, starting on the ground state
+        self.lmt_series_start_up(bs1_lmt_offset, (N - 1))
 
-        # Phase step
-        t_start_upper_mirror_mu = t_end_pi_by_2_mu + self.core.seconds_to_mu(
-            self.delay_between_interferometry_pulses.get()
-        )
+        # stop the ramp
+        # self.clock_opll.clock_frequency_ramper.stop_ramp()
+        # # set the offset frequency
+        # self.clock_opll.clock_OPLL_offset.set(start_opll_offset)
 
-        # Mirror pulse upper arm
-        at_mu(t_start_upper_mirror_mu)
-        self.lmt_series(upper_mirror_offset, N)
+        # # Phase step
+        # t_start_upper_mirror_mu = t_end_pi_by_2_mu + self.core.seconds_to_mu(
+        #     self.delay_between_interferometry_pulses.get()
+        # )
+
+        # # Mirror pulse upper arm
+        # at_mu(t_start_upper_mirror_mu)
+        # self.lmt_series_start_up(upper_mirror_offset, 0)  # N)
 
         # # Mirror pulse lower arm
         # self.lmt_series(lower_mirror_offset, N)
