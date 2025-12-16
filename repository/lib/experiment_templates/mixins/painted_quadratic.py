51f97d0472fa5711d172eb8cf57abae8aa9d3913
Ultracold Strontium Laboratory <stronlab@imperial.ac.uk>
2025-12-16 11:42:31 +0000

    increased delay

--- repository/lib/experiment_templates/mixins/painted_quadratic.py
+++ repository/lib/experiment_templates/mixins/painted_quadratic.py
@@ -229,6 +229,6 @@ class AdiabaticCoolingWithPaintedQuadraticMixin(DipoleTrapWithExperiment):
         self.adiabatic_cooling_ramp.do_phase()
         delay(DELAY_BETWEEN_RTIO_EVENTS)
         self.dipole_beam_controller.turn_off_dipole_beams()
-        delay(DELAY_BETWEEN_RTIO_EVENTS)
+        delay(1e-6)
         self.dipole_beam_controller.turn_off_painter_suservo()
         delay(DELAY_BETWEEN_RTIO_EVENTS)
