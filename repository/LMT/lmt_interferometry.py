be51a6c8fdacd0c1bfd437a833fbfa96d503c3f6
acj320 <alice.josset20@ic.ac.uk>
2025-12-18 14:56:03 +0000

    add alice's experiments

---
+++ repository/LMT/lmt_interferometry.py
@@ -0,0 +1,153 @@
+from artiq.coredevice.core import Core
+from artiq.language import delay
+from artiq.language import kernel
+from artiq.language import now_mu
+from ndscan.experiment import *
+from ndscan.experiment.entry_point import make_fragment_scan_exp
+
+from repository.lib import constants
+from repository.lib.experiment_templates.dipole_trap_experiment import (
+    DipoleTrapWithExperiment,
+)
+from repository.lib.experiment_templates.mixins.andor_imaging.em_gain import EMGain
+from repository.lib.experiment_templates.mixins.andor_imaging.normalised_fast_kinetics import (
+    NormalisedDipoleTrapFastKineticsMixin,
+)
+from repository.lib.experiment_templates.mixins.andor_imaging.normalised_fast_kinetics_base import (
+    NormalisedFastKineticsRepumpedMixin,
+)
+from repository.lib.experiment_templates.mixins.clock_shelving import (
+    ClockShelvingAndClearoutDipoleTrapMixin,
+)
+from repository.lib.experiment_templates.mixins.doppler_compensation import (
+    DopplerCompensationForLMTMixin,
+)
+from repository.lib.experiment_templates.mixins.evaporation_mixin import (
+    FieldOnlyRampInEvapMixin,
+)
+from repository.lib.experiment_templates.mixins.flir_blue_mot_measurement import (
+    FLIRBlueMOTMeasurementMixin,
+)
+from repository.lib.experiment_templates.mixins.LMT_launch_mixins import (
+    LMTInterferometryMixin,
+)
+from repository.lib.experiment_templates.mixins.LMT_launch_mixins import (
+    LMTLaunchMixin,
+    ShapedFirstPulseLMTInterferometryMixin,
+)
+from repository.lib.experiment_templates.mixins.optical_pumping import (
+    OpticalPumpingWithFieldSettingDipoleTrapMixin,
+)
+from repository.lib.experiment_templates.mixins.XODT_loading import LoadSingleXODTMixin
+from repository.lib.experiment_templates.mixins.XODT_molasses import (
+    XODTSingleMolassesPlusDipoleRampMixin,
+)
+
+
+class LMTInterferometryFrag(
+    LMTInterferometryMixin,
+    NormalisedDipoleTrapFastKineticsMixin,
+    NormalisedFastKineticsRepumpedMixin,
+    EMGain,
+    # FLIRBlueMOTMeasurementMixin,
+    LoadSingleXODTMixin,
+    XODTSingleMolassesPlusDipoleRampMixin,
+    OpticalPumpingWithFieldSettingDipoleTrapMixin,
+    FieldOnlyRampInEvapMixin,
+    ClockShelvingAndClearoutDipoleTrapMixin,
+    DopplerCompensationForLMTMixin,
+    DipoleTrapWithExperiment,
+):
+    """
+    LMT interferometry without launch
+
+    """
+
+    @kernel
+    def DMA_initialization_hook(self):
+        self.DMA_initialization_hook_default()
+        self.DMA_initialization_hook_loading_xodt_mot()
+        self.DMA_initialization_hook_xodt_molasses()
+        self.DMA_initialization_hook_evap_with_field_ramp()
+
+    @kernel
+    def post_sequence_cleanup_hook(self):
+        self.post_sequence_cleanup_hook_base()
+        self.post_sequence_cleanup_hook_andor()
+        self.post_sequence_cleanup_hook_shelving()
+
+
+class LMTInterferometryWithLaunchFrag(
+    LMTInterferometryMixin,
+    LMTLaunchMixin,
+    NormalisedDipoleTrapFastKineticsMixin,
+    NormalisedFastKineticsRepumpedMixin,
+    EMGain,
+    # FLIRBlueMOTMeasurementMixin,
+    LoadSingleXODTMixin,
+    XODTSingleMolassesPlusDipoleRampMixin,
+    OpticalPumpingWithFieldSettingDipoleTrapMixin,
+    FieldOnlyRampInEvapMixin,
+    ClockShelvingAndClearoutDipoleTrapMixin,
+    DopplerCompensationForLMTMixin,
+    DipoleTrapWithExperiment,
+):
+    """
+    LMT interferometry with launch
+
+    """
+
+    @kernel
+    def DMA_initialization_hook(self):
+        self.DMA_initialization_hook_default()
+        self.DMA_initialization_hook_loading_xodt_mot()
+        self.DMA_initialization_hook_xodt_molasses()
+        self.DMA_initialization_hook_evap_with_field_ramp()
+
+    @kernel
+    def post_sequence_cleanup_hook(self):
+        self.post_sequence_cleanup_hook_base()
+        self.post_sequence_cleanup_hook_andor()
+        self.post_sequence_cleanup_hook_shelving()
+
+
+class ShapedFirstPulseLMTInterferometryFrag(
+    ShapedFirstPulseLMTInterferometryMixin,
+    NormalisedDipoleTrapFastKineticsMixin,
+    NormalisedFastKineticsRepumpedMixin,
+    EMGain,
+    # FLIRBlueMOTMeasurementMixin,
+    LoadSingleXODTMixin,
+    XODTSingleMolassesPlusDipoleRampMixin,
+    OpticalPumpingWithFieldSettingDipoleTrapMixin,
+    FieldOnlyRampInEvapMixin,
+    ClockShelvingAndClearoutDipoleTrapMixin,
+    DopplerCompensationForLMTMixin,
+    DipoleTrapWithExperiment,
+):
+    """
+    LMT interferometry with shaped selective pulses
+
+    """
+
+    @kernel
+    def DMA_initialization_hook(self):
+        self.DMA_initialization_hook_default()
+        self.DMA_initialization_hook_loading_xodt_mot()
+        self.DMA_initialization_hook_xodt_molasses()
+        self.DMA_initialization_hook_evap_with_field_ramp()
+
+    @kernel
+    def post_sequence_cleanup_hook(self):
+        self.post_sequence_cleanup_hook_base()
+        self.post_sequence_cleanup_hook_andor()
+        self.post_sequence_cleanup_hook_shelving()
+
+
+LMTInterferometryExp = make_fragment_scan_exp(
+    LMTInterferometryFrag, max_rtio_underflow_retries=0
+)
+LMTInterferometryWithLaunch = make_fragment_scan_exp(LMTInterferometryWithLaunchFrag)
+ShapedFirstPulseLMTInterferometry = make_fragment_scan_exp(
+    ShapedFirstPulseLMTInterferometryFrag
+)
