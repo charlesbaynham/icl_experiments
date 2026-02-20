23d8623122f77670cebe19dc8cda12b3c36675a7
Stronlab <stronlab@imperial.ac.uk>
2026-02-20 14:33:08 +0000

    IP

--- flake.nix
+++ flake.nix
@@ -15,8 +15,8 @@
       # This is so that the server can run other ARTIQ sessions bound to other
       # IP addresses.
       bind_settings = {
-        bind_command = "--no-localhost-bind --bind 10.137.1.252";
-        connection_ip = "10.137.1.252";
+        bind_command = "--bind \*";
+        connection_ip = "127.0.0.1";
       };
     in flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
