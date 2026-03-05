df4fd5c0309246ab557d7fd2e92bbaa5de3bcee2
Ultracold Strontium Laboratory <stronlab@imperial.ac.uk>
2026-03-05 14:29:54 +0000

    increased attenuation of the squeezing

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -815,7 +815,7 @@ SUSERVOED_BEAMS = [
     SUServoedBeam(
         "squeezing_cavity_698_transmission",
         frequency=80e6,
-        attenuation=4.0,
+        attenuation=5.0,
         suservo_device="suservo_aom_698_squeezing_cavity_transmission",
         servo_enabled=True,
         initial_amplitude=1,
