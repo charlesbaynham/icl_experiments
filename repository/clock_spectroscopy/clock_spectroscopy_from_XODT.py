fdea6a2e2dad46a6b0df1c9db658cafce47aafb3
Ultracold Strontium Laboratory <stronlab@imperial.ac.uk>
2026-03-17 15:48:32 +0000

    nomal molasses in up spec

--- repository/clock_spectroscopy/clock_spectroscopy_from_XODT.py
+++ repository/clock_spectroscopy/clock_spectroscopy_from_XODT.py
@@ -52,9 +52,6 @@ from repository.lib.experiment_templates.mixins.XODT_loading import (
     LoadXXODTWithTransparencyBeamMixin,
 )
 from repository.lib.experiment_templates.mixins.XODT_molasses import (
-    XODTRetroedMolassesPlusDipoleRampMixin,
-)
-from repository.lib.experiment_templates.mixins.XODT_molasses import (
     XODTSingleMolassesPlusDipoleRampMixin,
 )
 
@@ -124,8 +121,8 @@ class ClockSpecFromSingleXODTEvaporatedShelvingFrag(
     EMGain,
     FLIRBlueMOTMeasurementMixin,
     LoadSingleXODTMixin,
-    # XODTSingleMolassesPlusDipoleRampMixin,
-    XODTRetroedMolassesPlusDipoleRampMixin,
+    XODTSingleMolassesPlusDipoleRampMixin,
+    # XODTRetroedMolassesPlusDipoleRampMixin,
     EvaporationThreeRampsWithFieldRampMixin,
     # OpticalPumpingWithFieldSettingDipoleTrapMixin,
     ClockShelvingAndClearoutDipoleTrapMixin,
