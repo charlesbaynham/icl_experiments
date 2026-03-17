2b316ddd2b686cf5d71f637d0c086a75ce4642e6
Ultracold Strontium Laboratory <stronlab@imperial.ac.uk>
2026-03-17 16:22:08 +0000

    clock freq

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -749,7 +749,7 @@ SUSERVOED_BEAMS = [
     ),
     SUServoedBeam(
         "clock_delivery",
-        99.653e6,
+        99.63e6,
         9,
         "suservo_aom_698_clock_delivery",
         servo_enabled=True,
