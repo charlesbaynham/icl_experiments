13b71f1ff32846d968661ac5e1657fdfa7d8eec7
Ultracold Strontium Laboratory <stronlab@imperial.ac.uk>
2026-03-20 13:58:26 +0000

    more attenuation to transfer cavity

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -103,7 +103,7 @@ URUKULED_BEAMS = [
     UrukuledBeam(
         "blue_xfer_offset",
         frequency=61e6,
-        attenuation=26.0,
+        attenuation=27.0,
         urukul_device="urukul9910_aom_doublepass_461_to_xfer_cavity",
     ),
     UrukuledBeam(
