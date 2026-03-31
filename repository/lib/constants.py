62d99b75c3670c0fd5f67280108d8b751de61516
Ultracold Strontium Laboratory <stronlab@imperial.ac.uk>
2026-03-31 11:42:08 +0100

    drift in clock freq

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -749,7 +749,7 @@ SUSERVOED_BEAMS = [
     ),
     SUServoedBeam(
         "clock_delivery",
-        99.621e6,
+        99.606e6,
         9,
         "suservo_aom_698_clock_delivery",
         servo_enabled=True,
