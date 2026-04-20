86bf01e3ecf280a5a833730734ac541b8faf79af
Charles Baynham <c.baynham@imperial.ac.uk>
2026-04-20 17:31:44 +0100

    removed 25 pixel height from excited

--- repository/lib/experiment_templates/mixins/andor_imaging/single_fast_kinetics.py
+++ repository/lib/experiment_templates/mixins/andor_imaging/single_fast_kinetics.py
@@ -68,10 +68,7 @@ def calculate_grabber_rois(
             x0 - bg_width,
             y0 + i * (fast_kinetics_height - excited_shift) - fast_kinetics_offset,
             x0,
-            y1
-            + i * (fast_kinetics_height - excited_shift)
-            - fast_kinetics_offset
-            + 25,  # FIXME Debugging code? Uncommitted
+            y1 + i * (fast_kinetics_height - excited_shift) - fast_kinetics_offset,
         ]
         for i in range(num_images)
     ] + [
@@ -79,10 +76,7 @@ def calculate_grabber_rois(
             x1,
             y0 + i * (fast_kinetics_height - excited_shift) - fast_kinetics_offset,
             x1 + bg_width,
-            y1
-            + i * (fast_kinetics_height - excited_shift)
-            - fast_kinetics_offset
-            + 25,  # FIXME Debugging code? Uncommitted
+            y1 + i * (fast_kinetics_height - excited_shift) - fast_kinetics_offset,
         ]
         for i in range(num_images)
     ]
