b7d874c4704f924a79d6ac57808c3571e0b51d4c
Charles Baynham <c.baynham@imperial.ac.uk>
2026-04-28 17:44:37 +0100

    red freqs

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -56,7 +56,7 @@ USE_LATTICE_MODE = False
 URUKULED_BEAMS = [
     UrukuledBeam(
         name="red_doublepass_injection",
-        frequency=364.8e6,
+        frequency=364.7e6,
         amplitude=1.0,
         attenuation=0.0,
         urukul_device="urukul9910_aom_doublepass_689_red_injection",
@@ -782,7 +782,7 @@ SUSERVOED_BEAMS = [
     ),
     SUServoedBeam(
         "clock_delivery",
-        99.606e6,
+        99.555e6,
         9,
         "suservo_aom_698_clock_delivery",
         servo_enabled=True,
