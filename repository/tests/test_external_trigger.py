b9454dd4f2cd38a4a1de1042866c89f43eff1d23
Stronlab <stronlab@imperial.ac.uk>
2026-05-18 12:52:28 +0100

    Debug

--- repository/tests/test_external_trigger.py
+++ repository/tests/test_external_trigger.py
@@ -2,6 +2,7 @@ from artiq.coredevice.core import Core
 from artiq.coredevice.ttl import TTLInOut
 from artiq.experiment import EnvExperiment
 from artiq.experiment import kernel
+from artiq.language import delay
 from artiq.language import kernel
 from artiq.language import now_mu
 from ndscan.experiment import ExpFragment
@@ -43,4 +44,17 @@ class TestExternalTriggerDirect(EnvExperiment):
 
     @kernel
     def run(self):
-        print("Hello")
+        self.core.break_realtime()
+
+        self.ttl1.input()
+
+        delay(1e-6)
+
+        while True:
+            self.core.break_realtime()
+            fin = self.ttl1.gate_rising(1.0)
+            # while True:
+            r = self.ttl1.count(fin)
+            print(r)
+            # if r == -1:
+            #     break
