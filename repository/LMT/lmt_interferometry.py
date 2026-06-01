ae9f039f7fcfd905fa5769b382547c576dddcdea
acj320 <alice.josset20@ic.ac.uk>
2026-06-01 20:07:32 +0100

    symmetric lmt

--- repository/LMT/lmt_interferometry.py
+++ repository/LMT/lmt_interferometry.py
@@ -26,6 +26,9 @@ from repository.lib.experiment_templates.mixins.doppler_compensation import (
 from repository.lib.experiment_templates.mixins.evaporation_mixin import (
     FieldOnlyRampInEvapMixin,
 )
+from repository.lib.experiment_templates.mixins.lmt_interferometry_symmetric_mixin import (
+    LMTSymmetricInterferometryMixin,
+)
 from repository.lib.experiment_templates.mixins.LMT_launch_mixins import (
     LMTInterferometryMixin,
 )
@@ -160,6 +163,45 @@ class LMTInterferometryWithShapedDoubleLaunchFrag(
         self.post_sequence_cleanup_hook_loading()
 
 
+class LMTInterferometrySymmetricFrag(
+    LMTSymmetricInterferometryMixin,
+    NormalisedDipoleTrapFastKineticsMixin,
+    NormalisedFastKineticsRepumpedMixin,
+    EMGainMixin,
+    # FLIRBlueMOTMeasurementMixin,
+    XODTSingleMolassesPlusDipoleRampMixin,
+    OpticalPumpingWithFieldSettingDipoleTrapMixin,
+    FieldOnlyRampInEvapMixin,
+    ClockShelvingAndClearoutDipoleTrapMixin,
+    AdiabaticCoolingWithPaintedQuadraticMixin,
+    LoadSingleXODTWithPainterMixin,
+    DopplerCompensationForLMTMixin,
+    DipoleTrapWithExperimentBase,
+):
+    """
+    Symmetric LMT interferometry
+
+    """
+
+    @kernel
+    def DMA_initialization_hook(self):
+        self.DMA_initialization_hook_redmot_default()
+        self.DMA_initialization_hook_dipole_trap_default()
+        self.DMA_initialization_hook_loading_xodt_mot()
+        self.DMA_initialization_hook_xodt_molasses()
+        self.DMA_initialization_hook_painter_on()
+        self.DMA_initialization_hook_adiabatic_cooling()
+        self.DMA_initialization_hook_evap_with_field_ramp()
+
+    @kernel
+    def post_sequence_cleanup_hook(self):
+        self.post_sequence_cleanup_hook_base()
+        self.post_sequence_cleanup_hook_andor()
+        self.post_sequence_cleanup_hook_shelving()
+        self.post_sequence_cleanup_hook_lmt()
+        self.post_sequence_cleanup_hook_loading()
+
+
 class LMTInterferometryWithLaunchFrag(
     LMTInterferometryMixin,
     LMTLaunchMixin,
@@ -243,3 +285,5 @@ LMTInterferometryWithDoubleLaunch = make_fragment_scan_exp(
 LMTInterferometryWithShapedDoubleLaunch = make_fragment_scan_exp(
     LMTInterferometryWithShapedDoubleLaunchFrag, max_rtio_underflow_retries=0
 )
+
+LMTInterferometrySymmetric = make_fragment_scan_exp(LMTInterferometrySymmetricFrag)
