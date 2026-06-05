2c41002b547207f8a3841ad373d90b44241f090f
Stronlab <stronlab@imperial.ac.uk>
2026-06-05 23:03:45 +0100

    wip

--- repository/lib/experiment_templates/mixins/LMT_launch_mixins.py
+++ repository/lib/experiment_templates/mixins/LMT_launch_mixins.py
@@ -926,7 +926,7 @@ class LMTInterferometryMixin(
     @kernel
     def do_clock_interferometry(self):
         N = self.lmt_pulses_number.get()
-        N_launch = 8
+        N_launch = 14
         t_pi_down = self.down_pulses_duration.get()
         t_first_pi = self.first_lmt_duration.get()
 
