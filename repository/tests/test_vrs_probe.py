00f1f62198361a27e7b151a94102a2338fb5b56e
Stronlab <stronlab@imperial.ac.uk>
2026-06-18 17:01:42 +0100

    make ttl 1 sec

--- repository/tests/test_vrs_probe.py
+++ repository/tests/test_vrs_probe.py
@@ -117,7 +117,7 @@ class TestRTBSetupFrag(ExpFragment):
         # Pulse the TTL for 10 ms
         logger.warning("Begin the pulse")
         self.core.break_realtime()
-        self.ttl.pulse(1e-3)
+        self.ttl.pulse(1)
         self.core.break_realtime()
         logger.warning("start the wait")
         delay(self.acquisition_time.get())
