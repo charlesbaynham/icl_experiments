1bf709b6261015281b0faf19ca01c7b52e087033
Charles Baynham <c.baynham@imperial.ac.uk>
2026-06-30 14:32:06 +0100

    Tweak clock

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -812,7 +812,7 @@ SUSERVOED_BEAMS = [
     ),
     SUServoedBeam(
         "clock_delivery",
-        99.436e6,
+        99.441e6,
         9,
         "suservo_aom_698_clock_delivery",
         servo_enabled=True,
