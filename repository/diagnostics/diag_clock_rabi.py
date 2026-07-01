f73287556f230b1bdf1f4b92ec978b85c893e78f
Charles Baynham <c.baynham@imperial.ac.uk>
2026-07-01 11:24:55 +0100

    Stubs

---
+++ repository/diagnostics/diag_clock_rabi.py
@@ -0,0 +1,21 @@
+from artiq.experiment import EnvExperiment
+
+
+class ClockRabiUpBeamDiagnostic(EnvExperiment):
+    def build(self):
+        pass
+
+    def run(self):
+        raise NotImplementedError(
+            "This is a stub and will be implemented in a feature branch."
+        )
+
+
+class ClockRabiDownBeamDiagnostic(EnvExperiment):
+    def build(self):
+        pass
+
+    def run(self):
+        raise NotImplementedError(
+            "This is a stub and will be implemented in a feature branch."
+        )
