5f8febf7b1071ec811168d89d2bbcfd64372f8e4
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2026-07-02 10:37:04 +0100

    change so that the mock actually works

--- device_db_config/_non_core_devices.py
+++ device_db_config/_non_core_devices.py
@@ -364,8 +364,8 @@ def get_non_core_devices(simulation_mode=False):
             "arguments": {
                 "address": "TCPIP::10.137.1.19::INSTR",
             },
-            "mockmodule": "repository.lib.devices.rohde_schwarz_devices",
-            "mockclass": "MockRSDevice",
+            "mockmodule": "repository.lib.mock_device",
+            "mockclass": "MockDevice",
         },
         # Example devices: edit to suit your lab
         # An example of a local device:
