50efd1a4c893e0949b3a0d8646bac26240fe1486
Imperial Stronlab (labserver) <stronlab@imperial.ac.uk>
2026-07-07 15:16:20 +0100

    vrs stub

---
+++ repository/squeezing/vacuum_rabi_splitting.py
@@ -0,0 +1,19 @@
+from repository.lib.utils import Stub
+
+
+class SingleVRSSweepFrag(Stub):
+    """
+    Single sided RF sweep on the 689 VRS AM
+
+    This sequence traps the atoms from a narrowband red mot and does a vacuum Rabi
+    splitting measurement. This includes preparing a scope to readout from the PMT
+    and triggering it while sweeping the amplitude modulator on the 689 probe beam.
+
+    This is a mixin - see the documentation for :mod:`~.red_mot_experiment` for
+    details.
+
+    Kernel hooks used (multiple mixins cannot use the same hooks)
+
+        * :meth:`~do_experiment_after_red_mot_hook`
+        * :meth:`~host_functions_after_experiment_hook`
+    """
