fdf3125e2d4e7458cc8f64bd97b8d6b5f4a96900
Charles Baynham <c.baynham@imperial.ac.uk>
2026-07-10 13:54:44 +0100

    Kernel

--- repository/tests/test_echo.py
+++ repository/tests/test_echo.py
@@ -1,16 +1,26 @@
 import time
 
+from artiq.coredevice.core import Core
 from artiq.experiment import EnvExperiment
+from artiq.language import kernel
 
 from repository.lib.test import this_str
 
 
 class TestEcho(EnvExperiment):
     def build(self):
-        pass
+        self.setattr_device("core")
+        self.core: Core
 
     def run(self):
         print("Hello, I'm an experiment!")
         print(this_str)
 
+        self.kernel()
+
         time.sleep(3)
+
+    @kernel
+    def kernel(self):
+        print("Hello from the kernel!")
+        print(this_str)
