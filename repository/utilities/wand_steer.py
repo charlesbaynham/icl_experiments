73c2dee9166d782d74dbd34d463bde3bdb3e2459
Imperial Strontium lab <stronlab@imperial.ac.uk>
2026-07-14 16:04:18 +0100

    Upload New File

--- repository/utilities/wand_steer.py
+++ repository/utilities/wand_steer.py
@@ -1,16 +1,24 @@
-import logging
+from enum import Enum
+from enum import unique
 
-from artiq.experiment import EnumerationValue
 from ndscan.experiment import ExpFragment
 from ndscan.experiment import make_fragment_scan_exp
 from ndscan.experiment.parameters import BoolParam
 from ndscan.experiment.parameters import BoolParamHandle
+from ndscan.experiment.parameters import EnumParam
 from ndscan.experiment.parameters import FloatParam
 from ndscan.experiment.parameters import FloatParamHandle
+from ndscan.experiment.parameters import ParamHandle
 
 from repository.lib.fragments.wand_steering import WandSteering
 
-logger = logging.getLogger(__name__)
+
+@unique
+class Lasers(Enum):
+    laser_461 = "461"
+    laser_707 = "707"
+    laser_679 = "679"
+    laser_688 = "688"
 
 
 class WANDSteerFrag(ExpFragment):
@@ -21,13 +29,13 @@ class WANDSteerFrag(ExpFragment):
     def build_fragment(self):
         self.set_default_scheduling(pipeline_name="wand")
 
-        self.setattr_argument(
+        self.setattr_param(
             "laser",
-            EnumerationValue(
-                ["461", "689", "707", "679", "641", "487", "688"], default="461"
-            ),
+            EnumParam,
+            "Laser to steer",
+            default=Lasers.laser_461,
         )
-        self.laser: str
+        self.laser: ParamHandle
 
         self.setattr_param(
             "offset",
@@ -39,43 +47,55 @@ class WANDSteerFrag(ExpFragment):
         self.offset: FloatParamHandle
 
         self.setattr_param(
-            "set_ref_freq", BoolParam, "Set reference frequency?", default=False
+            "initial_gain",
+            FloatParam,
+            default=1e-6,
+            description="Initial gain x 1e-6",
         )
-        self.set_ref_freq: BoolParamHandle
+        self.initial_gain: FloatParamHandle
 
         self.setattr_param(
-            "reference_freq",
+            "initial_poll_time",
             FloatParam,
-            "Reference frequency",
-            default=0.0,
-            unit="THz",
-            scale=1e12,
+            default=5,
+            description="Initial poll time",
+            unit="s",
         )
-        self.reference_freq: FloatParamHandle
+        self.initial_poll_time: FloatParamHandle
+
+        self.setattr_param("set_gain", BoolParam, "Set Gain Manually?", default=False)
+        self.set_gain: BoolParamHandle
+
+        self.setattr_param(
+            "set_poll_time", BoolParam, "Set Poll Time Manually?", default=False
+        )
+        self.set_poll_time: BoolParamHandle
+
+        self.setattr_param("leave_locked", BoolParam, "Leave locked?", default=True)
+        self.leave_locked: BoolParamHandle
 
         self.setattr_fragment("wand_steering", WandSteering)
         self.wand_steering: WandSteering
 
-        self.setattr_param("leave_locked", BoolParam, "Leave locked?", default=False)
-        self.leave_locked: BoolParamHandle
-
     def run_once(self):
-        if self.set_ref_freq.get():
-            logger.info(
-                "Setting laser %s reference frequency to %.0f THz",
-                self.laser,
-                self.reference_freq.get() * 1e-12,
-            )
-            self.wand_steering.wand_server.set_reference_freq(
-                laser=self.laser, f_ref=self.reference_freq.get()
-            )
+        if self.set_gain.get() == True:
+            initial_gain = self.initial_gain.get() * 1e-6
+        else:
+            initial_gain = None
+
+        if self.set_poll_time.get() == True:
+            initial_poll_time = self.initial_poll_time.get()
+        else:
+            initial_poll_time = None
 
         self.wand_steering.steer_wand(
-            self.laser,
+            self.laser.get().value,
             offset=self.offset.get(),
             timeout=20.0,
             required_accuracy=2e6,
             leave_locked=self.leave_locked.get(),
+            initial_gain=initial_gain,
+            initial_poll_time=initial_poll_time,
         )
 
 
