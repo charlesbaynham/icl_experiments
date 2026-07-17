a74d67e51a6b8a94106ea73a68b2b9c512e4e000
Strontium lab <stronlab@imperial.ac.uk>
2026-07-17 12:41:49 +0000

    clock freq

--- repository/lib/constants.py
+++ repository/lib/constants.py
@@ -812,7 +812,7 @@ SUSERVOED_BEAMS = [
     ),
     SUServoedBeam(
         "clock_delivery",
-        99.4058e6,
+        99.4038e6,
         9,
         "suservo_aom_698_clock_delivery",
         servo_enabled=True,
