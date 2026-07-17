4277c4899980835b09c2999943c7d1f57bca89fd
Dillen Lee <dillen.lee20@imperial.ac.uk>
2026-07-17 11:48:45 +0100

    this should just be array

--- repository/tests/test_vrs_probe.py
+++ repository/tests/test_vrs_probe.py
@@ -272,7 +272,7 @@ class TestDHOSetupFrag(ExpFragment):
 
     @rpc
     def get_data_from_scope(self):
-        data = self.rigol.get_waveform()
+        data = np.array(self.rigol.get_waveform())
         print(len(data))
         print(type(data))
         print(data)
