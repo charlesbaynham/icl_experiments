995ad285e5d0371bb42b347109c99605d18bc1df
Strontium lab <stronlab@imperial.ac.uk>
2026-07-17 15:19:07 +0000

    clearout after launch

--- repository/LMT_declarative/lmt_declarative.py
+++ repository/LMT_declarative/lmt_declarative.py
@@ -103,7 +103,7 @@ class DeclarativeLMTSymmetricMachZehnderFrag(
         Clearout(duration=2.2e-3),
         # Launch: alternating pi pulses walking the atoms up the momentum
         # ladder from |e, 1> to m = M_TOP
-        *ladder(start_m=1, n=N_LAUNCH, first_beam=Beam.DOWN, clearout_from=None),
+        *ladder(start_m=1, n=N_LAUNCH, first_beam=Beam.DOWN, clearout_from=-1),
         Wait(t=DROP, label="droptime"),
         # # Now do some actual interferometry
         # # %% LMT beamsplitter
